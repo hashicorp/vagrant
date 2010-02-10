4a6d6074d276aecd0f881ad6c6b9b40702c11f2d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-02-09 18:29:52 -0800

    Chef-solo configuration script is generated and uploaded.

--- lib/hobo/provisioning.rb
+++ lib/hobo/provisioning.rb
@@ -7,17 +7,17 @@ module Hobo
 
       # Share the cookbook folder. We'll use the provisioning path exclusively for
       # chef stuff.
-      cookbooks_path = File.join(Hobo.config.chef.provisioning_path, "cookbooks")
       @vm.share_folder("hobo-provisioning", File.expand_path(Hobo.config.chef.cookbooks_path, Env.root_path), cookbooks_path)
     end
 
     def run
       chown_provisioning_folder
       setup_json
+      setup_solo_config
     end
 
     def chown_provisioning_folder
-      logger.info "Setting permissions on deployment folder..."
+      logger.info "Setting permissions on provisioning folder..."
       SSH.execute do |ssh|
         ssh.exec!("sudo chown #{Hobo.config.ssh.username} #{Hobo.config.chef.provisioning_path}")
       end
@@ -26,6 +26,20 @@ module Hobo
     def setup_json
       logger.info "Generating JSON and uploading..."
       SSH.upload!(StringIO.new(Hobo.config.chef.json.to_json), File.join(Hobo.config.chef.provisioning_path, "dna.json"))
+    end
+
+    def setup_solo_config
+      solo_file = <<-solo
+file_cache_path "#{Hobo.config.chef.provisioning_path}"
+cookbook_path "#{cookbooks_path}"
+solo
+
+      logger.info "Uploading chef-solo configuration script..."
+      SSH.upload!(StringIO.new(solo_file), File.join(Hobo.config.chef.provisioning_path, "solo.rb"))
+    end
+
+    def cookbooks_path
+      File.join(Hobo.config.chef.provisioning_path, "cookbooks")
     end
   end
 end
--- test/hobo/provisioning_test.rb
+++ test/hobo/provisioning_test.rb
@@ -13,10 +13,15 @@ class ProvisioningTest < Test::Unit::TestCase
   context "initializing" do
     should "setup shared folder on VM for the cookbooks" do
       File.expects(:expand_path).with(Hobo.config.chef.cookbooks_path, Hobo::Env.root_path).returns("foo")
-      cookbooks_path = File.join(Hobo.config.chef.provisioning_path, "cookbooks")
+      Hobo::Provisioning.any_instance.expects(:cookbooks_path).returns("bar")
       vm = mock("vm")
-      vm.expects(:share_folder).with("hobo-provisioning", "foo", cookbooks_path)
+      vm.expects(:share_folder).with("hobo-provisioning", "foo", "bar")
       Hobo::Provisioning.new(vm)
+    end
+
+    should "return the proper cookbook path" do
+      cookbooks_path = File.join(Hobo.config.chef.provisioning_path, "cookbooks")
+      assert_equal cookbooks_path, @prov.cookbooks_path
     end
   end
 
@@ -41,6 +46,26 @@ class ProvisioningTest < Test::Unit::TestCase
       File.expects(:join).with(Hobo.config.chef.provisioning_path, "dna.json").once.returns("baz")
       Hobo::SSH.expects(:upload!).with("bar", "baz").once
       @prov.setup_json
+    end
+  end
+
+  context "generating and uploading chef solo configuration file" do
+    should "upload properly generate the configuration file using configuration data" do
+      expected_config = <<-config
+file_cache_path "#{Hobo.config.chef.provisioning_path}"
+cookbook_path "#{@prov.cookbooks_path}"
+config
+
+      StringIO.expects(:new).with(expected_config).once
+      @prov.setup_solo_config
+    end
+
+    should "upload this file as solo.rb to the provisioning folder" do
+      @prov.expects(:cookbooks_path).returns("cookbooks")
+      StringIO.expects(:new).returns("foo")
+      File.expects(:join).with(Hobo.config.chef.provisioning_path, "solo.rb").once.returns("bar")
+      Hobo::SSH.expects(:upload!).with("foo", "bar").once
+      @prov.setup_solo_config
     end
   end
 end
