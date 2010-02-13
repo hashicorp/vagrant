8f63272403a1843fdb6040b97821b363ae3318c8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-02-13 11:43:58 -0800

    Cleaned up the requiring of files in vagrant.rb

--- lib/vagrant.rb
+++ lib/vagrant.rb
@@ -2,20 +2,13 @@ libdir = File.dirname(__FILE__)
 $:.unshift(libdir)
 PROJECT_ROOT = File.join(libdir, '..')
 
-require 'ftools'
-require 'json'
-require 'pathname'
-require 'logger'
-require 'virtualbox'
-require 'net/ssh'
-require 'net/scp'
-require 'tarruby'
-require 'fileutils'
-require 'vagrant/busy'
-require 'vagrant/util'
-require 'vagrant/commands'
-require 'vagrant/config'
-require 'vagrant/env'
-require 'vagrant/provisioning'
-require 'vagrant/ssh'
-require 'vagrant/vm'
+# The libs which must be loaded prior to the rest
+%w{ftools json pathname logger virtualbox net/ssh
+  net/scp tarruby fileutils vagrant/util}.each do |f|
+  require f
+end
+
+# Glob require the rest
+Dir[File.join(PROJECT_ROOT, "lib", "vagrant", "**", "*.rb")].each do |f|
+  require f
+end
