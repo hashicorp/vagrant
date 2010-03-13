9da68bb1294b846bd0bfdbe49c131da42a5669e5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-03-13 03:08:26 -0800

    Expand paths properly for loading so that files already loaded aren't loaded twice

--- lib/vagrant.rb
+++ lib/vagrant.rb
@@ -4,12 +4,17 @@ PROJECT_ROOT = File.join(libdir, '..') unless defined?(PROJECT_ROOT)
 
 # The libs which must be loaded prior to the rest
 %w{tempfile open-uri json pathname logger uri net/http virtualbox net/ssh archive/tar/minitar
-  net/scp fileutils vagrant/util vagrant/actions/base vagrant/downloaders/base vagrant/actions/runner
+  net/scp fileutils}.each do |lib|
+  require lib
+end
+
+# The vagrant specific files which must be loaded prior to the rest
+%w{vagrant/util vagrant/actions/base vagrant/downloaders/base vagrant/actions/runner
   vagrant/config vagrant/provisioners/base vagrant/provisioners/chef}.each do |f|
-  require f
+  require File.expand_path(f, libdir)
 end
 
 # Glob require the rest
-Dir[File.join(PROJECT_ROOT, "lib", "vagrant", "**", "*.rb")].each do |f|
-  require f
+Dir[File.join(libdir, "vagrant", "**", "*.rb")].each do |f|
+  require File.expand_path(f, PROJECT_ROOT)
 end
