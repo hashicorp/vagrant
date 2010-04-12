1245a0c1c86d52aea58a62dff7d8ab811feb4080
Lincoln Stoll <lstoll@lstoll.net>
2010-04-12 22:30:06 +0200

    Shorten shared folder names
    On some setups, the generated name is truncated, which causes issues
    because they then look the same, and the kernel throws duplicate
    filename errors. Shorten the name so this doesn't happen

--- lib/vagrant/provisioners/chef_solo.rb
+++ lib/vagrant/provisioners/chef_solo.rb
@@ -16,13 +16,13 @@ module Vagrant
 
       def share_cookbook_folders
         host_cookbook_paths.each_with_index do |cookbook, i|
-          env.config.vm.share_folder("vcsc-#{i}", cookbook_path(i), cookbook)
+          env.config.vm.share_folder("vagrant-csc-#{i}", cookbook_path(i), cookbook)
         end
       end
 
       def share_role_folders
         host_role_paths.each_with_index do |role, i|
-          env.config.vm.share_folder("vcsr-#{i}", role_path(i), role)
+          env.config.vm.share_folder("vagrant-csr-#{i}", role_path(i), role)
         end
       end
 
