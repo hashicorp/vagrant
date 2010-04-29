822a5e3154bdecb5a05763215bd96e481b02faf6
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-04-28 20:47:58 -0700

    Linux shutdown state checking no longer requires the force param. Its default

--- lib/vagrant/systems/linux.rb
+++ lib/vagrant/systems/linux.rb
@@ -38,7 +38,7 @@ module Vagrant
         # occur within a reasonable amount of time (15 seconds by default),
         # then simply return and allow Vagrant to kill the machine.
         count = 0
-        while vm.vm.state(true) != :powered_off
+        while vm.vm.state != :powered_off
           count += 1
 
           return if count >= vm.env.config.linux.halt_timeout
