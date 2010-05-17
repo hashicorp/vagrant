5be932bf6c09b27f624e89c243774807fe4a4670
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-05-16 18:01:53 -0700

    Destroying properly updates the dotfile

--- lib/vagrant/actions/vm/destroy.rb
+++ lib/vagrant/actions/vm/destroy.rb
@@ -5,17 +5,18 @@ module Vagrant
         def execute!
           @runner.invoke_around_callback(:destroy) do
             destroy_vm
-            depersist
+            update_dotfile
           end
         end
 
         def destroy_vm
           logger.info "Destroying VM and associated drives..."
           @runner.vm.destroy(:destroy_medium => :delete)
+          @runner.vm = nil
         end
 
-        def depersist
-          @runner.env.depersist_vm
+        def update_dotfile
+          @runner.env.update_dotfile
         end
       end
     end
