f2587463842ebdb660cdd3ad81091aa88f802a0c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-07-05 17:18:16 +0200

    Middleware to destroy unused network interfaces

---
+++ test/vagrant/action/vm/destroy_unused_network_interfaces_test.rb
@@ -0,0 +1,46 @@
+require File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper')
+
+class DestroyUnusedNetworkInterfacesVMActionTest < Test::Unit::TestCase
+  setup do
+    @klass = Vagrant::Action::VM::DestroyUnusedNetworkInterfaces
+    @app, @env = mock_action_data
+
+    @vm = mock("vm")
+    @env["vm"] = @vm
+
+    @internal_vm = mock("internal")
+    @vm.stubs(:vm).returns(@internal_vm)
+
+    @instance = @klass.new(@app, @env)
+  end
+
+  context "calling" do
+    setup do
+      @network_adapters = []
+      @internal_vm.stubs(:network_adapters).returns(@network_adapters)
+    end
+
+    def stub_interface(length=5)
+      interface = mock("interface")
+      adapter = mock("adapter")
+      adapter.stubs(:host_interface_object).returns(interface)
+      interface.stubs(:attached_vms).returns(Array.new(length))
+
+      @network_adapters << adapter
+      interface
+    end
+
+    should "destroy only the unused network interfaces" do
+      stub_interface(5)
+      stub_interface(7)
+      results = [stub_interface(1), stub_interface(1)]
+
+      results.each do |result|
+        result.expects(:destroy).once
+      end
+
+      @app.expects(:call).with(@env).once
+      @instance.call(@env)
+    end
+  end
+end
