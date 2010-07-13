c78a3f3e3fd3d9a460a7e0f889230b9a0cbc7f4d
Brian Donovan <me@brian-donovan.com>
2010-07-13 07:59:24 -0700

    Include util.rb early so the included hook is set up properly.
    Fixes a NoMethodError running any command that invokes Environment.load!

--- lib/vagrant.rb
+++ lib/vagrant.rb
@@ -11,7 +11,7 @@ end
 require File.expand_path("util/glob_loader", libdir)
 
 # Load them up
-Vagrant::GlobLoader.glob_require(libdir, %w{util/stacked_proc_runner
+Vagrant::GlobLoader.glob_require(libdir, %w{util util/stacked_proc_runner
   downloaders/base config provisioners/base provisioners/chef systems/base
   commands/base commands/box action/exception_catcher hosts/base})
 
