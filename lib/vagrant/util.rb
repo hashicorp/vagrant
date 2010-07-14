549fad0b91e8b8d6f1e3fe1880c6b24e05666daf
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-07-13 21:35:47 -0700

    Move output helpers into the actual util class, gets rid of method missing on environment

--- lib/vagrant/util.rb
+++ lib/vagrant/util.rb
@@ -3,5 +3,21 @@ module Vagrant
     def self.included(base)
       base.extend(self)
     end
+
+    def error_and_exit(key, data = {})
+      abort <<-error
+=====================================================================
+Vagrant experienced an error!
+
+#{Translator.t(key, data).chomp}
+=====================================================================
+error
+    end
+
+    def wrap_output
+      puts "====================================================================="
+      yield
+      puts "====================================================================="
+    end
   end
 end
--- test/vagrant/util/output_helper_test.rb
+++
@@ -1,5 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'test_helper')
-
-class OutputHelperUtilTest < Test::Unit::TestCase
-  # TODO
-end
