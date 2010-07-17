f8909dcbfa08d466b742a7a29b6d7be868a555c1
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-07-17 09:58:30 -0700

    Fix `sed` calls for linux and add proper options for the UID/GID mapping.

--- lib/vagrant/hosts/linux.rb
+++ lib/vagrant/hosts/linux.rb
@@ -42,7 +42,7 @@ module Vagrant
         if $?.to_i == 0
           # Use sed to just strip out the block of code which was inserted
           # by Vagrant
-          system("sudo sed -e '/^# VAGRANT-BEGIN: #{env.vm.uuid}/,/^# VAGRANT-END: #{env.vm.uuid}/ d' -i bak /etc/exports")
+          system("sudo sed -e '/^# VAGRANT-BEGIN: #{env.vm.uuid}/,/^# VAGRANT-END: #{env.vm.uuid}/ d' -ibak /etc/exports")
         end
       end
     end
