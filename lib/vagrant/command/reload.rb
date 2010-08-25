5a1846f77e56fbf8c945ee6ce469258b603d15bd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-08-24 23:30:32 -0700

    `vagrant reload`

---
+++ lib/vagrant/command/reload.rb
@@ -0,0 +1,18 @@
+module Vagrant
+  module Command
+    class ReloadCommand < Base
+      desc "Reload the environment, halting it then restarting it."
+      register "reload"
+
+      def execute
+        target_vms.each do |vm|
+          if vm.created?
+            vm.reload
+          else
+            vm.env.ui.info "VM not created. Moving on..."
+          end
+        end
+      end
+    end
+  end
+end
