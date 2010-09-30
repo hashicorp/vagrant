465392f99c858565fc74f3fb41925877ff2d7095
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-09-30 00:55:00 -0700

    config.vagrant validation tests

--- test/vagrant/config/vagrant_test.rb
+++ test/vagrant/config/vagrant_test.rb
@@ -6,6 +6,38 @@ class ConfigVagrantTest < Test::Unit::TestCase
   end
 
   context "validation" do
-    # TODO
+    setup do
+      @config.dotfile_name = "foo"
+      @config.home = "foo"
+      @config.host = "foo"
+
+      @errors = Vagrant::Config::ErrorRecorder.new
+    end
+
+    should "be valid with given set of values" do
+      @config.validate(@errors)
+      assert @errors.errors.empty?
+    end
+
+    should "be invalid with no dotfile" do
+      @config.dotfile_name = nil
+
+      @config.validate(@errors)
+      assert !@errors.errors.empty?
+    end
+
+    should "be invalid with no home" do
+      @config.home = nil
+
+      @config.validate(@errors)
+      assert !@errors.errors.empty?
+    end
+
+    should "be invalid with no host" do
+      @config.host = nil
+
+      @config.validate(@errors)
+      assert !@errors.errors.empty?
+    end
   end
 end
