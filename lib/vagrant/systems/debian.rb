912463b3f95fd9a2243535488aa4df19f5a8052d
Chris Beauchamp <chris@chillibean.tv>
2011-02-07 16:59:34 +0000

    Changes to fix the fqdn

--- lib/vagrant/systems/debian.rb
+++ lib/vagrant/systems/debian.rb
@@ -26,6 +26,7 @@ module Vagrant
         vm.ssh.execute do |ssh|
           if !ssh.test?("sudo hostname | grep '#{name}'")
             ssh.exec!("sudo sed -i 's/.*$/#{name}/' /etc/hostname")
+            ssh.exec!("sudo sed -i 's@^\\(127[.]0[.]1[.]1[[:space:]]\\+\\)@\\1#{name} #{name.split('.')[0]} @' /etc/hosts")
             ssh.exec!("sudo service hostname start")
           end
         end
