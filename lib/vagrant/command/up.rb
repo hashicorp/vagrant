a0c7b96b7c52fed28e33eb9a4bf1c530d76038a2
Ubiratan Pires Alberton <u.alberton@gmail.com>
2011-05-16 12:07:17 -0400

    Pass provisioner list to up command

--- lib/vagrant/command/up.rb
+++ lib/vagrant/command/up.rb
@@ -2,7 +2,6 @@ module Vagrant
   module Command
     class UpCommand < NamedBase
       class_option :provision, :type => :boolean, :default => true, :desc => "Enable or disable provisioning"
-      class_option :provisioner, :type => :string, :desc => "Load only the specified provisioner"
       class_option :provisioners, :type => :array, :desc => "Load only the specified provisioners"
       register "up", "Creates the Vagrant environment"
 
@@ -10,9 +9,11 @@ module Vagrant
         target_vms.each do |vm|
           if vm.created?
             vm.env.ui.info I18n.t("vagrant.commands.up.vm_created")
-            vm.start("provision.enabled" => options[:provision])
+            vm.start("provision.enabled" => options[:provision],
+                     "provision.provisioners" => options[:provisioners])
           else
-            vm.up("provision.enabled" => options[:provision])
+            vm.up("provision.enabled" => options[:provision],
+                  "provision.provisioners" => options[:provisioners])
           end
         end
       end
