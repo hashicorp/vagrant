5cb02204d917c04daf52b57fca0962738fa54c37
Jeff Rafter <jeff@socialrange.org>
2011-07-22 08:38:03 -0700

    Rework indexes on chef solo provisioner to fix roles, data_bags

--- test/vagrant/provisioners/chef_solo_test.rb
+++ test/vagrant/provisioners/chef_solo_test.rb
@@ -52,9 +52,23 @@ class ChefSoloProvisionerTest < Test::Unit::TestCase
     should "expand host folders properly" do
       path = "foo"
       local_path = File.expand_path(path, @env.root_path)
-      remote_path = "#{@action.config.provisioning_path}/chef-solo-0"
+      remote_path = "#{@action.config.provisioning_path}/chef-solo-1"
       assert_equal [[:host, local_path, remote_path]], @action.expanded_folders([:host, path])
     end
+
+    should "expand multiple host folders properly" do
+      cookbooks_path = "cookbooks"
+      cookbooks_local_path = File.expand_path(cookbooks_path, @env.root_path)
+      cookbooks_remote_path = "#{@action.config.provisioning_path}/chef-solo-4"
+      roles_path = "roles"
+      roles_local_path = File.expand_path(roles_path, @env.root_path)
+      roles_remote_path = "#{@action.config.provisioning_path}/chef-solo-5"
+
+      assert_not_equal @action.expanded_folders([:host, cookbooks_path])[0].last, @action.expanded_folders([:host, roles_path])[0].last
+      assert_equal [[:host, cookbooks_local_path, cookbooks_remote_path]], @action.expanded_folders([:host, cookbooks_path])
+      assert_equal [[:host, roles_local_path, roles_remote_path]], @action.expanded_folders([:host, roles_path])
+    end
+
   end
 
   context "guest paths" do
