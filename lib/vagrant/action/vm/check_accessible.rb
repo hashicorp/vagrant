327a6f9d94b841c93d3bcf40305e5380c9b425d6
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-08-28 20:32:22 -0700

    Add a check for VM accessibility before every action

---
+++ lib/vagrant/action/vm/check_accessible.rb
@@ -0,0 +1,23 @@
+module Vagrant
+  class Action
+    module VM
+      class CheckAccessible
+        def initialize(app, env)
+          @app = app
+        end
+
+        def call(env)
+          if env["vm"] && env["vm"].created? && !env["vm"].vm.accessible?
+            # The VM we are attempting to manipulate is inaccessible. This
+            # is a very bad situation and can only be fixed by the user. It
+            # also prohibits us from actually doing anything with the virtual
+            # machine, so we raise an error.
+            raise Errors::VMInaccessible
+          end
+
+          @app.call(env)
+        end
+      end
+    end
+  end
+end
