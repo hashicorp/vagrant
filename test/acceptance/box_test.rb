cab1e4e49b9272d5d8df0f332f795bf4fe63bf7e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-11-03 21:55:53 -0700

    Make the file download the default for `vagrant box add`. Tests.

--- test/acceptance/box_test.rb
+++ test/acceptance/box_test.rb
@@ -19,4 +19,17 @@ class BoxTest < AcceptanceTest
     results = execute("vagrant", "box", "list")
     assert(results.stdout.read =~ /^foo$/, "Box should exist after it is added")
   end
+
+  should "give a helpful error message if the file doesn't exist" do
+    # Add a box which doesn't exist
+    results = execute("vagrant", "box", "add", "foo", "/tmp/nope/nope/nope/nonono.box")
+    assert(!results.success?, "Box add should fail.")
+    assert(results.stdout.read =~ /^The specified path to a file doesn't exist.$/,
+           "This should show an error message about the file not existing.")
+  end
+
+  should "add a box from an HTTP server" do
+    # TODO: Spin up an HTTP server to serve a file, add and test.
+    skip("Need to setup HTTP server functionality")
+  end
 end
