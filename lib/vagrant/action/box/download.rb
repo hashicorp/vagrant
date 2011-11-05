hashicorp/vagrant

commit cab1e4e49b9272d5d8df0f332f795bf4fe63bf7e

Make the file download the default for `vagrant box add`. Tests.

lib/vagrant/action/box/download.rb
@@ -26,13 +26,24 @@ module Vagrant
         end
 
         def instantiate_downloader
-          @env["download.classes"].each do |klass|
-            if klass.match?(@env["box"].uri)
+          # Assign to a temporary variable since this is easier to type out,
+          # since it is used so many times.
+          classes = @env["download.classes"]
+
+          # Find the class to use.
+          classes.each_index do |i|
+            klass = classes[i]
+
+            # Use the class if it matches the given URI or if this
+            # is the last class...
+            if classes.length == (i + 1) || klass.match?(@env["box"].uri)
               @env.ui.info I18n.t("vagrant.actions.box.download.with", :class => klass.to_s)
               @downloader = klass.new(@env)
             end
           end
 
+          # This line should never be reached, but we'll keep this here
+          # just in case for now.
           raise Errors::BoxDownloadUnknownType if !@downloader
 
           @downloader.prepare(@env["box"].uri)

test/acceptance/box_test.rb
@@ -19,4 +19,17 @@ class BoxTest < AcceptanceTest
     results = execute("vagrant", "box", "list")
     assert(results.stdout.read =~ /^foo$/, "Box should exist after it is added")
   end
+
+  should "give a helpful error message if the file doesn't exist" do
+    # Add a box which doesn't exist
+    results = execute("vagrant", "box", "add", "foo", "/tmp/nope/nope/nope/nonono.box")
+    assert(!results.success?, "Box add should fail.")
+    assert(results.stdout.read =~ /^The specified path to a file doesn't exist.$/,
+           "This should show an error message about the file not existing.")
+  end
+
+  should "add a box from an HTTP server" do
+    # TODO: Spin up an HTTP server to serve a file, add and test.
+    skip("Need to setup HTTP server functionality")
+  end
 end

commit f83c1b3c27c79cf473aae0c3a420d8c0fa1bbe3e

Fix regression introduced with cab1e4e

lib/vagrant/action/box/download.rb
@@ -39,6 +39,7 @@ module Vagrant
             if classes.length == (i + 1) || klass.match?(@env["box"].uri)
               @env.ui.info I18n.t("vagrant.actions.box.download.with", :class => klass.to_s)
               @downloader = klass.new(@env)
+              break
             end
           end
 

test/unit/vagrant/action/box/download_test.rb
@@ -39,9 +39,9 @@ class DownloadBoxActionTest < Test::Unit::TestCase
         assert @instance.instantiate_downloader
       end
 
-      should "error environment if URI is invalid for any downloaders" do
+      should "complain that the file doesn't exist if the URI is invalid for any downloaders" do
         @env["box"].uri = "foobar"
-        assert_raises(Vagrant::Errors::BoxDownloadUnknownType) {
+        assert_raises(Vagrant::Errors::DownloaderFileDoesntExist) {
           @instance.instantiate_downloader
         }
       end