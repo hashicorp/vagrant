aeae640c34b7f68870304fb2a6a163d852440b7a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-11-12 13:18:19 -0800

    Buildbot: Add a scheduler for the master branch to run

--- test/buildbot/buildbot_config/master/schedulers.py
+++ test/buildbot/buildbot_config/master/schedulers.py
@@ -3,5 +3,12 @@ This module contains the logic which returns the set of
 schedulers to use for the build master.
 """
 
+from buildbot.changes.filter import ChangeFilter
+from buildbot.schedulers.basic import SingleBranchScheduler
+
 def get_schedulers():
-    return []
+    full = SingleBranchScheduler(name="full",
+                                 change_filter=ChangeFilter(branch="master"),
+                                 treeStableTimer=60,
+                                 builderNames=["vagrant-master"])
+    return [full]
