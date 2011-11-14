9ee301c525600cfeb8b8ca3d59f75ff9b7823008
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-11-13 20:57:51 -0800

    Buildbot: Make the acceptance tests dependent on the unit tests passing

--- test/buildbot/buildbot_config/master/schedulers.py
+++ test/buildbot/buildbot_config/master/schedulers.py
@@ -4,13 +4,19 @@ schedulers to use for the build master.
 """
 
 from buildbot.changes.filter import ChangeFilter
-from buildbot.schedulers.basic import SingleBranchScheduler
+from buildbot.schedulers.basic import (
+    Dependent,
+    SingleBranchScheduler)
 
 def get_schedulers():
     # Run the unit tests for master
-    master_unit = SingleBranchScheduler(name="full",
+    master_unit = SingleBranchScheduler(name="master-unit",
                                  change_filter=ChangeFilter(branch="master"),
                                  treeStableTimer=60,
                                  builderNames=["vagrant-master-unit"])
 
-    return [master_unit]
+    master_acceptance = Dependent(name="master-acceptance",
+                                  upstream=master_unit,
+                                  builderNames=["vagrant-master-acceptance"])
+
+    return [master_unit, master_acceptance]
