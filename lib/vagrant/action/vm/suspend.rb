71c39126e5237b99c8625c3c48128872ea524afb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-12-10 14:15:18 -0800

    Fix suspend action to work again

--- lib/vagrant/action/vm/suspend.rb
+++ lib/vagrant/action/vm/suspend.rb
@@ -7,9 +7,9 @@ module Vagrant
         end
 
         def call(env)
-          if env["vm"].vm.running?
-            env.ui.info I18n.t("vagrant.actions.vm.suspend.suspending")
-            env["vm"].vm.save_state
+          if env[:vm].vm.running?
+            env[:ui].info I18n.t("vagrant.actions.vm.suspend.suspending")
+            env[:vm].vm.save_state
           end
 
           @app.call(env)
