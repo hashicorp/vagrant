703f455d5ae63657dae82629a9d5c8ebf2daf4ff
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-12-18 13:51:58 -0800

    `vagrant version` is gone, so remove the acceptance test for it

--- test/acceptance/version_test.rb
+++ test/acceptance/version_test.rb
@@ -3,11 +3,6 @@ require File.expand_path("../base", __FILE__)
 describe "vagrant version" do
   include_context "acceptance"
 
-  it "prints the version to stdout" do
-    result = execute("vagrant", "version")
-    result.stdout.should match_output(:version, config.vagrant_version)
-  end
-
   it "prints the version when called with '-v'" do
     result = execute("vagrant", "-v")
     result.stdout.should match_output(:version, config.vagrant_version)
