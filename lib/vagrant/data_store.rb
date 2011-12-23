40fdab3de813da53d700e05ae044d0250eebb69b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-12-22 21:18:59 -0800

    DataStore tests and slight modifications

--- lib/vagrant/data_store.rb
+++ lib/vagrant/data_store.rb
@@ -1,3 +1,5 @@
+require 'pathname'
+
 require 'log4r'
 
 module Vagrant
@@ -21,19 +23,16 @@ module Vagrant
       @logger    = Log4r::Logger.new("vagrant::datastore")
       @logger.info("Created: #{file_path}")
 
-      @file_path = file_path
-      return if !file_path
+      @file_path = Pathname.new(file_path)
 
-      raise Errors::DotfileIsDirectory if File.directory?(file_path)
+      if @file_path.exist?
+        raise Errors::DotfileIsDirectory if @file_path.directory?
 
-      if File.exist?(file_path)
-        File.open(file_path, "r") do |f|
-          begin
-            merge!(JSON.parse(f.read))
-          rescue JSON::ParserError
-            # Ignore if the data is invalid in the file.
-            @logger.error("Data store contained invalid JSON. Ignoring.")
-          end
+        begin
+          merge!(JSON.parse(@file_path.read))
+        rescue JSON::ParserError
+          # Ignore if the data is invalid in the file.
+          @logger.error("Data store contained invalid JSON. Ignoring.")
         end
       end
     end
@@ -48,10 +47,14 @@ module Vagrant
       if empty?
         # Delete the file since an empty data store is not useful
         @logger.info("Deleting data store since we're empty: #{@file_path}")
-        File.delete(@file_path) if File.file?(@file_path)
+        @file_path.delete if @file_path.exist?
       else
         @logger.info("Committing data to data store: #{@file_path}")
-        File.open(@file_path, "w") { |f| f.write(to_json) }
+
+        @file_path.open("w") do |f|
+          f.write(to_json)
+          f.fsync
+        end
       end
     end
 
