638881614a3ff589dad23a0d0536bff5b1dedb72
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-10 22:04:31 -0800

    Test that multiple provisioners can be used

--- test/acceptance/provisioning/basic_test.rb
+++ test/acceptance/provisioning/basic_test.rb
@@ -25,6 +25,23 @@ vf
     result_file.exist?.should_not be
   end
 
+  it "can provision with multiple provisioners" do
+    # Skeleton for multiple provisioners
+    environment.skeleton!("provisioner_multi")
+
+    # Setup the basic environment
+    require_box("default")
+    assert_execute("vagrant", "box", "add", "base", box_path("default"))
+
+    # Bring up the VM, only using the shell provisioner
+    assert_execute("vagrant", "up")
+
+    # Verify the file the shell provisioner made is there, but not the
+    # Chef one.
+    environment.workdir.join("shell").exist?.should be
+    environment.workdir.join("chef_solo").exist?.should be
+  end
+
   it "only uses the provisioners specified with `--provision-with`" do
     # Skeleton for multiple provisioners
     environment.skeleton!("provisioner_multi")
