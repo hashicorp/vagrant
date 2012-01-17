d675b7c83c2f4c0c52333ff7e5d2e53451ace90e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-17 14:43:32 -0800

    Replace `test?` call with proper `test` call

--- lib/vagrant/guest/debian.rb
+++ lib/vagrant/guest/debian.rb
@@ -50,7 +50,7 @@ module Vagrant
       end
 
       def change_host_name(name)
-        if !vm.channel.test?("hostname --fqdn | grep '^#{name}$' || hostname --short | grep '^#{name}$'")
+        if !vm.channel.test("hostname --fqdn | grep '^#{name}$' || hostname --short | grep '^#{name}$'")
           vm.channel.sudo("sed -r -i 's/^(127[.]0[.]1[.]1[[:space:]]+).*$/\\1#{name} #{name.split('.')[0]}/' /etc/hosts")
           vm.channel.sudo("sed -i 's/.*$/#{name.split('.')[0]}/' /etc/hostname")
           vm.channel.sudo("hostname -F /etc/hostname")
