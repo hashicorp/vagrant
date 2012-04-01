d712f94800e5218d380fe2af75be40f1675a32de
David Calavera <david.calavera@gmail.com>
2012-04-01 16:54:11 -0700

    fix #844: allow to specify nfs user id mappings and sync

--- lib/vagrant/config/nfs.rb
+++ lib/vagrant/config/nfs.rb
@@ -3,6 +3,8 @@ module Vagrant
     class NFSConfig < Base
       attr_accessor :map_uid
       attr_accessor :map_gid
+      attr_accessor :sync
+      attr_accessor :user_id_mapping
     end
   end
 end
