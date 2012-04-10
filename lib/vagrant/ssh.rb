59852e79deee94796ce21322bd5640fc909a8c09
Jeff Forcier <jeff@bitprophet.org>
2012-04-09 17:57:41 -0700

    Don't use IdentitiesOnly under 'ssh -p'

--- lib/vagrant/ssh.rb
+++ lib/vagrant/ssh.rb
@@ -79,7 +79,8 @@ module Vagrant
                          "-o", "StrictHostKeyChecking=no", "-o", "LogLevel=QUIET"]
 
       # Solaris/OpenSolaris/Illumos uses SunSSH which doesn't support the IdentitiesOnly option
-      command_options += ["-o", "IdentitiesOnly=yes"] if !Util::Platform.solaris?
+      # (Also don't use it in plain mode, it'll skip user agents.)
+      command_options += ["-o", "IdentitiesOnly=yes"] if !Util::Platform.solaris? || plain_mode
 
       command_options += ["-i", options[:private_key_path]] if !plain_mode
       command_options += ["-o", "ForwardAgent=yes"] if ssh_info[:forward_agent]
