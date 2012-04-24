f59054dae4d6aa12aac7a94bfde62f1c64da80f6
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-04-24 21:46:54 +0200

    Make it more future proof by not using first and last command also fixed a typo

--- contrib/bash/completion.sh
+++ contrib/bash/completion.sh
@@ -1,3 +1,3 @@
-# Autocompletion for Vagrant just put this line in your ~/.profile or lik this file into it like:
+# Autocompletion for Vagrant just put this line in your ~/.profile or link this file into it like:
 # source /path/to/vagrant/contrib/bash/completion.sh
-complete -W "$(echo `vagrant --help | awk '/box/,/up/ {print $1}'`;)" vagrant
+complete -W "$(echo `vagrant --help | awk '/^     /{print $1}'`;)" vagrant
