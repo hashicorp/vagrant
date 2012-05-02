9b20173dfbdae0e6bf522f05d81fb06f2b9c149a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-05-01 22:08:30 -0700

    Clean up some of the SSH key tests

--- test/unit/vagrant/ssh_test.rb
+++ test/unit/vagrant/ssh_test.rb
@@ -2,24 +2,30 @@ require File.expand_path("../../base", __FILE__)
 
 describe Vagrant::SSH do
   context "check_key_permissions" do
-    let(:key_path) { File.expand_path("../id_rsa", __FILE__) }
+    let(:key_path) do
+      # We create a tempfile to guarantee some level of uniqueness
+      # then explicitly close/unlink but save the path so we can re-use
+      temp = Tempfile.new("vagrant")
+      result = Pathname.new(temp.path)
+      temp.close
+      temp.unlink
+
+      result
+    end
+
     let(:ssh_instance) { Vagrant::SSH.new(double) }
 
     before(:each) do
-      File.open(key_path, 'w') do |file|
-        file.write("hello!")
+      key_path.open("w") do |f|
+        f.write("hello!")
       end
-      File.chmod(644, key_path)
-    end
 
-    after(:each) do
-      FileUtils.rm(key_path)
+      key_path.chmod(0644)
     end
 
     it "should not raise an exception if we set a keyfile permission correctly" do
       ssh_instance.check_key_permissions(key_path)
     end
-    
   end
 end
 
