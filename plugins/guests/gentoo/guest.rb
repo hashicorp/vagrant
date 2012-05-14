46d72bb53a1def4735a7b16789da20e062341073
Benedikt Böhm <bb@xnull.de>
2012-05-14 17:16:41 +0200

    fix typo in change_host_name

--- plugins/guests/gentoo/guest.rb
+++ plugins/guests/gentoo/guest.rb
@@ -39,7 +39,7 @@ module VagrantPlugins
       def change_host_name(name)
         if !vm.channel.test("sudo hostname --fqdn | grep '#{name}'")
           vm.channel.sudo("echo 'hostname=#{name.split('.')[0]}' > /etc/conf.d/hostname")
-          vm.channel.sudo("sed -i 's@^\\(127[.]0[.]1[.]1[[:space:]]\\+\\)@\\1#{name} #{name.split('.')[0]} @' /etc/hosts")
+          vm.channel.sudo("sed -i 's@^\\(127[.]0[.]0[.]1[[:space:]]\\+\\)@\\1#{name} #{name.split('.')[0]} @' /etc/hosts")
           vm.channel.sudo("hostname #{name.split('.')[0]}")
         end
       end
