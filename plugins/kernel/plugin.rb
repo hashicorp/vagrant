162227765f1c4bef28f4cbf3e1c5d9590bfb7060
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-05-23 16:04:41 -0700

    Change kernel plugin to use `activated` block

--- plugins/kernel/plugin.rb
+++ plugins/kernel/plugin.rb
@@ -2,12 +2,6 @@ require "vagrant"
 
 module VagrantPlugins
   module Kernel
-    autoload :SSHConfig,     File.expand_path("../config/ssh", __FILE__)
-    autoload :NFSConfig,     File.expand_path("../config/nfs", __FILE__)
-    autoload :PackageConfig, File.expand_path("../config/package", __FILE__)
-    autoload :VagrantConfig, File.expand_path("../config/vagrant", __FILE__)
-    autoload :VMConfig,      File.expand_path("../config/vm", __FILE__)
-
     # This is the "kernel" of Vagrant and contains the configuration classes
     # that make up the core of Vagrant.
     class Plugin < Vagrant.plugin("1")
@@ -16,6 +10,14 @@ module VagrantPlugins
       The kernel of Vagrant. This plugin contains required items for even
       basic functionality of Vagrant version 1.
       DESC
+
+      activated do
+        require File.expand_path("../config/ssh", __FILE__)
+        require File.expand_path("../config/nfs", __FILE__)
+        require File.expand_path("../config/package", __FILE__)
+        require File.expand_path("../config/vagrant", __FILE__)
+        require File.expand_path("../config/vm", __FILE__)
+      end
 
       # Core configuration keys provided by the kernel.
       config("ssh")     { SSHConfig }
