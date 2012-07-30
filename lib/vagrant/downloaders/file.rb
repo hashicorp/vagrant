e5ec8200aa1f0344b09a5601fcde1f9f8bf51ab0
Ben Burkert <ben@benburkert.com>
2012-07-30 16:44:41 -0700

    Match the http & file downloaders on the scheme.
    Identify a http url vs. a file url by the scheme used in the url. If
    it's http or https, use the http downloader. For file or nil, use the
    file downloader.

--- lib/vagrant/downloaders/file.rb
+++ lib/vagrant/downloaders/file.rb
@@ -5,17 +5,23 @@ module Vagrant
     # "Downloads" a file to a temporary file. Basically, this downloader
     # simply does a file copy.
     class File < Base
-      def self.match?(uri)
-        ::File.file?(::File.expand_path(uri))
+      def self.match?(url)
+        uri = URI.parse(url)
+
+        [nil, 'file'].include?(uri.scheme)
       end
 
       def prepare(source_url)
-        raise Errors::DownloaderFileDoesntExist if !::File.file?(::File.expand_path(source_url))
+        file_path = URI.parse(source_url).path
+
+        raise Errors::DownloaderFileDoesntExist if !::File.file?(::File.expand_path(file_path))
       end
 
       def download!(source_url, destination_file)
+        file_path = URI.parse(source_url).path
+
         @ui.info I18n.t("vagrant.downloaders.file.download")
-        FileUtils.cp(::File.expand_path(source_url), destination_file.path)
+        FileUtils.cp(::File.expand_path(file_path), destination_file.path)
       end
     end
   end
