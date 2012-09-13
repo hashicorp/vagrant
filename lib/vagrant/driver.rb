f268f62c823579751c61aedbe4ec7f45fe8f73eb
Julian Hille <julian.hille@netimpact-group.de>
2012-09-13 15:48:47 +0200

    Added driver for VirtualBox 4.2 to 1.0.x stable this is so far not very deep tested. but vagrant up and base commands to work.

--- lib/vagrant/driver.rb
+++ lib/vagrant/driver.rb
@@ -3,5 +3,6 @@ module Vagrant
     autoload :VirtualBox, 'vagrant/driver/virtualbox'
     autoload :VirtualBox_4_0, 'vagrant/driver/virtualbox_4_0'
     autoload :VirtualBox_4_1, 'vagrant/driver/virtualbox_4_1'
+    autoload :VirtualBox_4_2, 'vagrant/driver/virtualbox_4_2'
   end
 end
