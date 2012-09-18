7a4c8087c39cf3fc23396d7965d93c66907186c0
Steven Merrill <steven.merrill@gmail.com>
2012-09-18 15:59:10 -0400

    Add Fedora 17+ NFS support.

--- lib/vagrant/hosts/fedora.rb
+++ lib/vagrant/hosts/fedora.rb
@@ -23,7 +23,21 @@ module Vagrant
       def initialize(*args)
         super
 
+        release_file = Pathname.new("/etc/redhat-release")
+
         @nfs_server_binary = "/etc/init.d/nfs"
+
+        if release_file.exist?
+          release_file.open("r") do |f|
+            version_number = /Fedora release ([0-9]+)/.match(f.gets)[1].to_i
+            if version_number >= 17
+              # For now, "service nfs-server" will redirect properly to systemctl
+              # when "service nfs-server restart" is called.
+              @nfs_server_binary = "/usr/sbin/service nfs-server"
+            end
+          end
+        end
+
       end
     end
   end
