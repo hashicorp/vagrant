f7dfce539badec35459f79dd4d76daf371b92507
Steven Merrill <steven.merrill@gmail.com>
2012-09-18 17:15:45 -0400

    Also change Fedora 16 NFS handling.

--- lib/vagrant/hosts/fedora.rb
+++ lib/vagrant/hosts/fedora.rb
@@ -30,7 +30,7 @@ module Vagrant
         if release_file.exist?
           release_file.open("r") do |f|
             version_number = /Fedora release ([0-9]+)/.match(f.gets)[1].to_i
-            if version_number >= 17
+            if version_number >= 16
               # For now, "service nfs-server" will redirect properly to systemctl
               # when "service nfs-server restart" is called.
               @nfs_server_binary = "/usr/sbin/service nfs-server"
