3f68c834fdc991a674d3e8c1301b268d893dde48
patrick brisbin <pbrisbin@gmail.com>
2012-09-18 19:37:24 -0400

    Fix arch host class
    1. Detect os by /etc/os-release
    2. Bring nfs_cleanup from parent class
    3. Use systemd if available

--- lib/vagrant/hosts/arch.rb
+++ lib/vagrant/hosts/arch.rb
@@ -2,12 +2,21 @@ module Vagrant
   module Hosts
     class Arch < Linux
       def self.match?
-        File.exist?("/etc/rc.conf") && File.exist?("/etc/pacman.conf")
+        File.exist?("/etc/os-release") && File.read("/etc/os-release") =~ /arch linux/i
       end
 
       # Normal, mid-range precedence.
       def self.precedence
         5
+      end
+
+      # rewriting nfs? here since my system shows the filesystem as nfs
+      # or nfs4, not nfsd... is the Linux host broken?
+      def nfs?
+        retryable(:tries => 10, :on => TypeError) do
+          # Check procfs to see if NFS is a supported filesystem
+          system("cat /proc/filesystems | grep -q nfs 2>/dev/null")
+        end
       end
 
       def nfs_export(id, ip, folders)
@@ -19,17 +28,34 @@ module Vagrant
         @ui.info I18n.t("vagrant.hosts.arch.nfs_export.prepare")
         sleep 0.5
 
+        nfs_cleanup(id)
+
         output.split("\n").each do |line|
           # This should only ask for administrative permission once, even
           # though its executed in multiple subshells.
           system(%Q[sudo su root -c "echo '#{line}' >> /etc/exports"])
         end
 
-        # We run restart here instead of "update" just in case nfsd
-        # is not starting
-        system("sudo /etc/rc.d/rpcbind restart")
-        system("sudo /etc/rc.d/nfs-common restart")
-        system("sudo /etc/rc.d/nfs-server restart")
+        if systemd?
+          # call start to be nice. this will be a no-op if things are
+          # already running. then use exportfs to pick up the changes we
+          # just made.
+          system("sudo systemctl start nfsd.service rpc-idmapd.service rpc-mountd.service rpcbind.service")
+          system("sudo exportfs -r")
+        else
+          # the restarting of services when we might not need to can be
+          # considered evil, but this will be obviated by systemd soon
+          # enough anyway.
+          system("sudo /etc/rc.d/rpcbind restart")
+          system("sudo /etc/rc.d/nfs-common restart")
+          system("sudo /etc/rc.d/nfs-server restart")
+        end
+      end
+
+      private
+
+      def systemd?
+        system("which systemctl &>/dev/null")
       end
     end
   end
