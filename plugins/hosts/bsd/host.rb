96e65e7f19f1d27bfa65b28bea54381c8fa3f094
Yann Robert <yann.robert@anantaplex.fr>
2012-09-25 21:04:51 +0200

    Using sed to edit /etc/exports file

--- plugins/hosts/bsd/host.rb
+++ plugins/hosts/bsd/host.rb
@@ -49,10 +49,14 @@ module VagrantPlugins
         nfs_cleanup(id)
 
         # Output the rendered template into the exports
-        output.split("\n").each do |line|
-          line = line.gsub('"', '\"')
-          system(%Q[sudo su root -c "echo '#{line}' >> /etc/exports"])
-        end
+        # now we need to escape some chars to make sure sed will work
+        # we know we make use of double-quotes, parenthesis, and multiple lines. So we are going to handle those cases
+        output = output.gsub("\"", "\\\"")
+        output = output.gsub("(", "\\(")
+        output = output.gsub(")", "\\)")
+        output = output.gsub("\n", "\\\n")
+        sed_command = "sudo sed -e '$a#{output}' -ibak /etc/exports"
+        system(sed_command)
 
         # We run restart here instead of "update" just in case nfsd
         # is not starting
--- plugins/hosts/linux/host.rb
+++ plugins/hosts/linux/host.rb
@@ -45,12 +45,15 @@ module VagrantPlugins
 
         nfs_cleanup(id)
 
-        output.split("\n").each do |line|
-          # This should only ask for administrative permission once, even
-          # though its executed in multiple subshells.
-          system(%Q[sudo su root -c "echo '#{line}' >> /etc/exports"])
-        end
-
+        # now we need to escape some chars to make sure sed will work
+        # we know we make use of double-quotes, parenthesis, and multiple lines. So we are going to handle those cases
+        output = output.gsub("\"", "\\\"")
+        output = output.gsub("(", "\\(")
+        output = output.gsub(")", "\\)")
+        output = output.gsub("\n", "\\\n")
+        sed_command = "sudo sed -e '$a#{output}' -ibak /etc/exports"
+        system(sed_command)
+		
         # We run restart here instead of "update" just in case nfsd
         # is not starting
         system("sudo #{@nfs_server_binary} restart")
