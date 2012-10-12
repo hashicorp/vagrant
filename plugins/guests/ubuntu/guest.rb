fc0a0d04e54b6d714c9b836661af4108680d4b0b
LoreLLo <lorello@openweb.it>
2012-10-12 18:11:52 +0200

    fix change_host_name for Ubuntu Hardy

--- plugins/guests/ubuntu/guest.rb
+++ plugins/guests/ubuntu/guest.rb
@@ -29,7 +29,12 @@ module VagrantPlugins
           if !comm.test("sudo hostname | grep '#{name}'")
             comm.sudo("sed -i 's/.*$/#{name}/' /etc/hostname")
             comm.sudo("sed -i 's@^\\(127[.]0[.]1[.]1[[:space:]]\\+\\)@\\1#{name} #{name.split('.')[0]} @' /etc/hosts")
-            comm.sudo("service hostname start")
+            if comm.test("[ `lsb_release -c -s` = hardy ]")
+                # hostname.sh returns 1, so I grep for the right name in /etc/hostname just to have a 0 exitcode
+                comm.sudo("/etc/init.d/hostname.sh start; grep '#{name}' /etc/hostname")
+            else
+                comm.sudo("service hostname start")
+            end
             comm.sudo("hostname --fqdn > /etc/mailname")
           end
         end
