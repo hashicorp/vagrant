5cf737d4fc0ef2a88cfb64a717e59a155307494c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-11-03 13:47:57 -0700

    Remove the buildbot stuff, we'll move away from it.

--- test/buildbot/buildbot_config/master/__init__.py
+++
@@ -1,6 +0,0 @@
-import buildsteps
-from builders import get_builders
-from change_sources import get_change_sources
-from schedulers import get_schedulers
-from slaves import get_slaves_from_config
-from status import get_status
