9c9ecf999c3b2729c21e3835fa51c724225aa322
Zeh Rizzatti <zehrizzatti@gmail.com>
2012-12-19 14:51:58 -0300

    Halt Arch using systemctl poweroff

--- lib/vagrant/guest/arch.rb
+++ lib/vagrant/guest/arch.rb
@@ -43,6 +43,21 @@ module Vagrant
             end
           end
         end
+
+        def halt
+          vm.channel.sudo("systemctl poweroff")
+
+          # Wait until the VM's state is actually powered off. If this doesn't
+          # occur within a reasonable amount of time (15 seconds by default),
+          # then simply return and allow Vagrant to kill the machine.
+          count = 0
+          while vm.state != :poweroff
+            count += 1
+
+            return if count >= vm.config.linux.halt_timeout
+            sleep vm.config.linux.halt_check_interval
+          end
+        end
       end
 
       module SysVInit
