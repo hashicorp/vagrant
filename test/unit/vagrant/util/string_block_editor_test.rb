335503a6881f802c3d410e89d24d99b344095289
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-01-15 17:49:46 -0800

    StringBlockEditor to help plugins with owning parts of files

---
+++ test/unit/vagrant/util/string_block_editor_test.rb
@@ -0,0 +1,78 @@
+require File.expand_path("../../../base", __FILE__)
+
+require "vagrant/util/string_block_editor"
+
+describe Vagrant::Util::StringBlockEditor do
+  describe "#keys" do
+    it "should return all the keys" do
+      data = <<DATA
+# VAGRANT-BEGIN: foo
+value
+# VAGRANT-END: foo
+another
+# VAGRANT-BEGIN: bar
+content
+# VAGRANT-END: bar
+DATA
+
+      described_class.new(data).keys.should == ["foo", "bar"]
+    end
+  end
+
+  describe "#delete" do
+    it "should delete nothing if the key doesn't exist" do
+      data = "foo"
+
+      instance = described_class.new(data)
+      instance.delete("key")
+      instance.value.should == data
+    end
+
+    it "should delete the matching blocks if they exist" do
+      data = <<DATA
+# VAGRANT-BEGIN: foo
+value
+# VAGRANT-END: foo
+# VAGRANT-BEGIN: foo
+another
+# VAGRANT-END: foo
+another
+# VAGRANT-BEGIN: bar
+content
+# VAGRANT-END: bar
+DATA
+
+      new_data = <<DATA
+another
+# VAGRANT-BEGIN: bar
+content
+# VAGRANT-END: bar
+DATA
+
+      instance = described_class.new(data)
+      instance.delete("foo")
+      instance.value.should == new_data
+    end
+  end
+
+  describe "#insert" do
+    it "should insert the given key and value" do
+      data = <<DATA
+# VAGRANT-BEGIN: bar
+content
+# VAGRANT-END: bar
+DATA
+
+      new_data = <<DATA
+#{data}
+# VAGRANT-BEGIN: foo
+value
+# VAGRANT-END: foo
+DATA
+
+      instance = described_class.new(data)
+      instance.insert("foo", "value")
+      instance.value.should == new_data
+    end
+  end
+end
