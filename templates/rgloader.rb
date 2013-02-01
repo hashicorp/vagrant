c8ae1a265a14688a5b0dbc3d8928dea8c742a1cc
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-01-31 19:54:50 -0800

    Extra protection in the rgloader file

--- templates/rgloader.rb
+++ templates/rgloader.rb
@@ -4,4 +4,6 @@
 if ENV["VAGRANT_INSTALLER_EMBEDDED_DIR"]
   require File.expand_path(
     "rgloader/loader", ENV["VAGRANT_INSTALLER_EMBEDDED_DIR"])
+else
+  raise "Encoded files can't be read outside of the Vagrant installer."
 end
