2dc14af82580e34387d5a74b63841acbf509778e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-02-01 14:03:59 -0800

    `vagrant up` says what provider it will use

--- plugins/commands/up/command.rb
+++ plugins/commands/up/command.rb
@@ -30,6 +30,10 @@ module VagrantPlugins
         # Go over each VM and bring it up
         @logger.debug("'Up' each target VM...")
         with_target_vms(argv, :provider => options[:provider]) do |machine|
+          @env.ui.info(I18n.t(
+            "vagrant.commands.up.upping",
+            :name => machine.name,
+            :provider => machine.provider_name))
           machine.action(:up, options)
         end
 
