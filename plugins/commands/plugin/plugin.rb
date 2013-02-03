db2e27bab68f5f79c2ba212c8a173b6c485c8bea
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-02-02 17:12:46 -0800

    Initial plugin command plugin

---
+++ plugins/commands/plugin/plugin.rb
@@ -0,0 +1,18 @@
+require "vagrant"
+
+module VagrantPlugins
+  module CommandPlugin
+    class Plugin < Vagrant.plugin("2")
+      name "plugin command"
+      description <<-DESC
+      This command helps manage and install plugins within the
+      Vagrant environment.
+DESC
+
+      command("plugin") do
+        require File.expand_path("../command/root", __FILE__)
+        Command::Root
+      end
+    end
+  end
+end
