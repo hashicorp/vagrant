98f5e17f2354cb2865212d6d89caa6a5c141163c
Chris Liles <cliles@capario.com>
2013-03-06 16:12:28 -0800

    add fix for issue #1063, config.vm.host_name on suse guests

--- lib/vagrant/guest/suse.rb
+++ lib/vagrant/guest/suse.rb
@@ -4,6 +4,14 @@ module Vagrant
       def network_scripts_dir
         '/etc/sysconfig/network/'
       end
+      def change_host_name(name)
+        # Only do this if the hostname is not already set
+        if !vm.channel.test("sudo hostname | grep '#{name}'")
+          vm.channel.sudo("echo #{name} > /etc/HOSTNAME")
+          vm.channel.sudo("hostname #{name}")
+          vm.channel.sudo("sed -i 's@^\\(127[.]0[.]0[.]1[[:space:]]\\+\\)@\\1#{name} #{name.split('.')[0]} @' /etc/hosts")
+        end
+      end
     end
   end
 end
