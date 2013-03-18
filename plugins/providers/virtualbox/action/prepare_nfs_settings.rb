hashicorp/vagrant

commit 022c1daec00d273fb62f71950d18ec3454708352

Raise error if no hostonly network on VBox with NFS [GH-1437]

plugins/providers/virtualbox/action/prepare_nfs_settings.rb
@@ -12,6 +12,8 @@ module VagrantPlugins
 
           env[:nfs_host_ip]    = read_host_ip(env[:machine])
           env[:nfs_machine_ip] = read_machine_ip(env[:machine])
+
+          raise Errors::NFSNoHostonlyNetwork if !env[:nfs_machine_ip]
         end
 
         # Returns the IP address of the first host only network adapter

lib/vagrant/errors.rb
@@ -267,6 +267,10 @@ module Vagrant
       error_key(:nfs_no_host_ip)
     end
 
+    class NFSNoHostonlyNetwork < VagrantError
+      error_key(:nfs_no_hostonly_network)
+    end
+
     class NoEnvironmentError < VagrantError
       error_key(:no_env)
     end

templates/locales/en.yml
@@ -185,6 +185,10 @@ en:
       nfs_no_host_ip: |-
         No host IP was given to the Vagrant core NFS helper. This is
         an internal error that should be reported as a bug.
+      nfs_no_hostonly_network: |-
+        NFS requires a host-only network with a static IP to be created.
+        Please add a host-only network with a static IP to the machine
+        for NFS to work.
       no_env: |-
         A Vagrant environment is required to run this command. Run `vagrant init`
         to set one up in this directory, or change to a directory with a

commit c45d3ee728b973c31ad8efa224762ae11678cfd2

Fix wrong reference to Errors::NFSNoHostonlyNetwork introduced in 022c1da [GH-1437].

plugins/providers/virtualbox/action/prepare_nfs_settings.rb
@@ -13,7 +13,7 @@ module VagrantPlugins
           env[:nfs_host_ip]    = read_host_ip(env[:machine])
           env[:nfs_machine_ip] = read_machine_ip(env[:machine])
 
-          raise Errors::NFSNoHostonlyNetwork if !env[:nfs_machine_ip]
+          raise Vagrant::Errors::NFSNoHostonlyNetwork if !env[:nfs_machine_ip]
         end
 
         # Returns the IP address of the first host only network adapter