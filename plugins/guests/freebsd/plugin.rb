2c362d4d2854aa89b7598f2d6d22582ba9392199
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-04-04 11:56:42 -0700

    FreeBSD capabilities

--- plugins/guests/freebsd/plugin.rb
+++ plugins/guests/freebsd/plugin.rb
@@ -10,6 +10,26 @@ module VagrantPlugins
         require File.expand_path("../guest", __FILE__)
         Guest
       end
+
+      guest_capability("freebsd", "change_host_name") do
+        require_relative "cap/change_host_name"
+        Cap::ChangeHostName
+      end
+
+      guest_capability("freebsd", "configure_networks") do
+        require_relative "cap/configure_networks"
+        Cap::ConfigureNetworks
+      end
+
+      guest_capability("freebsd", "halt") do
+        require_relative "cap/halt"
+        Cap::Halt
+      end
+
+      guest_capability("freebsd", "mount_nfs_folder") do
+        require_relative "cap/mount_nfs_folder"
+        Cap::MountNFSFolder
+      end
     end
   end
 end
