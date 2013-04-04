439499fc117e28e4b0fff16a031b63e3b41e66e5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-04-04 12:25:10 -0700

    Solaris capabilities

--- plugins/guests/solaris/guest.rb
+++ plugins/guests/solaris/guest.rb
@@ -6,76 +6,8 @@ module VagrantPlugins
     #
     # Contributed by Blake Irvin <b.irvin@modcloth.com>
     class Guest < Vagrant.plugin("2", :guest)
-      # Here for whenever it may be used.
-      class SolarisError < Vagrant::Errors::VagrantError
-        error_namespace("vagrant.guest.solaris")
-      end
-
       def detect?(machine)
         machine.communicate.test("grep 'Solaris' /etc/release")
-      end
-
-      def configure_networks(networks)
-        networks.each do |network|
-          device = "#{vm.config.solaris.device}#{network[:interface]}"
-          su_cmd = vm.config.solaris.suexec_cmd
-          ifconfig_cmd = "#{su_cmd} /sbin/ifconfig #{device}"
-
-          vm.communicate.execute("#{ifconfig_cmd} plumb")
-
-          if network[:type].to_sym == :static
-            vm.communicate.execute("#{ifconfig_cmd} inet #{network[:ip]} netmask #{network[:netmask]}")
-            vm.communicate.execute("#{ifconfig_cmd} up")
-            vm.communicate.execute("#{su_cmd} sh -c \"echo '#{network[:ip]}' > /etc/hostname.#{device}\"")
-          elsif network[:type].to_sym == :dhcp
-            vm.communicate.execute("#{ifconfig_cmd} dhcp start")
-          end
-        end
-      end
-
-      def change_host_name(name)
-        su_cmd = vm.config.solaris.suexec_cmd
-
-        # Only do this if the hostname is not already set
-        if !vm.communicate.test("#{su_cmd} hostname | grep '#{name}'")
-          vm.communicate.execute("#{su_cmd} sh -c \"echo '#{name}' > /etc/nodename\"")
-          vm.communicate.execute("#{su_cmd} uname -S #{name}")
-        end
-      end
-
-      # There should be an exception raised if the line
-      #
-      #     vagrant::::profiles=Primary Administrator
-      #
-      # does not exist in /etc/user_attr. TODO
-      def halt
-        begin
-          vm.communicate.execute("#{vm.config.solaris.suexec_cmd} /usr/sbin/poweroff")
-        rescue IOError
-          # Ignore, this probably means connection closed because it
-          # shut down.
-        end
-      end
-
-      def mount_shared_folder(name, guestpath, options)
-        # These are just far easier to use than the full options syntax
-        owner = options[:owner]
-        group = options[:group]
-
-        # Create the shared folder
-        vm.communicate.execute("#{vm.config.solaris.suexec_cmd} mkdir -p #{guestpath}")
-
-        # We have to use this `id` command instead of `/usr/bin/id` since this
-        # one accepts the "-u" and "-g" flags.
-        id_cmd        = "/usr/xpg4/bin/id"
-
-        # Mount the folder with the proper owner/group
-        mount_options = "-o uid=`#{id_cmd} -u #{owner}`,gid=`#{id_cmd} -g #{group}`"
-        mount_options += ",#{options[:extra]}" if options[:extra]
-        vm.communicate.execute("#{vm.config.solaris.suexec_cmd} /sbin/mount -F vboxfs #{mount_options} #{name} #{guestpath}")
-
-        # chown the folder to the proper owner/group
-        vm.communicate.execute("#{vm.config.solaris.suexec_cmd} chown `#{id_cmd} -u #{owner}`:`#{id_cmd} -g #{group}` #{guestpath}")
       end
     end
   end
--- plugins/guests/suse/guest.rb
+++ plugins/guests/suse/guest.rb
@@ -1,27 +1,10 @@
 require "vagrant"
 
-require Vagrant.source_root.join("plugins/guests/redhat/guest")
-
 module VagrantPlugins
   module GuestSuse
-    class Guest < VagrantPlugins::GuestRedHat::Guest
+    class Guest < Vagrant.plugin("2", :guest)
       def detect?(machine)
         machine.communicate.test("cat /etc/SuSE-release")
-      end
-
-      def network_scripts_dir
-        '/etc/sysconfig/network/'
-      end
-
-      def change_host_name(name)
-        vm.communicate.tap do |comm|
-          # Only do this if the hostname is not already set
-          if !comm.test("sudo hostname | grep '#{name}'")
-            comm.sudo("echo '#{name}' > /etc/HOSTNAME")
-            comm.sudo("hostname #{name}")
-            comm.sudo("sed -i 's@^\\(127[.]0[.]0[.]1[[:space:]]\\+\\)@\\1#{name} #{name.split('.')[0]} @' /etc/hosts")
-          end
-        end
       end
     end
   end
