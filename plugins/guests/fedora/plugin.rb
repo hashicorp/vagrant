0e4e347308c7fa4be94392f1cee0def1092045dd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-04-04 11:49:26 -0700

    Fedora capabilities

--- plugins/guests/fedora/plugin.rb
+++ plugins/guests/fedora/plugin.rb
@@ -10,6 +10,21 @@ module VagrantPlugins
         require File.expand_path("../guest", __FILE__)
         Guest
       end
+
+      guest_capability("fedora", "change_host_name") do
+        require_relative "cap/change_host_name"
+        Cap::ChangeHostName
+      end
+
+      guest_capability("fedora", "configure_networks") do
+        require_relative "cap/configure_networks"
+        Cap::ConfigureNetworks
+      end
+
+      guest_capability("fedora", "network_scripts_dir") do
+        require_relative "cap/network_scripts_dir"
+        Cap::NetworkScriptsDir
+      end
     end
   end
 end
