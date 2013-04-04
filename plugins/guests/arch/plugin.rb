7f33081387d1cfe726414b926c6c204e67f0e08d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-04-04 11:39:58 -0700

    Convert arch to capabilities

--- plugins/guests/arch/plugin.rb
+++ plugins/guests/arch/plugin.rb
@@ -10,6 +10,16 @@ module VagrantPlugins
         require File.expand_path("../guest", __FILE__)
         Guest
       end
+
+      guest_capability("arch", "change_host_name") do
+        require_relative "cap/change_host_name"
+        Cap::ChangeHostName
+      end
+
+      guest_capability("arch", "configure_networks") do
+        require_relative "cap/configure_networks"
+        Cap::ConfigureNetworks
+      end
     end
   end
 end
