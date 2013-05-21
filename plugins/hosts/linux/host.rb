8153dad4a3ae1925ac665a8bc1b94a1262d9cc36
Alex Rodionov <p0deje@gmail.com>
2013-05-21 19:57:07 +0700

    properly parse exportfs output
    Hash[*exports] would return incorrect hash if host folder paths are the
    same (the last one is used), while this is a valid case in multi-VM
    environment

--- plugins/hosts/linux/host.rb
+++ plugins/hosts/linux/host.rb
@@ -35,24 +35,24 @@ module VagrantPlugins
       end
 
       def nfs_export(id, ip, folders)
-        exports = folders.map do |name, opts| 
+        exports = folders.map do |name, opts|
           # prepare NFS options
           options = %W(rw no_subtree_check all_squash fsid=#{opts[:uuid]})
           options << "anonuid=#{opts[:map_uid]}" if opts[:map_uid]
           options << "anongid=#{opts[:map_gid]}" if opts[:map_gid]
-          
+
           {
             :ip      => ip,
             :host    => opts[:hostpath],
             :options => options,
           }
-        end 
+        end
 
         @ui.info I18n.t("vagrant.hosts.linux.nfs_export")
         sleep 0.5
 
         exports.each do |export|
-          system(%Q[sudo exportfs -o #{export[:options].join(',')} #{export[:ip]}:#{export[:host]}])
+          system("sudo exportfs -o #{export[:options].join(',')} #{export[:ip]}:#{export[:host]}")
         end
       end
 
@@ -86,7 +86,7 @@ module VagrantPlugins
       def nfs_cleanup(ip, host)
         system("sudo exportfs -u #{ip}:#{host}")
       end
-      
+
       def current_exports
         exports = `sudo exportfs -v`
         # exportfs output should be properly parsed
@@ -100,17 +100,19 @@ module VagrantPlugins
         #   #=> "/short_path      \t192.168.122.153(rw,all_squash,no_subtree_check,fsid=68f4ad0105a6a490826e8d8861140d84)"
         #
         # and we need to properly handle both cases
-        exports = exports.split "\n"         # each new line is either folder or ip
-        exports.map! { |e| e.split("\t") }   # tab either prepends ip
-        exports.flatten!                     # make one-dimensional array
-        exports.select! { |e| !e.empty? }    # remove all empty strings (created by old entries)
-        exports.map! &:strip                 # make sure there are no leading spaces
-        
-        Hash[*exports].map do |folder, ip_and_opts|
+        exports = exports.split "\n"       # each new line is either folder or ip
+        exports.map! { |e| e.split("\t") } # tab prepends ip
+        exports.flatten!                   # make one-dimensional array
+        exports.select! { |e| !e.empty? }  # remove all empty strings created by splitting with long entries
+        exports.map! &:strip               # make sure there are no leading spaces
+
+        folders = exports.each_slice(2).map(&:first) # array of host paths
+        opts    = exports.each_slice(2).map(&:last)  # array of IP address and options
+        folders.each_with_index.map do |folder, i|
           # get IP and options froms string like
           # "192.168.122.167(rw,all_squash,no_subtree_check,fsid=7a99ad461317ed7c6514de3ce64db3e5)"
-          ip_and_opts = ip_and_opts.match(/^(\d+\.\d+\.\d+\.\d+)\((.+)\)$/)
-          
+          ip_and_opts = opts[i].match(/^(\d+\.\d+\.\d+\.\d+)\((.+)\)$/)
+
           {
             :host    => folder,
             :ip      => ip_and_opts[1],
