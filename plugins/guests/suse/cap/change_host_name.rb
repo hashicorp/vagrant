497ebb0f72c2a5dfe211a211348c4149830bff79
Deepak Seshadri <maildeepaks@gmail.com>
2013-05-31 19:31:43 +1000

    Fix setting persistent hostname on SLES guests.

--- plugins/guests/suse/cap/change_host_name.rb
+++ plugins/guests/suse/cap/change_host_name.rb
@@ -6,7 +6,7 @@ module VagrantPlugins
           machine.communicate.tap do |comm|
             # Only do this if the hostname is not already set
             if !comm.test("sudo hostname | grep '#{name}'")
-              comm.sudo("sed -i 's/\\(HOSTNAME=\\).*/\\1#{name}/' /etc/sysconfig/network")
+              comm.sudo("echo #{name} > /etc/HOSTNAME")
               comm.sudo("hostname #{name}")
               comm.sudo("sed -i 's@^\\(127[.]0[.]0[.]1[[:space:]]\\+\\)@\\1#{name} #{name.split('.')[0]} @' /etc/hosts")
             end
