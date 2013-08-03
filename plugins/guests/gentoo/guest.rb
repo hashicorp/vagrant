501073804366a2bf8140b5577f3e2bb59ff861df
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-08-03 19:55:17 +0200

    Improve Gentoo guest detection

--- plugins/guests/gentoo/guest.rb
+++ plugins/guests/gentoo/guest.rb
@@ -2,7 +2,7 @@ module VagrantPlugins
   module GuestGentoo
     class Guest < Vagrant.plugin("2", :guest)
       def detect?(machine)
-        machine.communicate.test("cat /etc/gentoo-release")
+        machine.communicate.test("grep Gentoo /etc/gentoo-release")
       end
     end
   end
