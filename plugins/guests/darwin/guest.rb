8b925e6c154ea1e3228662d8987173fc38fbc651
Timothy Sutton <tim@synthist.net>
2013-08-06 10:18:12 -0400

    Update Darwin guest description

--- plugins/guests/darwin/guest.rb
+++ plugins/guests/darwin/guest.rb
@@ -2,9 +2,10 @@ require 'vagrant/util/template_renderer'
 
 module VagrantPlugins
   module GuestDarwin
-    # A general Vagrant system implementation for "freebsd".
+    # A general Vagrant system implementation for OS X (ie. "Darwin").
     #
-    # Contributed by Kenneth Vestergaard <kvs@binarysolutions.dk>
+    # Contributed by: - Brian Johnson <b2jrock@gmail.com>
+    #                 - Tim Sutton <tim@synthist.net>
     class Guest < Vagrant.plugin("2", :guest)
       def detect?(machine)
         machine.communicate.test("uname -s | grep 'Darwin'")
