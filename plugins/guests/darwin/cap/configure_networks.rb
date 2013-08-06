3611ff39f4871386a42bbc69be4ba4dc6270c363
Brian Johnson <b2jrock@gmail.com>
2013-08-05 17:39:44 -0700

    Change mount_nfs_folder cap to be more like the linux version, adding a bunch of retires rather than a long sleep.
    Add DarwinNFSMountFailed error. This might move be more appropriate at
    the plugin level.
    
    Integrate some of tvsutton's work in configure_networks to get the
    implementation closer to working in both fusion and virtualbox.
    
    Add shell_expand_guest_path capability (also copied from linux)

--- plugins/guests/darwin/cap/configure_networks.rb
+++ plugins/guests/darwin/cap/configure_networks.rb
@@ -3,48 +3,55 @@ require "tempfile"
 require "vagrant/util/template_renderer"
 
 module VagrantPlugins
-  module GuestDarwin
-    module Cap
-      class ConfigureNetworks
-        include Vagrant::Util
+	module GuestDarwin
+		module Cap
+			class ConfigureNetworks
+				include Vagrant::Util
 
-        def self.configure_networks(machine, networks)
-          # Slightly different than other plugins, using the template to build commands 
-          # rather than templating the files.
+				def self.configure_networks(machine, networks)
+					# Slightly different than other plugins, using the template to build commands 
+					# rather than templating the files.
 
-          machine.communicate.sudo("networksetup -detectnewhardware")
-          devmap = {}
-          machine.communicate.sudo("networksetup -listnetworkserviceorder > /tmp/vagrant.interfaces")
-          tmpints = File.join(Dir.tmpdir, "#{machine.id}.interfaces")
-          machine.communicate.download("/tmp/vagrant.interfaces",tmpints)
-          ints = IO.read(tmpints)
-          ints.split(/\n\n/m).each do |i|
-              if i.match(/Hardware/) and not i.match(/Ethernet/).nil?
-                  # Ethernet, should be 2 lines, 
-                  # (3) Thunderbolt Ethernet
-                  # (Hardware Port: Thunderbolt Ethernet, Device: en1)
-                  devicearry = i.match(/Hardware Port: (.+), Device: en(.+)\)/)
-                  devmap[devicearry[2]] = devicearry[1]
-              end
-          end
-          networks.each do |network|
+					machine.communicate.sudo("networksetup -detectnewhardware")
+					machine.communicate.sudo("networksetup -listnetworkserviceorder > /tmp/vagrant.interfaces")
+					tmpints = File.join(Dir.tmpdir, File.basename("#{machine.id}.interfaces"))
+					machine.communicate.download("/tmp/vagrant.interfaces",tmpints)
 
+					devlist = []
+					ints = IO.read(tmpints)
+					ints.split(/\n\n/m).each do |i|
+						if i.match(/Hardware/) and not i.match(/Ethernet/).nil?
+							devmap = {}
+							# Ethernet, should be 2 lines, 
+							# (3) Thunderbolt Ethernet
+							# (Hardware Port: Thunderbolt Ethernet, Device: en1)
 
-              if network[:type].to_sym == :static
-                  # network seems 1 indexed - skip NAT interface (en0) also en1 because it seems to not *really* exist on virtualbox?
-                  intnum = network[:interface] + 1
-                  puts "Network - #{intnum}"
-              command = "networksetup -setmanual \"#{devmap[intnum.to_s]}\" #{network[:ip]} #{network[:netmask]} #{network[:gateway]}"
+							# multiline, should match "Thunderbolt Ethernet", "en1"
+							devicearry = i.match(/\([0-9]+\) (.+)\n.*Device: (.+)\)/m)
+							devmap[:interface] = devicearry[2]
+							devmap[:service] = devicearry[1]
+							devlist << devmap
+						end
+					end
+					puts devlist
 
-              elsif network[:type].to_sym == :dhcp
-                command = "networksetup -setdhcp \"#{devmap[options[:interface]]}\""
+					networks.each do |network|
+						intnum = network[:interface]
+						puts network[:interface]
+						puts network[:type]
+						if network[:type].to_sym == :static
+							# network seems 1 indexed - skip NAT interface (en0) also en1 because it seems to not *really* exist on virtualbox?
+							command = "networksetup -setmanual \"#{devlist[intnum+1][:service]}\" #{network[:ip]} #{network[:netmask]}"
 
-              end
+						elsif network[:type].to_sym == :dhcp
+							command = "networksetup -setdhcp \"#{devlist[intnum+1][:service]}\""
 
-            machine.communicate.sudo("#{command}")
-          end
-        end
-      end
-    end
-  end
+						end
+
+						machine.communicate.sudo(command)
+					end
+				end
+			end
+		end
+	end
 end
--- plugins/guests/darwin/cap/mount_nfs_folder.rb
+++ plugins/guests/darwin/cap/mount_nfs_folder.rb
@@ -1,16 +1,25 @@
+require "vagrant/util/retryable"
+
 module VagrantPlugins
-    module GuestDarwin
-        module Cap
-            class MountNFSFolder
-                def self.mount_nfs_folder(machine, ip, folders)
-                    puts "30 second nap...."
-                    sleep(30)
-                    folders.each do |name, opts|
-                        machine.communicate.sudo("if [ ! -d  #{opts[:guestpath]} ]; then  mkdir -p #{opts[:guestpath]};fi")
-                        machine.communicate.sudo("mount -t nfs '#{ip}:#{opts[:hostpath]}' '#{opts[:guestpath]}'")
-                    end
-                end
-            end
-        end
-    end
+	module GuestDarwin
+		module Cap
+			class MountNFSFolder
+				extend Vagrant::Util::Retryable
+				def self.mount_nfs_folder(machine, ip, folders)
+					folders.each do |name, opts|
+						# Expand the guest path so we can handle things like "~/vagrant"
+						expanded_guest_path = machine.guest.capability(
+							:shell_expand_guest_path, opts[:guestpath])
+
+							machine.communicate.sudo("if [ ! -d  #{expanded_guest_path} ]; then  mkdir -p #{expanded_guest_path};fi")
+
+							mount_command = "mount -t nfs '#{ip}:#{opts[:hostpath]}' '#{expanded_guest_path}'"
+							retryable(:on => Vagrant::Errors::DarwinNFSMountFailed, :tries => 10, :sleep => 5) do
+								machine.communicate.sudo(mount_command, :error_class => Vagrant::Errors::DarwinNFSMountFailed)
+							end
+					end
+				end
+			end
+		end
+	end
 end
