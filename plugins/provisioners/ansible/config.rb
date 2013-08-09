ac8b2c50472e6b3cad26be1f27f61968ee748440
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-09 11:04:35 -0700

    plugins/provisioners/ansible: use exist? check
    /cc @ches

--- plugins/provisioners/ansible/config.rb
+++ plugins/provisioners/ansible/config.rb
@@ -59,7 +59,7 @@ module VagrantPlugins
         # Validate the existence of the inventory_file, if specified
         if inventory_file
           expanded_path = Pathname.new(inventory_file).expand_path(machine.env.root_path)
-          unless expanded_path.file? || expanded_path.directory?
+          if !expanded_path.exist?
             errors << I18n.t("vagrant.provisioners.ansible.inventory_file_path_invalid",
                               :path => expanded_path)
           end
