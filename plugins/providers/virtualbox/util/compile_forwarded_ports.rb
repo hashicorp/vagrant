172a5dac5afeaef6b5de074ab68d7ef952493380
Michael Lex <mich.lex@gmail.com>
2013-08-16 12:10:03 +0200

    allow port-forwarding of the same port number but with different protocols [GH-1108]

--- plugins/providers/virtualbox/util/compile_forwarded_ports.rb
+++ plugins/providers/virtualbox/util/compile_forwarded_ports.rb
@@ -15,10 +15,11 @@ module VagrantPlugins
             if type == :forwarded_port
               guest_port = options[:guest]
               host_port  = options[:host]
+              protocol   = options[:protocol] || "tcp"
               options    = scoped_hash_override(options, :virtualbox)
               id         = options[:id]
 
-              mappings[host_port] =
+              mappings[host_port.to_s + protocol.to_s] =
                 Model::ForwardedPort.new(id, host_port, guest_port, options)
             end
           end
