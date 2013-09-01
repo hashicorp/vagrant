77dd626341734b5f0b2177ff9bdcac2fa0ebfead
Fabio Rehm <fgrehm@gmail.com>
2013-09-01 17:41:17 -0300

    guest: Remove dead code

--- plugins/guests/ubuntu/guest.rb
+++ plugins/guests/ubuntu/guest.rb
@@ -8,25 +8,6 @@ module VagrantPlugins
       def detect?(machine)
         machine.communicate.test("cat /etc/issue | grep 'Ubuntu'")
       end
-
-      def mount_shared_folder(name, guestpath, options)
-        # Mount it like normal
-        super
-
-        # Emit an upstart event if upstart is available
-        vm.communicate.sudo("[ -x /sbin/initctl ] && /sbin/initctl emit vagrant-mounted MOUNTPOINT=#{guestpath}")
-      end
-
-      def mount_nfs(ip, folders)
-        # Mount it like normal
-        super
-
-        # Emit an upstart events if upstart is available
-        folders.each do |name, opts|
-          real_guestpath = expanded_guest_path(opts[:guestpath])
-          vm.communicate.sudo("[ -x /sbin/initctl ] && /sbin/initctl emit vagrant-mounted MOUNTPOINT=#{real_guestpath}")
-        end
-      end
     end
   end
 end
