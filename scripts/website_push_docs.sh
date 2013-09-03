edc6d716825f46c861835f955bd0f6a09afe9283
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-09-03 11:37:54 -0700

    scripts: add website_push_docs

---
+++ scripts/website_push_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+# Change into that directory
+cd $DIR
+
+# Push the subtree (force)
+git push heroku-docs `git subtree split --prefix website/docs master`:master --force
