1443d634c7b6ee46b777944da84f58dce787e7e5
Fabio Rehm <fgrehm@gmail.com>
2013-10-04 21:29:52 -0300

    core: Write box information after download

--- lib/vagrant/action/builtin/write_box_info.rb
+++ lib/vagrant/action/builtin/write_box_info.rb
@@ -11,10 +11,13 @@ module Vagrant
         end
 
         def call(env)
-          box_url   = env[:box_url]
-          box_added = env[:box_added]
+          box_url        = env[:box_url]
+          box_added      = env[:box_added]
+          box_state_file = env[:box_state_file]
 
-          # TODO: Persist box_url / provider / datetime
+          # Mark that we downloaded the box
+          @logger.info("Adding the box to the state file...")
+          box_state_file.add_box(box_added, box_url)
 
           @app.call(env)
         end
