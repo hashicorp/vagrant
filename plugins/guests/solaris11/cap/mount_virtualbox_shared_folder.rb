ebb85b57fdaf5885e411819442f82e36ab209c78
Evan Borgstrom <evan@fatbox.ca>
2013-10-17 22:40:21 -0400

    Allow owner & group to be supplied as an Integer and skip lookup

--- plugins/guests/solaris11/cap/mount_virtualbox_shared_folder.rb
+++ plugins/guests/solaris11/cap/mount_virtualbox_shared_folder.rb
@@ -12,22 +12,32 @@ module VagrantPlugins
           group = options[:group]
 
           # Create the shared folder
-          machine.communicate.execute("#{machine.config.solaris11.suexec_cmd} mkdir -p #{guestpath}")
+          machine.communicate.execute("#{machine.config.solaris.suexec_cmd} mkdir -p #{guestpath}")
 
-          # We have to use this `id` command instead of `/usr/bin/id` since this
-          # one accepts the "-u" and "-g" flags.
-          id_cmd        = "/usr/xpg4/bin/id"
+          if owner.is_a? Integer
+            mount_uid = owner
+          else
+            # We have to use this `id` command instead of `/usr/bin/id` since this
+            # one accepts the "-u" and "-g" flags.
+            mount_uid = "`/usr/xpg4/bin/id -u #{owner}`"
+          end
+
+          if group.is_a? Integer
+            mount_gid = group
+          else
+            mount_gid = "`/usr/xpg4/bin/id -g #{group}`"
+          end
 
           # Mount the folder with the proper owner/group
-          mount_options = "-o uid=`#{id_cmd} -u #{owner}`,gid=`#{id_cmd} -g #{group}`"
+          mount_options = "-o uid=#{mount_uid},gid=#{mount_gid}"
           if options[:mount_options]
             mount_options += ",#{options[:mount_options].join(",")}"
           end
 
-          machine.communicate.execute("#{machine.config.solaris11.suexec_cmd} /sbin/mount -F vboxfs #{mount_options} #{name} #{guestpath}")
+          machine.communicate.execute("#{machine.config.solaris.suexec_cmd} /sbin/mount -F vboxfs #{mount_options} #{name} #{guestpath}")
 
           # chown the folder to the proper owner/group
-          machine.communicate.execute("#{machine.config.solaris11.suexec_cmd} chown `#{id_cmd} -u #{owner}`:`#{id_cmd} -g #{group}` #{guestpath}")
+          machine.communicate.execute("#{machine.config.solaris.suexec_cmd} chown #{mount_uid}:#{mount_gid} #{guestpath}")
         end
       end
     end
