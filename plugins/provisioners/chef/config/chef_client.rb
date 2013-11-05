54cf318f842cf84268922046a74cd9214b2295af
Tom Duffield <tom@opscode.com>
2013-11-05 09:00:46 -0600

    added support for the 11.8 feature of running chef-client locally.

--- plugins/provisioners/chef/config/chef_client.rb
+++ plugins/provisioners/chef/config/chef_client.rb
@@ -16,6 +16,15 @@ module VagrantPlugins
         attr_accessor :validation_key_path
         attr_accessor :validation_client_name
 
+        attr_accessor :local_mode
+        attr_accessor :cookbooks_path
+        attr_accessor :roles_path
+        attr_accessor :data_bags_path
+        attr_accessor :environments_path
+        attr_accessor :nodes_path
+        attr_accessor :clients_path
+
+
         def initialize
           super
 
@@ -28,6 +37,15 @@ module VagrantPlugins
           @environment                        = UNSET_VALUE
           @validation_key_path                = UNSET_VALUE
           @validation_client_name             = UNSET_VALUE
+
+
+          @local_mode                         = UNSET_VALUE
+          @cookbooks_path                     = UNSET_VALUE
+          @roles_path                         = UNSET_VALUE
+          @data_bags_path                     = UNSET_VALUE
+          @environments_path                  = UNSET_VALUE
+          @nodes_path                         = UNSET_VALUE
+          @clients_path                       = UNSET_VALUE
         end
 
         def finalize!
@@ -42,6 +60,32 @@ module VagrantPlugins
           @environment = nil if @environment == UNSET_VALUE
           @validation_client_name = "chef-validator" if @validation_client_name == UNSET_VALUE
           @validation_key_path = nil if @validation_key_path == UNSET_VALUE
+
+          @local_mode = false if @local_mode == UNSET_VALUE
+
+          #
+          # Taken from chef_solo.rb
+          #
+          if @cookbooks_path == UNSET_VALUE
+            @cookbooks_path = []
+            @cookbooks_path << [:host, "cookbooks"] if !@recipe_url
+            @cookbooks_path << [:vm, "cookbooks"]
+          end
+
+          @data_bags_path    = [] if @data_bags_path == UNSET_VALUE
+          @roles_path        = [] if @roles_path == UNSET_VALUE
+          @environments_path = [] if @environments_path == UNSET_VALUE
+          @environments_path = [@environments_path].flatten
+          @nodes_path        = [] if @nodes_path == UNSET_VALUE
+          @clients_path      = [] if @clients_path == UNSET_VALUE
+
+          # Make sure the path is an array.
+          @cookbooks_path    = prepare_folders_config(@cookbooks_path)
+          @data_bags_path    = prepare_folders_config(@data_bags_path)
+          @roles_path        = prepare_folders_config(@roles_path)
+          @environments_path = prepare_folders_config(@environments_path)
+          @nodes_path        = prepare_folders_config(@nodes_path)
+          @clients_path      = prepare_folders_config(@clients_path)
         end
 
         def validate(machine)
@@ -59,6 +103,23 @@ module VagrantPlugins
           end
 
           { "chef client provisioner" => errors }
+        end
+
+        protected
+
+        # This takes any of the configurations that take a path or
+        # array of paths and turns it into the proper format.
+        #
+        # @return [Array]
+        def prepare_folders_config(config)
+          # Make sure the path is an array
+          config = [config] if !config.is_a?(Array) || config.first.is_a?(Symbol)
+
+          # Make sure all the paths are in the proper format
+          config.map do |path|
+            path = [:host, path] if !path.is_a?(Array)
+            path
+          end
         end
       end
     end
--- plugins/provisioners/chef/provisioner/chef_client.rb
+++ plugins/provisioners/chef/provisioner/chef_client.rb
@@ -9,17 +9,60 @@ module VagrantPlugins
       # This class implements provisioning via chef-client, allowing provisioning
       # with a chef server.
       class ChefClient < Base
+
+        attr_reader :cookbook_folders
+        attr_reader :role_folders
+        attr_reader :data_bags_folders
+        attr_reader :environments_folders
+        attr_reader :nodes_folders
+        attr_reader :clients_folders
+
         def configure(root_config)
-          raise ChefError, :server_validation_key_required if @config.validation_key_path.nil?
-          raise ChefError, :server_validation_key_doesnt_exist if !File.file?(validation_key_path)
-          raise ChefError, :server_url_required if @config.chef_server_url.nil?
+          if @config.local_mode 
+            @cookbook_folders  = expanded_folders(@config.cookbooks_path, "cookbooks")
+            @role_folders      = expanded_folders(@config.roles_path, "roles")
+            @data_bags_folders = expanded_folders(@config.data_bags_path, "data_bags")
+            @environments_folders = expanded_folders(@config.environments_path, "environments")
+            @nodes_folders      = expanded_folders(@config.nodes_path, "nodes")
+            @clients_folders   = expanded_folders(@config.clients_path, "clients")
+
+            share_folders(root_config, "csc", @cookbook_folders)
+            share_folders(root_config, "csr", @role_folders)
+            share_folders(root_config, "csdb", @data_bags_folders)
+            share_folders(root_config, "cse", @environments_folders)
+            share_folders(root_config, "csn", @nodes_folders)
+            share_folders(root_config, "cscl", @clients_folders)
+          else 
+            raise ChefError, :server_validation_key_required if @config.validation_key_path.nil?
+            raise ChefError, :server_validation_key_doesnt_exist if !File.file?(validation_key_path)
+            raise ChefError, :server_url_required if @config.chef_server_url.nil?
+          end
         end
 
         def provision
+          # TODO: do we need to verify chef-zero and/or knife-essentials?
           verify_binary(chef_binary_path("chef-client"))
           chown_provisioning_folder
-          create_client_key_folder
-          upload_validation_key
+
+          if @config.local_mode
+            # Verify that the proper shared folders exist.
+            check = []
+            [@cookbook_folders, @role_folders, @data_bags_folders, @environments_folders, 
+              @nodes_folders, @clients_folders].each do |folders|
+              folders.each do |type, local_path, remote_path|
+                # We only care about checking folders that have a local path, meaning
+                # they were shared from the local machine, rather than assumed to
+                # exist on the VM.
+                check << remote_path if local_path
+              end
+            end
+
+            verify_shared_folders(check)
+          else
+            create_client_key_folder
+            upload_validation_key
+          end
+
           upload_encrypted_data_bag_secret if @config.encrypted_data_bag_secret_key_path
           setup_json
           setup_server_config
@@ -50,15 +93,35 @@ module VagrantPlugins
         end
 
         def setup_server_config
-          setup_config("provisioners/chef_client/client", "client.rb", {
-            :node_name => @config.node_name,
-            :chef_server_url => @config.chef_server_url,
-            :validation_client_name => @config.validation_client_name,
-            :validation_key => guest_validation_key_path,
-            :client_key => @config.client_key_path,
-            :environment => @config.environment,
-            :encrypted_data_bag_secret => @config.encrypted_data_bag_secret
-          })
+          if @config.local_mode
+            cookbooks_path = guest_paths(@cookbook_folders)
+            roles_path = guest_paths(@role_folders).first
+            data_bags_path = guest_paths(@data_bags_folders).first
+            environments_path = guest_paths(@environments_folders).first
+            clients_path = guest_paths(@clients_folders).first
+            nodes_path = guest_paths(@nodes_folders).first
+
+            setup_config("provisioners/chef_client/client", "client.rb", {
+              :node_name => @config.node_name,
+              :cookbook_path => cookbooks_path,
+              :node_path => nodes_path,
+              :client_path => clients_path,
+              :role_path => roles_path,
+              :data_bag_path => data_bags_path,
+              :environment => @config.environment,
+              :encrypted_data_bag_secret => @config.encrypted_data_bag_secret
+            })
+          else
+            setup_config("provisioners/chef_client/client", "client.rb", {
+              :node_name => @config.node_name,
+              :chef_server_url => @config.chef_server_url,
+              :validation_client_name => @config.validation_client_name,
+              :validation_key => guest_validation_key_path,
+              :client_key => @config.client_key_path,
+              :environment => @config.environment,
+              :encrypted_data_bag_secret => @config.encrypted_data_bag_secret
+            })
+          end
         end
 
         def run_chef_client
@@ -68,7 +131,15 @@ module VagrantPlugins
 
           command_env = @config.binary_env ? "#{@config.binary_env} " : ""
           command_args = @config.arguments ? " #{@config.arguments}" : ""
-          command = "#{command_env}#{chef_binary_path("chef-client")} -c #{@config.provisioning_path}/client.rb -j #{@config.provisioning_path}/dna.json #{command_args}"
+
+          command_opts = [
+            "-c #{@config.provisioning_path}/client.rb",
+            "-j #{@config.provisioning_path}/dna.json"
+          ]
+
+          command_opts << "-z" if @config.local_mode
+
+          command = "#{command_env}#{chef_binary_path("chef-client")} #{command_opts.join(" ")} #{command_args}"
 
           @config.attempts.times do |attempt|
             if attempt == 0
