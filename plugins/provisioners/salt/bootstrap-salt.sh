55f0ae8bbfaffd737460f30071e9dccf7017b80f
Daniel Miller <daniel@sixfeetup.com>
2013-11-21 10:28:26 -0500

    Make salt bootstrap work on systems without python

--- plugins/provisioners/salt/bootstrap-salt.sh
+++ plugins/provisioners/salt/bootstrap-salt.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -
 
 # We just download the bootstrap script by default and execute that.
-python \
-    -c 'import urllib; print urllib.urlopen("http://bootstrap.saltstack.org").read()' \
-    | sh -s -- "$@"
+if [ -x /usr/bin/fetch ]; then
+    fetch -o - http://bootstrap.saltstack.org | sh -s -- "$@"
+elif [ -x /usr/bin/curl ]; then
+    curl -L http://bootstrap.saltstack.org | sh -s -- "$@"
+else
+    python \
+        -c 'import urllib; print urllib.urlopen("http://bootstrap.saltstack.org").read()' \
+        | sh -s -- "$@"
+fi
