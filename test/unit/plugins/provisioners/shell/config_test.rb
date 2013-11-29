hashicorp/vagrant

commit 57d477514067a37665386968ef0a37abf86e3380

provisioners/shell: allow array args [GH-1949]

plugins/provisioners/shell/config.rb
@@ -56,8 +56,8 @@ module VagrantPlugins
         end
 
         # If there are args and its not a string, that is a problem
-        if args && !args.is_a?(String)
-          errors << I18n.t("vagrant.provisioners.shell.args_not_string")
+        if args && (!args.is_a?(String) || !args.is_a?(Array))
+          errors << I18n.t("vagrant.provisioners.shell.args_bad_type")
         end
 
         { "shell provisioner" => errors }

plugins/provisioners/shell/provisioner.rb
@@ -7,11 +7,14 @@ module VagrantPlugins
   module Shell
     class Provisioner < Vagrant.plugin("2", :provisioner)
       def provision
-        case config.args
-        when String then args = " #{config.args}"
-        when Array  then args = " #{config.args.map{|arg| quote_and_escape(arg)}.join(" ")}"
-        else args = ""
+        args = ""
+        if args.is_a?(String)
+          args = " #{config.args}"
+        elsif args.is_a?(Array)
+          args = config.args.map { |a| quote_and_escape(a) }
+          args = " #{args.join(" ")}"
         end
+
         command = "chmod +x #{config.upload_path} && #{config.upload_path}#{args}"
 
         with_script_file do |path|
@@ -53,7 +56,7 @@ module VagrantPlugins
 
       protected
 
-      # Quote and escape strings for shell execution, thanks to @capistrano
+      # Quote and escape strings for shell execution, thanks to Capistrano.
       def quote_and_escape(text, quote = '"')
         "#{quote}#{text.gsub(/#{quote}/) { |m| "#{m}\\#{m}#{m}" }}#{quote}"
       end

templates/locales/en.yml
@@ -1266,7 +1266,7 @@ en:
         running_puppetd: "Running Puppet agent..."
 
       shell:
-        args_not_string: "Shell provisioner `args` must be a string."
+        args_bad_type: "Shell provisioner `args` must be a string or array."
         no_path_or_inline: "One of `path` or `inline` must be set."
         path_and_inline_set: "Only one of `path` or `inline` may be set."
         path_invalid: "`path` for shell provisioner does not exist on the host system: %{path}"

commit fef60242b06191f721e10c6df9f74b4259065656

provisioners/shell: fix validation for args [GH-1949]
The logic change in 57d477514067a37665386968ef0a37abf86e3380 introduced
a bug where neither strings nor arrays provided as `args` for shell
provisioners would pass validation.

This fixes that problem along with a few extras:

- split out arg validation into a private method
- update comment describing valid args
- add a few unit tests around config validation

plugins/provisioners/shell/config.rb
@@ -55,12 +55,18 @@ module VagrantPlugins
           errors << I18n.t("vagrant.provisioners.shell.upload_path_not_set")
         end
 
-        # If there are args and its not a string, that is a problem
-        if args && (!args.is_a?(String) || !args.is_a?(Array))
+        unless args_valid?
           errors << I18n.t("vagrant.provisioners.shell.args_bad_type")
         end
 
         { "shell provisioner" => errors }
+      end
+
+      # Args are optional, but if they're provided we only support them as a
+      # string or as an array.
+      def args_valid?
+        return true unless args
+        args.is_a?(String) || args.is_a?(Array)
       end
 
       def remote?

test/unit/plugins/provisioners/shell/config_test.rb
@@ -1,0 +1,57 @@
+require File.expand_path("../../../../base", __FILE__)
+
+describe "VagrantPlugins::Shell::Config" do
+  let(:described_class) do
+    VagrantPlugins::Shell::Plugin.components.configs[:provisioner][:shell]
+  end
+
+  let(:machine)          { double('machine', env: Vagrant::Environment.new) }
+  let(:file_that_exists) { File.expand_path(__FILE__)                       }
+
+  subject { described_class.new }
+
+  describe "validate" do
+    it "passes with no args" do
+      subject.path = file_that_exists
+      subject.finalize!
+
+      result = subject.validate(machine)
+
+      result["shell provisioner"].should == []
+    end
+
+    it "passes with string args" do
+      subject.path = file_that_exists
+      subject.args = "a string"
+      subject.finalize!
+
+      result = subject.validate(machine)
+
+      result["shell provisioner"].should == []
+    end
+
+    it "passes with array args" do
+      subject.path = file_that_exists
+      subject.args = ["an", "array"]
+      subject.finalize!
+
+      result = subject.validate(machine)
+
+      result["shell provisioner"].should == []
+    end
+
+    it "returns an error if args is neither a string nor an array" do
+      neither_array_nor_string = Object.new
+
+      subject.path = file_that_exists
+      subject.args = neither_array_nor_string
+      subject.finalize!
+
+      result = subject.validate(machine)
+
+      result["shell provisioner"].should == [
+        I18n.t("vagrant.provisioners.shell.args_bad_type")
+      ]
+    end
+  end
+end