b65462faf19e1ea2ba5a647b0b4196a2562a6844
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-06 13:21:07 -0800

    providers/virtualbox: resume should wait for boot

--- plugins/providers/virtualbox/action.rb
+++ plugins/providers/virtualbox/action.rb
@@ -209,6 +209,7 @@ module VagrantPlugins
               b2.use PrepareForwardedPortCollisionParams
               b2.use HandleForwardedPortCollisions
               b2.use Resume
+              b2.use WaitForCommunicator, [:restoring, :running]
             else
               b2.use MessageNotCreated
             end
