deb6ba07b2ca9e0e539c2582ddf1bc30e965a793
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-06 15:29:38 -0800

    scripts: bintray upload

---
+++ scripts/bintray_upload.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+# Change into that dir because we expect that
+cd $DIR
+
+# Get the version from the command line
+VERSION=$1
+if [ -z $VERSION ]; then
+    echo "Please specify a version."
+    exit 1
+fi
+
+# Make sure we have a bintray API key
+if [ -z $BINTRAY_API_KEY ]; then
+    echo "Please set your bintray API key in the BINTRAY_API_KEY env var."
+    exit 1
+fi
+
+for ARCHIVE in ./dist/*; do
+    ARCHIVE_NAME=$(basename ${ARCHIVE})
+
+    echo Uploading: $ARCHIVE_NAME
+    curl \
+        -T ${ARCHIVE} \
+        -umitchellh:${BINTRAY_API_KEY} \
+        "https://api.bintray.com/content/mitchellh/vagrant/vagrant/${VERSION}/${ARCHIVE_NAME}"
+done
