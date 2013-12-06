47b99d9bb84552800cad288c9e7abefe4820afa5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-05 21:44:19 -0800

    test/acceptance: don't have the context show up as a component

--- test/acceptance/shared/context_virtualbox.rb
+++ test/acceptance/shared/context_virtualbox.rb
@@ -1,4 +1,4 @@
-shared_context "provider/virtualbox" do
+shared_context "provider-context/virtualbox" do
   let(:extra_env) {{
     "VBOX_USER_HOME" => "{{homedir}}",
   }}
--- vagrant-spec.config.example.rb
+++ vagrant-spec.config.example.rb
@@ -2,6 +2,6 @@ require_relative "test/acceptance/base"
 
 Vagrant::Spec::Acceptance.configure do |c|
   c.provider "virtualbox",
-    box_basic: "<PATH TO MINIMAL BOX>",
+    box: "<PATH TO MINIMAL BOX>",
     contexts: ["provider/virtualbox"]
 end
