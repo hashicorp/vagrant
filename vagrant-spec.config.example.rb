ab377560edebc64820e3e7ab2a0fcc3bd3e8e7ae
Mike Fiedler <miketheman@gmail.com>
2013-12-11 01:27:35 -0500

    fix path to acceptance test contexts in example

--- vagrant-spec.config.example.rb
+++ vagrant-spec.config.example.rb
@@ -3,5 +3,5 @@ require_relative "test/acceptance/base"
 Vagrant::Spec::Acceptance.configure do |c|
   c.provider "virtualbox",
     box: "<PATH TO MINIMAL BOX>",
-    contexts: ["provider/virtualbox"]
+    contexts: ["provider-context/virtualbox"]
 end
