5a842fc609cd3e3e142ee7e161030e17a4f19ab9
Fabio Rehm <fgrehm@gmail.com>
2013-12-13 22:28:51 -0200

    provisioners/docker: Check if epel-release is installed on RedHat-ian guests before attempting installation [GH-2649]

--- plugins/provisioners/docker/cap/redhat/docker_install.rb
+++ plugins/provisioners/docker/cap/redhat/docker_install.rb
@@ -5,7 +5,9 @@ module VagrantPlugins
         module DockerInstall
           def self.docker_install(machine, version)
             machine.communicate.tap do |comm|
-              comm.sudo("rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm")
+              if ! comm.test("rpm -qa | grep epel-release")
+                comm.sudo("rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm")
+              end
               comm.sudo("yum -y upgrade")
               comm.sudo("yum -y install docker-io")
             end
