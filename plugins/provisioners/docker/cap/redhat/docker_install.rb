46ba43e6ac637a9eb36a318743093c62eb18c3bd
Fabio Rehm <fgrehm@gmail.com>
2013-12-13 22:56:13 -0200

    provisioners/docker: Display warning in case a version get specified on the Vagrantfile and the guest is a RedHat-ian guest [GH-2649]

--- plugins/provisioners/docker/cap/redhat/docker_install.rb
+++ plugins/provisioners/docker/cap/redhat/docker_install.rb
@@ -4,6 +4,10 @@ module VagrantPlugins
       module Redhat
         module DockerInstall
           def self.docker_install(machine, version)
+            if version != :latest
+              machine.ui.warn(I18n.t("vagrant.docker_install_with_version_not_supported"))
+            end
+
             machine.communicate.tap do |comm|
               if ! comm.test("rpm -qa | grep epel-release")
                 comm.sudo("rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm")
