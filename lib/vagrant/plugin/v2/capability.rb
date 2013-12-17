48f2fdb009a62099ce0c44f50e68280d0bdb87b5
John Bellone <john.bellone.jr@gmail.com>
2013-12-17 18:38:07 -0500

    Support original guest capability implementation.
    This commit makes an attempt to not have a breaking change with the new
    capability class.

--- lib/vagrant/plugin/v2/capability.rb
+++ lib/vagrant/plugin/v2/capability.rb
@@ -2,7 +2,9 @@
 module Vagrant
   module Plugin
     module V2
-      # Base class responsible for defining a capability in Vagrant.
+      # Base class responsible for defining a capability in Vagrant.  A 
+      # capability is responsible for exposing specialized, unique features
+      # of a particular subsystem of a machine.
       class Capability
         # This should return a brief (60 characters or less) synposis of what
         # this capability does.  It will be used in the output of the help.
@@ -24,10 +26,24 @@ module Vagrant
         # @param [Symbol] name Capability identifier name.
         def initialize(name, &block)
           @name = name.to_sym
-          define_method(name, &block) if block_given?
+          
+          # When a block is passed into the initialization of this class we
+          # treat it as an callable which responds when called with the name
+          # of the capability. This is mainly for backwards compatability. 
+          if block_given?
+            alias_method(name, execute)
+            define_method('execute', -> { k = block.call; k.send(@name) })
+          end
         end
-        
-        def detect?
+
+        # Executes the capability in the context of the machine and name provided.
+        # 
+        # @param [Vagrant::Machine] machine
+        # @param [String] name The name of the capability.
+        def execute(machine, name)
+        end
+
+        def detect?(machine)
           false
         end
       end
