a4c74ea87a147b6d71b4412556c5c07041c11db1
Seginoviax <jharveysmith@gmail.com>
2013-12-31 17:12:19 -0800

    Revert "Add funtoo guest support.  Based heavily on https://github.com/mitchellh/vagrant/pull/1840"
    This reverts commit 3559c0808607e90237c3f86c750404b97e7a531c.

--- plugins/guests/funtoo/guest.rb
+++
@@ -1,9 +0,0 @@
-module VagrantPlugins
-  module GuestFuntoo
-    class Guest < Vagrant.plugin("2", :guest)
-      def detect?(machine)
-        machine.communicate.test("grep Funtoo /etc/gentoo-release")
-      end
-    end
-  end
-end
