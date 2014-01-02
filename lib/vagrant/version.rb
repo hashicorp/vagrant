4f0eb9504cc786d5a57a43814427e8eb35407a4c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-02 10:16:06 -0800

    v1.4.3

--- lib/vagrant/version.rb
+++ lib/vagrant/version.rb
@@ -2,5 +2,5 @@ module Vagrant
   # This will always be up to date with the current version of Vagrant,
   # since it is used to generate the gemspec and is also the source of
   # the version for `vagrant -v`
-  VERSION = "1.4.3.dev"
+  VERSION = "1.4.3"
 end
