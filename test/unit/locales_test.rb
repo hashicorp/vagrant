cc29b2de5c920ac484a7771a97f3fe7cdae962f6
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-01-07 20:07:29 +0300

    fix locales_test.rb

--- test/unit/locales_test.rb
+++ test/unit/locales_test.rb
@@ -1,3 +1,4 @@
+require File.expand_path("../base", __FILE__)
 require 'i18n/tasks'
 require 'i18n/tasks/base_task'
 
@@ -5,10 +6,10 @@ describe 'translation keys'  do
   let(:i18n) { I18n::Tasks::BaseTask.new }
 
   it 'are all used' do
-    i18n.unused_keys.should have(0).keys
+    expect(i18n.unused_keys).to be_empty
   end
 
   it 'are all present' do
-    i18n.untranslated_keys.should have(0).keys
+    expect(i18n.untranslated_keys).to be_empty
   end
 end
