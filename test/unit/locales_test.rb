e8d173b06bfb1dd98225cdfe00e60a79e2a8a5b6
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-01-07 20:12:01 +0300

    locales_test: improve failure message

--- test/unit/locales_test.rb
+++ test/unit/locales_test.rb
@@ -6,10 +6,10 @@ describe 'translation keys'  do
   let(:i18n) { I18n::Tasks::BaseTask.new }
 
   it 'are all used' do
-    expect(i18n.unused_keys).to be_empty
+    expect(i18n.unused_keys).to have(0).keys
   end
 
   it 'are all present' do
-    expect(i18n.untranslated_keys).to be_empty
+    expect(i18n.untranslated_keys).to have(0).keys
   end
 end
