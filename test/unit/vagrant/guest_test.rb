40babfc3f7cbbe9a0a0b4d95db833bd96f7c3a39
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-07 16:27:12 -0800

    core: modify Guest to use new CapabilityHost mixin

--- test/unit/vagrant/guest_test.rb
+++ test/unit/vagrant/guest_test.rb
@@ -3,7 +3,7 @@ require "pathname"
 require File.expand_path("../../base", __FILE__)
 
 describe Vagrant::Guest do
-  include_context "unit"
+  include_context "capability_helpers"
 
   let(:capabilities) { {} }
   let(:guests)  { {} }
@@ -18,44 +18,27 @@ describe Vagrant::Guest do
 
   subject { described_class.new(machine, guests, capabilities) }
 
-  # This registers a capability with a specific guest
-  def register_capability(guest, capability, options=nil)
-    options ||= {}
+  describe "#detect!" do
+    it "auto-detects if no explicit guest name given" do
+      machine.config.vm.stub(guest: nil)
+      subject.should_receive(:initialize_capabilities!).
+        with(nil, guests, capabilities, machine)
 
-    cap = Class.new do
-      if !options[:corrupt]
-        define_method(capability) do |*args|
-          raise "cap: #{capability} #{args.inspect}"
-        end
-      end
+      subject.detect!
     end
 
-    capabilities[guest] ||= {}
-    capabilities[guest][capability] = cap.new
-  end
+    it "uses the explicit guest name if specified" do
+      machine.config.vm.stub(guest: :foo)
+      subject.should_receive(:initialize_capabilities!).
+        with(:foo, guests, capabilities, machine)
 
-  # This registers a guest with the class.
-  #
-  # @param [Symbol] name Name of the guest
-  # @param [Symbol] parent Name of the parent
-  # @param [Boolean] detect Whether or not to detect properly
-  def register_guest(name, parent, detect)
-    guest = Class.new(Vagrant.plugin("2", "guest")) do
-      define_method(:name) do
-        name
-      end
-
-      define_method(:detect?) do |m|
-        detect
-      end
+      subject.detect!
     end
-
-    guests[name] = [guest, parent]
   end
 
   describe "#ready?" do
     before(:each) do
-      register_guest(:foo, nil, true)
+      guests[:foo] = [detect_class(true), nil]
     end
 
     it "should not be ready by default" do
