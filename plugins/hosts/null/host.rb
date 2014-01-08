26b564566b5387dfdf32e25e7ed386407617c923
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-07 21:14:33 -0800

    hosts/null: needs to accept env as param

--- plugins/hosts/null/host.rb
+++ plugins/hosts/null/host.rb
@@ -3,7 +3,7 @@ require "vagrant"
 module VagrantPlugins
   module HostNull
     class Host < Vagrant.plugin("2", :host)
-      def detect?
+      def detect?(env)
         # This host can only be explicitly chosen.
         false
       end
