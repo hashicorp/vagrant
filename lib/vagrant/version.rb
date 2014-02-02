3ed7b2618aeb01ad4726e82007d02a970fe8f0bb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-02-02 15:33:12 +0100

    Add new version.txt used to generate installers

--- lib/vagrant/version.rb
+++ lib/vagrant/version.rb
@@ -2,5 +2,6 @@ module Vagrant
   # This will always be up to date with the current version of Vagrant,
   # since it is used to generate the gemspec and is also the source of
   # the version for `vagrant -v`
-  VERSION = "1.5.0.dev"
+  VERSION = File.read(
+    File.expand_path("../../../version.txt", __FILE__)).chomp
 end
