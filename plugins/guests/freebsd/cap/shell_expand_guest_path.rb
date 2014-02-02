023d3115dd5a74f406600ba450f1a2a1ed188fcf
Teemu Matilainen <teemu.matilainen@iki.fi>
2014-02-02 17:38:24 -0300

    guests/{free,net,open}bsd: Add `shell_expand_guest_path` capability

---
+++ plugins/guests/freebsd/cap/shell_expand_guest_path.rb
@@ -0,0 +1,27 @@
+module VagrantPlugins
+  module GuestFreeBSD
+    module Cap
+      class ShellExpandGuestPath
+        def self.shell_expand_guest_path(machine, path)
+          real_path = nil
+          machine.communicate.execute("printf #{path}",
+                                      shell: "sh") do |type, data|
+            if type == :stdout
+              real_path ||= ""
+              real_path += data
+            end
+          end
+
+          if !real_path
+            # If no real guest path was detected, this is really strange
+            # and we raise an exception because this is a bug.
+            raise ShellExpandFailed
+          end
+
+          # Chomp the string so that any trailing newlines are killed
+          return real_path.chomp
+        end
+      end
+    end
+  end
+end
