88247797d436a01133abb9674600f1fd82b8530f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-02-15 18:18:46 -0800

    providers/hyperv: more cleanup

--- plugins/providers/hyperv/communicator.rb
+++
@@ -1,8 +0,0 @@
-module VagrantPlugins
-  module HyperV
-    module Communicator
-      lib_path = Pathname.new(File.expand_path("../communicator", __FILE__))
-      autoload :SSH, lib_path.join("ssh")
-    end
-  end
-end
