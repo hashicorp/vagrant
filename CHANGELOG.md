eb9b85c3885941dd9cf0b1d543c536502180da0c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-03-11 09:58:19 -0700

    core: UI#ask with no echo works even if noecho not supported [GH-3119]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ BUG FIXES:
   - core: Upgrading the home directory for Vagrant 1.5 uses the Vagrant
     temp dir. [GH-3095]
   - core: Assume a box isn't metadata if it exceeds 20 MB. [GH-3107]
+  - core: Asking for input works even in consoles that don't support
+    hiding input. [GH-3119]
   - provisioners/ansible: Request SSH info within the provision method,
     when we know its available. [GH-3111]
 
--- lib/vagrant/ui.rb
+++ lib/vagrant/ui.rb
@@ -147,11 +147,19 @@ module Vagrant
         if opts[:echo]
           input = $stdin.gets
         else
-          input = $stdin.noecho(&:gets)
+          begin
+            input = $stdin.noecho(&:gets)
 
-          # Output a newline because without echo, the newline isn't
-          # echoed either.
-          say(:info, "\n", opts)
+            # Output a newline because without echo, the newline isn't
+            # echoed either.
+            say(:info, "\n", opts)
+          rescue Errno::EBADF
+            # This means that stdin doesn't support echoless input.
+            say(:info, "\n#{I18n.t("vagrant.stdin_cant_hide_input")}\n ", opts)
+
+            # Ask again, with echo enabled
+            input = ask(message, opts.merge(echo: true))
+          end
         end
 
         # Get the results and chomp off the newline. We do a logical OR
