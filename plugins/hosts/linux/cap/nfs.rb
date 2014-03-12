da15fa72c485079d1b4ef3ba063bd0e26a404187
Russell Jackson <raj@csub.edu>
2014-03-12 01:26:29 -0700

    Fix NFS exports being broken yet again on Linux
    The Ruby Regexp.escape method does not escape forward slashes because
    it is not a meta-character. It is unique to sed expressions. An
    alternative delimiter may be specified by prefixing the address
    expression with a backslash with the delimiter character folloing.
    
    Use control character (ASCII code 0x1a) as expression delimiter so it is
    very unlikely an identifier will conflict.

--- plugins/hosts/linux/cap/nfs.rb
+++ plugins/hosts/linux/cap/nfs.rb
@@ -94,7 +94,7 @@ module VagrantPlugins
 
           # Use sed to just strip out the block of code which was inserted
           # by Vagrant
-          system("sudo sed -r -e '/^# VAGRANT-BEGIN:( #{user})? #{id}/,/^# VAGRANT-END:( #{user})? #{id}/ d' -ibak /etc/exports")
+          system("sudo sed -r -e '\\\x1a^# VAGRANT-BEGIN:( #{user})? #{id}\x1a\\\x1a^# VAGRANT-END:( #{user})? #{id}\x1a d' -ibak /etc/exports")
         end
 
         def self.nfs_opts_setup(folders)
