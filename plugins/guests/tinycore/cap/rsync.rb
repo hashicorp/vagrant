5ef0d881d0c650b44e5a2dc6a127250842715a95
cuberri <cuberri@gmail.com>
2014-03-13 01:31:49 +0100

    add rsync_install capability to tinycore guest

---
+++ plugins/guests/tinycore/cap/rsync.rb
@@ -0,0 +1,14 @@
+module VagrantPlugins
+  module GuestTinyCore
+    module Cap
+      class RSync
+        def self.rsync_install(machine)
+          machine.communicate.tap do |comm|
+            # do not sudo tce-load
+            comm.execute("tce-load -wi rsync")
+          end
+        end
+      end
+    end
+  end
+end
