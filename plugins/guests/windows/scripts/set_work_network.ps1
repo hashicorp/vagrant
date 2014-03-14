d600e83e6d5f698a389dbfe6516736cde157bfb1
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-03-14 11:23:07 -0700

    guests/windows: configure networks

---
+++ plugins/guests/windows/scripts/set_work_network.ps1
@@ -0,0 +1,6 @@
+# Get network connections
+$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
+$connections = $networkListManager.GetNetworkConnections()
+
+# Set network location to Private for all networks
+$connections | % {$_.GetNetwork().SetCategory(1)}
