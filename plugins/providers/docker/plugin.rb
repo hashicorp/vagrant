8c8826fe096fdc65f25d237138f6f3597d9645b4
Fabio Rehm <fgrehm@gmail.com>
2014-03-26 21:00:41 -0300

    providers/docker: Get rid of vagrant version check from old plugin

--- plugins/providers/docker/plugin.rb
+++ plugins/providers/docker/plugin.rb
@@ -1,9 +1,3 @@
-# TODO: Switch to Vagrant.require_version before 1.0.0
-#       see: https://github.com/mitchellh/vagrant/blob/bc55081e9ffaa6820113e449a9f76b293a29b27d/lib/vagrant.rb#L202-L228
-unless Gem::Requirement.new('>= 1.4.0').satisfied_by?(Gem::Version.new(Vagrant::VERSION))
-  raise 'docker-provider requires Vagrant >= 1.4.0 in order to work!'
-end
-
 I18n.load_path << File.expand_path(File.dirname(__FILE__) + '/../../locales/en.yml')
 I18n.reload!
 
