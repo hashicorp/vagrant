00a0716aeeffcb4c7a38d7e7803bd75e443b4340
Fabio Rehm <fgrehm@gmail.com>
2014-03-26 21:01:34 -0300

    providers/docker: Lazy load locales

--- plugins/providers/docker/plugin.rb
+++ plugins/providers/docker/plugin.rb
@@ -1,6 +1,3 @@
-I18n.load_path << File.expand_path(File.dirname(__FILE__) + '/../../locales/en.yml')
-I18n.reload!
-
 module VagrantPlugins
   module DockerProvider
     class Plugin < Vagrant.plugin("2")
@@ -8,17 +5,29 @@ module VagrantPlugins
 
       provider(:docker, parallel: true) do
         require_relative 'provider'
+        init!
         Provider
       end
 
       config(:docker, :provider) do
         require_relative 'config'
+        init!
         Config
       end
 
       synced_folder(:docker) do
         require File.expand_path("../synced_folder", __FILE__)
         SyncedFolder
+      end
+
+      protected
+
+      def self.init!
+        return if defined?(@_init)
+        I18n.load_path << File.expand_path(
+          "templates/locales/providers_docker.yml", Vagrant.source_root)
+        I18n.reload!
+        @_init = true
       end
     end
   end
