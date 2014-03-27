3cbd98bd41483b584acd48ea63d5535026588962
Emilien Kenler <ekenler@wizcorp.jp>
2014-03-27 16:56:55 +0900

    Ansible Vault support
    Add the following options to the Ansible provisioner:
    - ask_vault_pass
    - vault_password_file

--- plugins/provisioners/ansible/config.rb
+++ plugins/provisioners/ansible/config.rb
@@ -5,6 +5,8 @@ module VagrantPlugins
       attr_accessor :extra_vars
       attr_accessor :inventory_path
       attr_accessor :ask_sudo_pass
+      attr_accessor :ask_vault_pass
+      attr_accessor :vault_password_file
       attr_accessor :limit
       attr_accessor :sudo
       attr_accessor :sudo_user
@@ -21,39 +23,43 @@ module VagrantPlugins
       attr_accessor :raw_ssh_args
 
       def initialize
-        @playbook          = UNSET_VALUE
-        @extra_vars        = UNSET_VALUE
-        @inventory_path    = UNSET_VALUE
-        @ask_sudo_pass     = UNSET_VALUE
-        @limit             = UNSET_VALUE
-        @sudo              = UNSET_VALUE
-        @sudo_user         = UNSET_VALUE
-        @verbose           = UNSET_VALUE
-        @tags              = UNSET_VALUE
-        @skip_tags         = UNSET_VALUE
-        @start_at_task     = UNSET_VALUE
-        @groups            = UNSET_VALUE
-        @host_key_checking = UNSET_VALUE
-        @raw_arguments     = UNSET_VALUE
-        @raw_ssh_args      = UNSET_VALUE
+        @playbook            = UNSET_VALUE
+        @extra_vars          = UNSET_VALUE
+        @inventory_path      = UNSET_VALUE
+        @ask_sudo_pass       = UNSET_VALUE
+        @ask_vault_pass      = UNSET_VALUE
+        @vault_password_file = UNSET_VALUE
+        @limit               = UNSET_VALUE
+        @sudo                = UNSET_VALUE
+        @sudo_user           = UNSET_VALUE
+        @verbose             = UNSET_VALUE
+        @tags                = UNSET_VALUE
+        @skip_tags           = UNSET_VALUE
+        @start_at_task       = UNSET_VALUE
+        @groups              = UNSET_VALUE
+        @host_key_checking   = UNSET_VALUE
+        @raw_arguments       = UNSET_VALUE
+        @raw_ssh_args        = UNSET_VALUE
       end
 
       def finalize!
-        @playbook          = nil if @playbook == UNSET_VALUE
-        @extra_vars        = nil if @extra_vars == UNSET_VALUE
-        @inventory_path    = nil if @inventory_path == UNSET_VALUE
-        @ask_sudo_pass     = false unless @ask_sudo_pass == true
-        @limit             = nil if @limit == UNSET_VALUE
-        @sudo              = false unless @sudo == true
-        @sudo_user         = nil if @sudo_user == UNSET_VALUE
-        @verbose           = nil if @verbose == UNSET_VALUE
-        @tags              = nil if @tags == UNSET_VALUE
-        @skip_tags         = nil if @skip_tags == UNSET_VALUE
-        @start_at_task     = nil if @start_at_task == UNSET_VALUE
-        @groups            = {}  if @groups == UNSET_VALUE
-        @host_key_checking = false unless @host_key_checking == true
-        @raw_arguments     = nil if @raw_arguments == UNSET_VALUE
-        @raw_ssh_args      = nil if @raw_ssh_args == UNSET_VALUE
+        @playbook            = nil if @playbook == UNSET_VALUE
+        @extra_vars          = nil if @extra_vars == UNSET_VALUE
+        @inventory_path      = nil if @inventory_path == UNSET_VALUE
+        @ask_sudo_pass       = false unless @ask_sudo_pass == true
+        @ask_vault_pass      = false unless @ask_sudo_pass == true
+        @vault_password_file = nil if @vault_password_file == UNSET_VALUE
+        @limit               = nil if @limit == UNSET_VALUE
+        @sudo                = false unless @sudo == true
+        @sudo_user           = nil if @sudo_user == UNSET_VALUE
+        @verbose             = nil if @verbose == UNSET_VALUE
+        @tags                = nil if @tags == UNSET_VALUE
+        @skip_tags           = nil if @skip_tags == UNSET_VALUE
+        @start_at_task       = nil if @start_at_task == UNSET_VALUE
+        @groups              = {}  if @groups == UNSET_VALUE
+        @host_key_checking   = false unless @host_key_checking == true
+        @raw_arguments       = nil if @raw_arguments == UNSET_VALUE
+        @raw_ssh_args        = nil if @raw_ssh_args == UNSET_VALUE
       end
 
       def validate(machine)
