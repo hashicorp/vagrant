0f337296fccf7360e314472002d8475ebad05ae6
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-04-11 16:34:53 -0700

    update CHANGELOG

--- plugins/guests/freebsd/config.rb
+++ plugins/guests/freebsd/config.rb
@@ -1,10 +1,19 @@
 module VagrantPlugins
   module GuestFreeBSD
     class Config < Vagrant.plugin("2", :config)
+      # The device prefix for network devices created by Vagrant.
+      # This defaults to "em" but can be set for example to "vtnet"
+      # for virtio devices and so on.
+      #
+      # @return [String]
       attr_accessor :device
 
       def initialize
-        @device = "em"
+        @device = UNSET_VALUE
+      end
+
+      def finalize!
+        @device = "em" if @device == UNSET_VALUE
       end
     end
   end
