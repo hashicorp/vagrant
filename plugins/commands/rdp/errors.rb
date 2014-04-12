ea0bb9acb594cfd3bf26080631020912c5a9f6a7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-04-12 15:40:27 -0700

    commands/rdp: basic skeleton

---
+++ plugins/commands/rdp/errors.rb
@@ -0,0 +1,14 @@
+module VagrantPlugins
+  module CommandRDP
+    module Errors
+      # A convenient superclass for all our errors.
+      class RDPError < Vagrant::Errors::VagrantError
+        error_namespace("vagrant_rdp.errors")
+      end
+
+      class HostUnsupported < RDPError
+        error_key(:host_unsupported)
+      end
+    end
+  end
+end
