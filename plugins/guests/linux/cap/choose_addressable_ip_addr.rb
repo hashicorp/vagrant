023891537138e47c7236c413605ce94cdee48fbf
Taliesin Sisson <taliesins@yahoo.com>
2014-04-13 11:05:39 +0100

    Try to select an ip address for the network share that is close as possible to management ip address. So for people with multiple network cards it should hopefully select the correct one without resorting to hard coding it in the vagrant file.

--- plugins/guests/linux/cap/choose_addressable_ip_addr.rb
+++ plugins/guests/linux/cap/choose_addressable_ip_addr.rb
@@ -1,10 +1,18 @@
+require 'ipaddr'
+
 module VagrantPlugins
   module GuestLinux
     module Cap
       module ChooseAddressableIPAddr
         def self.choose_addressable_ip_addr(machine, possible)
+          info = machine.ssh_info
+
+          host_ip = IPAddr.new(info[:host])
+          possible_ips = possible.map{ |ip| IPAddr.new(ip)}
+          possible_ips = possible_ips.sort_by{|ip| (host_ip.to_i - ip.to_i).abs}
+
           machine.communicate.tap do |comm|
-            possible.each do |ip|
+            possible_ips.each do |ip|
               command = "ping -c1 -w1 -W1 #{ip}"
               if comm.test(command)
                 return ip
