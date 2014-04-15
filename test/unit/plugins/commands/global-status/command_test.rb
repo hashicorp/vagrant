09ff31e3a317e7c334933cd7b229965ee8a1881d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-04-14 20:04:17 -0700

    commands/global-status: add --prune flag

--- test/unit/plugins/commands/global-status/command_test.rb
+++ test/unit/plugins/commands/global-status/command_test.rb
@@ -27,13 +27,54 @@ describe VagrantPlugins::CommandGlobalStatus::Command do
 
   subject { described_class.new(argv, iso_env) }
 
-  describe "execute" do
+  describe "execute with no args" do
     it "succeeds" do
       # Let's put some things in the index
       iso_env.machine_index.set(new_entry("foo"))
       iso_env.machine_index.set(new_entry("bar"))
 
       expect(subject.execute).to eq(0)
+    end
+  end
+
+  describe "execute with --prune" do
+    let(:argv) { ["--prune"] }
+
+    it "removes invalid entries" do
+      # Invalid entry because vagrantfile path is gone
+      entryA = new_entry("A")
+      entryA.vagrantfile_path = "/i/dont/exist"
+      locked = iso_env.machine_index.set(entryA)
+      iso_env.machine_index.release(locked)
+
+      # Invalid entry because that specific machine doesn't exist anymore.
+      entryB_env = isolated_environment
+      entryB_env.vagrantfile("")
+      entryB = new_entry("B")
+      entryB.vagrantfile_path = entryB_env.workdir.join("Vagrantfile")
+      locked = iso_env.machine_index.set(entryB)
+      iso_env.machine_index.release(locked)
+
+      # Valid entry because the machine does exist
+      entryC_env = isolated_environment
+      entryC_env.vagrantfile("")
+      entryC_venv = entryC_env.create_vagrant_env
+      entryC_machine = entryC_venv.machine(entryC_venv.machine_names[0], :dummy)
+      entryC_machine.id = "foo"
+      entryC = new_entry(entryC_machine.name)
+      entryC.provider = "dummy"
+      entryC.vagrantfile_path = entryC_env.workdir.join("Vagrantfile")
+      locked = iso_env.machine_index.set(entryC)
+      iso_env.machine_index.release(locked)
+
+      expect(subject.execute).to eq(0)
+
+      # Reload the data and see that we got things correct
+      entries = []
+      iso_env.machine_index.each(true) { |e| entries << e }
+
+      expect(entries.length).to eq(1)
+      expect(entries[0].name).to eq(entryC_machine.name.to_s)
     end
   end
 end
