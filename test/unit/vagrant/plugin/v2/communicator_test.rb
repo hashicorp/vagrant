e78d087c27c941f7277af740b707070c9834f2dd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-04-15 21:25:20 -0700

    Tests passing

--- test/unit/vagrant/plugin/v2/communicator_test.rb
+++ test/unit/vagrant/plugin/v2/communicator_test.rb
@@ -1,9 +1,4 @@
 require File.expand_path("../../../../base", __FILE__)
 
 describe Vagrant::Plugin::V2::Communicator do
-  let(:machine)  { Object.new }
-
-  it "should not match by default" do
-    expect(described_class.match?(machine)).not_to be
-  end
 end
