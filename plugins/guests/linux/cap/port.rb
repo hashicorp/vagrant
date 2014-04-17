19c9973a0d77b8347352ca97ced134915e81be36
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-04-17 15:10:32 -0700

    guests/linux: set a timeout on nc

--- plugins/guests/linux/cap/port.rb
+++ plugins/guests/linux/cap/port.rb
@@ -3,7 +3,7 @@ module VagrantPlugins
     module Cap
       class Port
         def self.port_open_check(machine, port)
-          machine.communicate.test("nc -z 127.0.0.1 #{port}")
+          machine.communicate.test("nc -z -w2 127.0.0.1 #{port}")
         end
       end
     end
