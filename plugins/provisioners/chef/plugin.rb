e7a5268312734004424917592017132135ec5f46
Teemu Matilainen <teemu.matilainen@iki.fi>
2014-05-20 00:05:46 +0300

    provisioners/chef: Add `chef_zero` provisioner
    Add Chef Zero provisioner using `local_mode` configuration for
    chef-solo/chef-client.

--- plugins/provisioners/chef/plugin.rb
+++ plugins/provisioners/chef/plugin.rb
@@ -24,6 +24,11 @@ module VagrantPlugins
         Config::ChefClient
       end
 
+      config(:chef_zero, :provisioner) do
+        require File.expand_path("../config/chef_zero", __FILE__)
+        Config::ChefZero
+      end
+
       provisioner(:chef_solo)   do
         require File.expand_path("../provisioner/chef_solo", __FILE__)
         Provisioner::ChefSolo
@@ -32,6 +37,11 @@ module VagrantPlugins
       provisioner(:chef_client) do
         require File.expand_path("../provisioner/chef_client", __FILE__)
         Provisioner::ChefClient
+      end
+
+      provisioner(:chef_zero)   do
+        require File.expand_path("../provisioner/chef_zero", __FILE__)
+        Provisioner::ChefZero
       end
     end
   end
