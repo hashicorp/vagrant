ac7012279cad54f7afa5950ba893103d988e2079
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-05-24 11:24:25 -0700

    commands/box/add: clarify help text

--- plugins/commands/box/command/add.rb
+++ plugins/commands/box/command/add.rb
@@ -8,7 +8,7 @@ module VagrantPlugins
           options = {}
 
           opts = OptionParser.new do |o|
-            o.banner = "Usage: vagrant box add [options] <url>"
+            o.banner = "Usage: vagrant box add [options] <name or url>"
             o.separator ""
             o.separator "Options:"
             o.separator ""
