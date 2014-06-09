df9ef15cd96d2f9c426020c51e8f8867b00cb2aa
Daniel Smith <dsmith@blitzagency.com>
2014-06-09 12:46:47 -0700

    Update Salt Bootstrap URLs to offical URLs

--- plugins/provisioners/salt/bootstrap-salt.sh
+++ plugins/provisioners/salt/bootstrap-salt.sh
@@ -2,11 +2,11 @@
 
 # We just download the bootstrap script by default and execute that.
 if [ -x /usr/bin/fetch ]; then
-    /usr/bin/fetch -o - https://raw.githubusercontent.com/saltstack/salt-bootstrap/stable/bootstrap-salt.sh | sh -s -- "$@"
+    /usr/bin/fetch -o - https://bootstrap.saltstack.com | sh -s -- "$@"
 elif [ -x /usr/bin/curl ]; then
-    /usr/bin/curl -L https://raw.githubusercontent.com/saltstack/salt-bootstrap/stable/bootstrap-salt.sh | sh -s -- "$@"
+    /usr/bin/curl -L https://bootstrap.saltstack.com | sh -s -- "$@"
 else
     python \
-        -c 'import urllib; print urllib.urlopen("https://raw.githubusercontent.com/saltstack/salt-bootstrap/stable/bootstrap-salt.sh").read()' \
+        -c 'import urllib; print urllib.urlopen("https://bootstrap.saltstack.com").read()' \
         | sh -s -- "$@"
 fi
