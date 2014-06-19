12c7b7f0fe519fe0f5b69c1c29562068e0f83067
Loïc PORTE <loic.porte@pmsipilot.com>
2014-06-19 14:14:41 +0200

    correction install nfs client on RHEL
    On Centos/Redhat just install nfs-utils is not enought. We need to start
    rpcbind, else we have this error :
    
    mount.nfs: rpc.statd is not running but is required for remote locking.
    mount.nfs: Either use '-o nolock' to keep locks local, or start statd.
    mount.nfs: an incorrect mount option was specified

--- plugins/guests/redhat/cap/nfs_client.rb
+++ plugins/guests/redhat/cap/nfs_client.rb
@@ -5,6 +5,8 @@ module VagrantPlugins
         def self.nfs_client_install(machine)
           machine.communicate.tap do |comm|
             comm.sudo("yum -y install nfs-utils nfs-utils-lib")
+            comm.sudo("chkconfig rpcbind on")
+            comm.sudo("service rpcbind start")
           end
         end
       end
