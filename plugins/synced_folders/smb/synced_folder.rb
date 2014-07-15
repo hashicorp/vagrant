33d8717556d5db7b2c3436f358d0d06a77e59370
Matt Wrock <matt@mattwrock.com>
2014-07-15 04:30:15 -0700

    adding smb sync folder implementation for windows guests fixing #3699

--- plugins/synced_folders/smb/synced_folder.rb
+++ plugins/synced_folders/smb/synced_folder.rb
@@ -150,29 +150,6 @@ module VagrantPlugins
 
         JSON.parse(r.stdout)["ip_addresses"]
       end
-
-=begin
-        def mount_shared_folders_to_windows
-          result = @env[:machine].provider.driver.execute('host_info.ps1', {})
-          @smb_shared_folders.each do |id, data|
-            begin
-              options = { share_name: data[:share_name],
-                          guest_path: data[:guestpath].gsub("/", "\\"),
-                          guest_ip: ssh_info[:host],
-                          username: ssh_info[:username],
-                          host_ip: result["host_ip"],
-                          password: @env[:machine].provider_config.guest.password,
-                          host_share_username: @env[:machine].provider_config.host_share.username,
-                          host_share_password: @env[:machine].provider_config.host_share.password}
-              @env[:ui].info("Linking #{data[:share_name]} to Guest at #{data[:guestpath]} ...")
-              @env[:machine].provider.driver.execute('mount_share.ps1', options)
-            rescue Error::SubprocessError => e
-              @env[:ui].info "Failed to link #{data[:share_name]} to Guest"
-              @env[:ui].info e.message
-            end
-          end
-        end
-=end
     end
   end
 end
