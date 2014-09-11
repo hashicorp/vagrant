063a903bdb2cde1a28da6f35e3db3be118807fdf
Thomas Boerger <tboerger@suse.de>
2014-09-11 10:52:58 +0200

    Fixed and extended suse guest capabilities
    In order to get a SUSE guest running and installing fine i have added a
    correct capability for installing rsync and nfs-client.
    
    I have included SUSE naming fixes as well because SUSe doesnt get
    spelled SuSE anymore :).

--- plugins/guests/suse/plugin.rb
+++ plugins/guests/suse/plugin.rb
@@ -1,12 +1,12 @@
 require "vagrant"
 
 module VagrantPlugins
-  module GuestSuse
+  module GuestSUSE
     class Plugin < Vagrant.plugin("2")
       name "SUSE guest"
       description "SUSE guest support."
 
-      guest("suse", "redhat") do
+      guest("suse", "linux") do
         require File.expand_path("../guest", __FILE__)
         Guest
       end
@@ -16,19 +16,34 @@ module VagrantPlugins
         Cap::ChangeHostName
       end
 
-      guest_capability("suse", "halt") do
-        require_relative "cap/halt"
-        Cap::Halt
-      end
-
       guest_capability("suse", "configure_networks") do
         require_relative "cap/configure_networks"
         Cap::ConfigureNetworks
       end
 
+      guest_capability("suse", "halt") do
+        require_relative "cap/halt"
+        Cap::Halt
+      end
+
       guest_capability("suse", "network_scripts_dir") do
         require_relative "cap/network_scripts_dir"
         Cap::NetworkScriptsDir
+      end
+
+      guest_capability("suse", "nfs_client_install") do
+        require_relative "cap/nfs_client"
+        Cap::NFSClient
+      end
+
+      guest_capability("suse", "rsync_install") do
+        require_relative "cap/rsync"
+        Cap::RSync
+      end
+
+      guest_capability("suse", "rsync_installed") do
+        require_relative "cap/rsync"
+        Cap::RSync
       end
     end
   end
