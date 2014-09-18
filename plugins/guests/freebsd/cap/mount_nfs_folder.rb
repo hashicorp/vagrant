d4b30dd12aa810d497c94ff26644288fed385402
Matthias Breddin <mb@lunetics.com>
2014-09-18 17:14:13 +0200

    Allow nfs

--- plugins/guests/freebsd/cap/mount_nfs_folder.rb
+++ plugins/guests/freebsd/cap/mount_nfs_folder.rb
@@ -4,8 +4,10 @@ module VagrantPlugins
       class MountNFSFolder
         def self.mount_nfs_folder(machine, ip, folders)
           folders.each do |name, opts|
-            machine.communicate.sudo("mkdir -p #{opts[:guestpath]}", {shell: "sh"})
-            machine.communicate.sudo("mount -t nfs '#{ip}:#{opts[:hostpath]}' '#{opts[:guestpath]}'", {shell: "sh"})
+            if opts[:nfs_version]
+              nfs_version_mount_option="-o nfsv#{opts[:nfs_version]}"
+   	        end
+            machine.communicate.sudo("mount -t nfs #{nfs_version_mount_option} '#{ip}:#{opts[:hostpath]}' '#{opts[:guestpath]}'", {shell: "sh"})
           end
         end
       end
