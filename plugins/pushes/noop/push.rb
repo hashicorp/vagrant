190da2640492f4bfe70ca3e3908425f0dfbcdc4a
Seth Vargo <sethvargo@gmail.com>
2014-10-23 16:02:51 -0400

    Push does not have access to @machine

--- plugins/pushes/noop/push.rb
+++ plugins/pushes/noop/push.rb
@@ -2,9 +2,7 @@ module VagrantPlugins
   module NoopDeploy
     class Push < Vagrant.plugin("2", :push)
       def push
-        @machine.communicate.tap do |comm|
-          puts "pushed"
-        end
+        puts "pushed"
       end
     end
   end
