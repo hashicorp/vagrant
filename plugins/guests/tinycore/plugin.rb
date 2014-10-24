ba994baa65b05fa30d8fb9bd1fe93cc3a114151c
Tarrant <tarrant@keyneston.com>
2014-10-24 13:52:06 -0700

    Revert "guests/tinycore: error if DHCP [GH-4699]"
    This reverts commit 64139f2158a957998126f6cf2abcf1042f982175.

--- plugins/guests/tinycore/plugin.rb
+++ plugins/guests/tinycore/plugin.rb
@@ -2,44 +2,28 @@ require "vagrant"
 
 module VagrantPlugins
   module GuestTinyCore
-    autoload :Errors, File.expand_path("../errors", __FILE__)
-
     class Plugin < Vagrant.plugin("2")
       name "TinyCore Linux guest."
       description "TinyCore Linux guest support."
 
       guest("tinycore", "linux")  do
         require File.expand_path("../guest", __FILE__)
-        init!
         Guest
       end
 
       guest_capability("tinycore", "configure_networks") do
         require_relative "cap/configure_networks"
-        init!
         Cap::ConfigureNetworks
       end
 
       guest_capability("tinycore", "halt") do
         require_relative "cap/halt"
-        init!
         Cap::Halt
       end
 
       guest_capability("tinycore", "rsync_install") do
         require_relative "cap/rsync"
-        init!
         Cap::RSync
-      end
-
-      protected
-
-      def self.init!
-        return if defined?(@_init)
-        I18n.load_path << File.expand_path(
-          "templates/locales/guest_tinycore.yml", Vagrant.source_root)
-        I18n.reload!
-        @_init = true
       end
     end
   end
