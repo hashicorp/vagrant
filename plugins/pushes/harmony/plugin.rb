f3f4f4aeb62c6893d3932b0c68f1e4cabe3461a5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-10-29 09:41:47 -0700

    pushes/harmony: stub I18n

--- plugins/pushes/harmony/plugin.rb
+++ plugins/pushes/harmony/plugin.rb
@@ -12,12 +12,24 @@ module VagrantPlugins
 
       config(:harmony, :push) do
         require File.expand_path("../config", __FILE__)
+        init!
         Config
       end
 
       push(:harmony) do
         require File.expand_path("../push", __FILE__)
+        init!
         Push
+      end
+
+      protected
+
+      def self.init!
+        return if defined?(@_init)
+        I18n.load_path << File.expand_path(
+          "templates/locales/TODO.yml", Vagrant.source_root)
+        I18n.reload!
+        @_init = true
       end
     end
   end
