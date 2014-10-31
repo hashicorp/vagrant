8f35ecaa6f00878a236acb04b0f07544cc8ab9b5
Seth Vargo <sethvargo@gmail.com>
2014-10-31 14:22:09 -0400

    Extract runner-specific Chef configs into their own subclass
    This separates the truly basic pieces of Chef (like install url and
    log_level) from the runner pieces of Chef (like provisioning_path). This
    is necessary because the Chef Apply provisioner does not actually need
    most of the Chef configuration options.

--- plugins/provisioners/chef/config/chef_apply.rb
+++ plugins/provisioners/chef/config/chef_apply.rb
@@ -1,9 +1,7 @@
 module VagrantPlugins
   module Chef
     module Config
-      class ChefApply < Vagrant.plugin("2", :config)
-        extend Vagrant::Util::Counter
-
+      class ChefApply < Base
         # The raw recipe text (as a string) to execute via chef-apply.
         # @return [String]
         attr_accessor :recipe
@@ -13,25 +11,17 @@ module VagrantPlugins
         # @return [String]
         attr_accessor :upload_path
 
-        # The Chef log level.
-        # @return [String]
-        attr_accessor :log_level
-
         def initialize
-          @recipe  = UNSET_VALUE
+          super
 
-          @log_level   = UNSET_VALUE
+          @recipe      = UNSET_VALUE
           @upload_path = UNSET_VALUE
         end
 
         def finalize!
-          @recipe = nil if @recipe == UNSET_VALUE
+          super
 
-          if @log_level == UNSET_VALUE
-            @log_level = :info
-          else
-            @log_level = @log_level.to_sym
-          end
+          @recipe = nil if @recipe == UNSET_VALUE
 
           if @upload_path == UNSET_VALUE
             counter = self.class.get_and_update_counter(:chef_apply)
@@ -40,27 +30,17 @@ module VagrantPlugins
         end
 
         def validate(machine)
-          errors = _detected_errors
+          errors = validate_base(machine)
 
-          if missing(recipe)
+          if missing?(recipe)
             errors << I18n.t("vagrant.provisioners.chef.recipe_empty")
           end
 
-          if missing(log_level)
-            errors << I18n.t("vagrant.provisioners.chef.log_level_empty")
-          end
-
-          if missing(upload_path)
+          if missing?(upload_path)
             errors << I18n.t("vagrant.provisioners.chef.upload_path_empty")
           end
 
           { "chef apply provisioner" => errors }
-        end
-
-        # Determine if the given string is "missing" (blank)
-        # @return [true, false]
-        def missing(obj)
-          obj.to_s.strip.empty?
         end
       end
     end
