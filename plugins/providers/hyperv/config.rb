4c35f6b07041ece1506b799dbc6a0941b965b9b1
Tomas Srnka <tomas.srnka@gmail.com>
2015-01-11 18:10:46 +0100

    Revert "Added config option to specify VLanId for Network Adapter when using HyperV provider"
    This reverts commit 2a487af68d968c75a6c3fc04406d00a54b2ad50a.

--- plugins/providers/hyperv/config.rb
+++ plugins/providers/hyperv/config.rb
@@ -9,23 +9,13 @@ module VagrantPlugins
       # @return [Integer]
       attr_accessor :ip_address_timeout
 
-      # The default VLAN ID for network interface for the virtual machine.
-      #
-      # @return [Integer]
-      attr_accessor :vlan_id
-
       def initialize
         @ip_address_timeout = UNSET_VALUE
-        @vlan_id  = UNSET_VALUE
       end
 
       def finalize!
         if @ip_address_timeout == UNSET_VALUE
           @ip_address_timeout = 120
-        end
-
-        if @vlan_id == UNSET_VALUE
-          @vlan_id = 0
         end
       end
 
--- test/unit/plugins/providers/hyperv/config_test.rb
+++ test/unit/plugins/providers/hyperv/config_test.rb
@@ -14,17 +14,5 @@ describe VagrantPlugins::HyperV::Config do
       subject.finalize!
       expect(subject.ip_address_timeout).to eq(120)
     end
-
-  describe "#vlan_id" do
-    it "can be set" do
-      subject.vlan_id = 100
-      subject.finalize!
-      expect(subject.vlan_id).to eq(100)
-    end
-
-    it "defaults to a number" do
-      subject.finalize!
-      expect(subject.vlan_id).to eq(0)
-    end
   end
 end
