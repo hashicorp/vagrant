56a6c85e7df482c922910c941c2dda7bfd1250c8
Richard Guin <rguin@thechannelcompany.com>
2015-01-28 13:07:36 -0500

    Windows rsync needs to create folders before syncing

---
+++ test/unit/plugins/guests/windows/cap/rsync_test.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../../../../base", __FILE__)
+
+require Vagrant.source_root.join("plugins/guests/windows/cap/rsync")
+
+describe "VagrantPlugins::GuestWindows::Cap::RSync" do
+  let(:described_class) do
+    VagrantPlugins::GuestWindows::Plugin.components.guest_capabilities[:windows].get(:rsync_pre)
+  end
+  let(:machine) { double("machine") }
+  let(:communicator) { VagrantTests::DummyCommunicator::Communicator.new(machine) }
+
+  before do
+    allow(machine).to receive(:communicate).and_return(communicator)
+  end
+
+  after do
+    communicator.verify_expectations!
+  end
+
+  describe ".rsync_pre" do
+    it 'makes the guestpath directory with mkdir' do
+      communicator.expect_command("mkdir '/sync_dir'")
+      described_class.rsync_pre(machine, guestpath: '/sync_dir')
+    end
+  end
+end
