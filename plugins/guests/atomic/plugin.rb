bd2f2fc3a01bc86630d277c100befde63c114187
Josef Stribny <jstribny@redhat.com>
2015-05-12 16:30:42 +0200

    Add Atomic guest support

---
+++ plugins/guests/atomic/plugin.rb
@@ -0,0 +1,25 @@
+require 'vagrant'
+
+module VagrantPlugins
+  module GuestAtomic
+    class Plugin < Vagrant.plugin("2")
+      name "Atomic Host guest"
+      description "Atomic Host guest support."
+
+      guest("atomic", "fedora") do
+        require File.expand_path("../guest", __FILE__)
+        Guest
+      end
+
+      guest_capability("atomic", "change_host_name") do
+        require_relative "cap/change_host_name"
+        Cap::ChangeHostName
+      end
+
+      guest_capability("atomic", "docker_daemon_running") do
+        require_relative "cap/docker"
+        Cap::Docker
+      end
+    end
+  end
+end
