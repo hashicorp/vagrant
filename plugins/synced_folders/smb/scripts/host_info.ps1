ac9b0b7978e234946e377272587d720a1bb238ac
Matt Wrock <matt@mattwrock.com>
2015-06-05 06:19:58 -0700

    prefer ipv4 host address over ipv6 for smb synced folders

--- plugins/synced_folders/smb/scripts/host_info.ps1
+++ plugins/synced_folders/smb/scripts/host_info.ps1
@@ -1,6 +1,9 @@
 $ErrorAction = "Stop"
 
-$net = Get-NetIPAddress | Where-Object {($_.IPAddress -ne "127.0.0.1") -and ($_.IPAddress -ne "::1")}
+$net = Get-NetIPAddress | Where-Object {
+    ($_.IPAddress -ne "127.0.0.1") -and ($_.IPAddress -ne "::1")
+} | Sort-Object $_.AddressFamily
+
 $result = @{
 	ip_addresses = $net.IPAddress
 }
