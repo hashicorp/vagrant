94549ab755e2aaefed856a999241b57a901e7f14
arthurvanduynhoven <arthur.vanduynhoven@irdeto.com>
2015-06-24 14:56:28 +0200

    Need to do update before installing linux-image-extra kernel in case system is outdated.

--- plugins/provisioners/docker/cap/debian/docker_install.rb
+++ plugins/provisioners/docker/cap/debian/docker_install.rb
@@ -8,11 +8,11 @@ module VagrantPlugins
             package << "-#{version}" if version != :latest
 
             machine.communicate.tap do |comm|
+              comm.sudo("apt-get update -y")
               # TODO: Perform check on the host machine if aufs is installed and using LXC
               if machine.provider_name != :lxc
                 comm.sudo("lsmod | grep aufs || modprobe aufs || apt-get install -y linux-image-extra-`uname -r`")
               end
-              comm.sudo("apt-get update -y")
               comm.sudo("apt-get install -y --force-yes -q curl")
               comm.sudo("curl -sSL https://get.docker.com/gpg | apt-key add -")
               comm.sudo("echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list")
