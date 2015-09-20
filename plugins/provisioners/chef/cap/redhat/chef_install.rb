acde6e1b16674f3d63f6d2e94e43ce57c95641fa
Rickard von Essen <rickard.von.essen@gmail.com>
2015-09-20 09:30:49 +0200

    Use dnf on Fedora guests instead of yum if available.
    Fixes #6286 now properly installs Docker on Fedora guests.
    Fixes #6287 use dnf if available.

--- plugins/provisioners/chef/cap/redhat/chef_install.rb
+++ plugins/provisioners/chef/cap/redhat/chef_install.rb
@@ -6,10 +6,20 @@ module VagrantPlugins
       module Redhat
         module ChefInstall
           def self.chef_install(machine, version, prerelease, download_path)
-            machine.communicate.sudo("yum install -y -q curl")
+            if dnf?(machine)
+              machine.communicate.sudo("dnf install -y -q curl")
+            else
+              machine.communicate.sudo("yum install -y -q curl")
+            end
 
             command = Omnibus.build_command(version, prerelease, download_path)
             machine.communicate.sudo(command)
+          end
+
+          protected
+
+          def self.dnf?(machine)
+            machine.communicate.test("/usr/bin/which -s dnf")
           end
         end
       end
