2da5c22fbc390113d619905f21adda03ec3ac3ea
gpkfr <gpkfr@imelbox.com>
2015-10-23 01:34:07 +0200

    #6315-FIX Added diversion when Virtualbox is used.

--- plugins/guests/debian8/cap/halt.rb
+++ plugins/guests/debian8/cap/halt.rb
@@ -1,10 +1,16 @@
 module VagrantPlugins
   module GuestDebian8
     module Cap
-      class Halt
+    class Halt
         def self.halt(machine)
           begin
-            machine.communicate.sudo("shutdown -h -H")
+            machine.communicate.tap do |comm|
+              if comm.test("lsmod |grep -w vboxguest")
+                machine.communicate.sudo("shutdown -h now")
+              else
+                machine.communicate.sudo("shutdown -h -H")
+              end
+            end
           rescue IOError
             # Do nothing, because it probably means the machine shut down
             # and SSH connection was lost.
