6e0740a6a8e5658340af879d65ebc6010d86f61f
Steve Moss <gawbul@gmail.com>
2015-10-28 14:26:30 +0000

    Fixes for vagrant halt/reload not finding shutdown command in some guest OSs (ref issue 6449)

--- plugins/guests/redhat/cap/halt.rb
+++ plugins/guests/redhat/cap/halt.rb
@@ -1,10 +1,10 @@
 module VagrantPlugins
-  module GuestLinux
+  module GuestRedHat
     module Cap
       class Halt
         def self.halt(machine)
           begin
-            machine.communicate.sudo("PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin shutdown -h now")
+            machine.communicate.sudo("/usr/sbin/shutdown -h now")
           rescue IOError
             # Do nothing, because it probably means the machine shut down
             # and SSH connection was lost.
