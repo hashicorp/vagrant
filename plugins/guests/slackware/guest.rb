80021ceafb10253c15d581ba2b2a1a98b2fb0da0
Olivier Meurice <olivier.meurice@trasysgroup.com>
2015-11-12 14:33:04 +0100

    Add network configuration plugin for Slackware Linux

---
+++ plugins/guests/slackware/guest.rb
@@ -0,0 +1,11 @@
+require "vagrant"
+
+module VagrantPlugins
+  module GuestSlackware
+    class Guest < Vagrant.plugin("2", :guest)
+      def detect?(machine)
+        machine.communicate.test("cat /etc/slackware-version")
+      end
+    end
+  end
+end
