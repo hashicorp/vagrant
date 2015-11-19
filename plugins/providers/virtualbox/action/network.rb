925d7c5bf6e106be3f97b0d2b015a4cf9b1e9879
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-11-18 17:11:58 -0800

    Revert "Make DHCP IP configurable"
    This reverts commit 004fe0e7bb0c17c4f0e0bc801440a6456d7b979d.

--- plugins/providers/virtualbox/action/network.rb
+++ plugins/providers/virtualbox/action/network.rb
@@ -250,14 +250,13 @@ module VagrantPlugins
             mac:         nil,
             nic_type:    nil,
             type:        :static,
-            dhcp_ip:     "172.28.128.1",
           }.merge(options)
 
           # Make sure the type is a symbol
           options[:type] = options[:type].to_sym
 
           # Default IP is in the 20-bit private network block for DHCP based networks
-          options[:ip] ||= options[:dhcp_ip] if options[:type] == :dhcp
+          options[:ip] = "172.28.128.1" if options[:type] == :dhcp && !options[:ip]
 
           ip = IPAddr.new(options[:ip])
           if ip.ipv4?
