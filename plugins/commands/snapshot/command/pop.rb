7ba398ead8ebca164be61c12c11bc79a5c01d82e
Jon Topper <jon@scalefactory.com>
2016-01-15 18:21:47 +0000

    Add --no-delete to 'snapshot pop' command
    In our test environments, it's good to be able to roll back to the same,
    anonymous, snapshot repeatedly.  This patch adds a `--no-delete` option
    to the `snapshot pop` command allowing this.
    
    This makes the new core snapshot behaviour more consistent with what we
    were doing with vagrant-multiprovider-snap
    (https://github.com/scalefactory/vagrant-multiprovider-snap)

--- plugins/commands/snapshot/command/pop.rb
+++ plugins/commands/snapshot/command/pop.rb
@@ -10,17 +10,22 @@ module VagrantPlugins
         include PushShared
 
         def execute
+          options = {}
           opts = OptionParser.new do |o|
             o.banner = "Usage: vagrant snapshot pop [options] [vm-name]"
             o.separator ""
             o.separator "Restore state that was pushed with `vagrant snapshot push`."
+
+            o.on("--no-delete", "Don't delete the snapshot after the restore") do
+                options[:no_delete] = true
+            end
           end
 
           # Parse the options
           argv = parse_options(opts)
           return if !argv
 
-          return shared_exec(argv, method(:pop))
+          return shared_exec(argv, method(:pop), options)
         end
       end
     end
