4796ab3b702ac60142e4ddef56c399fad5cb121a
Benjamin Koep <benjamin.koep@trivago.com>
2016-01-25 12:43:29 +0100

    * Ensure that run_highstate setting is respected when bringing up a   masterless minion

--- plugins/provisioners/salt/provisioner.rb
+++ plugins/provisioners/salt/provisioner.rb
@@ -321,15 +321,19 @@ module VagrantPlugins
       end
 
       def call_masterless
-        @machine.env.ui.info "Calling state.highstate in local mode... (this may take a while)"
-        cmd = "salt-call state.highstate --local#{get_loglevel}#{get_colorize}#{get_pillar}"
-        if @config.minion_id
-          cmd += " --id #{@config.minion_id}"
-        end
-        @machine.communicate.sudo(cmd) do |type, data|
-          if @config.verbose
-            @machine.env.ui.info(data)
+        if @config.run_highstate
+          @machine.env.ui.info "Calling state.highstate in local mode... (this may take a while)"
+          cmd = "salt-call state.highstate --local#{get_loglevel}#{get_colorize}#{get_pillar}"
+          if @config.minion_id
+            cmd += " --id #{@config.minion_id}"
           end
+          @machine.communicate.sudo(cmd) do |type, data|
+            if @config.verbose
+              @machine.env.ui.info(data)
+            end
+          end
+        else
+          @machine.env.ui.info "run_highstate set to false. Not running state.highstate"
         end
       end
 
