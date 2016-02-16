343a43cae6e2a7a75c9beafe6518b47734e36d56
Kevin Blair <kevin@interestingdevs.co.uk>
2016-02-16 14:57:53 +0000

    Preserve Disk Image Name
    detect and preserve the original name of the disk image rather than forcing it to be disk

--- plugins/providers/hyperv/action/import.rb
+++ plugins/providers/hyperv/action/import.rb
@@ -38,10 +38,12 @@ module VagrantPlugins
 
           image_path = nil
           image_ext = nil
+          image_filename = nil
           hd_dir.each_child do |f|
             if %w{.vhd .vhdx}.include?(f.extname.downcase)
               image_path = f
               image_ext = f.extname.downcase
+              image_filename = File.basename(f,image_ext)
               break
             end
           end
@@ -92,7 +94,7 @@ module VagrantPlugins
 
           env[:ui].detail("Cloning virtual hard drive...")
           source_path = image_path.to_s
-          dest_path   = env[:machine].data_dir.join("disk#{image_ext}").to_s
+          dest_path   = env[:machine].data_dir.join("#{image_filename}#{image_ext}").to_s
           FileUtils.cp(source_path, dest_path)
           image_path = dest_path
 
@@ -103,10 +105,10 @@ module VagrantPlugins
             image_path:      image_path.to_s.gsub("/", "\\")
           }
           options[:switchname] = switch if switch
-          options[:memory] = memory if memory 
+          options[:memory] = memory if memory
           options[:maxmemory] = maxmemory if maxmemory
           options[:cpus] = cpus if cpus
-          options[:vmname] = vmname if vmname 
+          options[:vmname] = vmname if vmname
 
           env[:ui].detail("Creating and registering the VM...")
           server = env[:machine].provider.driver.import(options)
