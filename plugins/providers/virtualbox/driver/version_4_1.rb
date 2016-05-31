c63f1cabaf63be814b9d16a12dd04cf5e891a305
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-05-31 14:51:56 +0100

    Improved resilience of the VirtualBox driver read_used_ports function by
    ignoring VMs that may have been deleted between the calls to
    'vboxmanage list vms' and 'vboxmanage showvminfo'

--- plugins/providers/virtualbox/driver/version_4_1.rb
+++ plugins/providers/virtualbox/driver/version_4_1.rb
@@ -525,8 +525,15 @@ module VagrantPlugins
               # Ignore our own used ports
               next if uuid == @uuid
 
-              read_forwarded_ports(uuid, true).each do |_, _, hostport, _|
-                ports << hostport
+              begin
+                read_forwarded_ports(uuid, true).each do |_, _, hostport, _|
+                  ports << hostport
+                end
+              rescue Vagrant::Errors::VBoxManageError => e
+                raise if !e.extra_data[:stderr].include?("VBOX_E_OBJECT_NOT_FOUND")
+
+                # VirtualBox could not find the vm. It may have been deleted
+                # by another process after we called 'vboxmanage list vms'? Ignore this error.
               end
             end
           end
