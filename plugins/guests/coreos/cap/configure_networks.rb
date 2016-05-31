3ca048a8fac025ff33674a8ef521522298b794dc
Seth Vargo <sethvargo@gmail.com>
2016-05-30 23:58:13 -0400

    guests/coreos: Configure networks in one command
    This commit configures all the network devices in a single command.

--- plugins/guests/coreos/cap/configure_networks.rb
+++ plugins/guests/coreos/cap/configure_networks.rb
@@ -10,47 +10,26 @@ module VagrantPlugins
 
         def self.configure_networks(machine, networks)
           machine.communicate.tap do |comm|
-            # Disable default etcd
-            comm.sudo("systemctl stop etcd")
-
             # Read network interface names
             interfaces = []
             comm.sudo("ifconfig | grep '(e[n,t][h,s,p][[:digit:]]([a-z][[:digit:]])?' | cut -f1 -d:") do |_, result|
               interfaces = result.split("\n")
             end
 
-            # Configure interfaces
-            # FIXME: fix matching of interfaces with IP adresses
-            networks.each do |network|
-              comm.sudo("ifconfig #{interfaces[network[:interface].to_i]} #{network[:ip]} netmask #{network[:netmask]}")
-            end
-
             primary_machine_config = machine.env.active_machines.first
             primary_machine = machine.env.machine(*primary_machine_config, true)
 
-            get_ip = lambda do |machine|
-              ip = nil
-              machine.config.vm.networks.each do |type, opts|
-                if type == :private_network && opts[:ip]
-                  ip = opts[:ip]
-                  break
-                end
-              end
-
-              ip
-            end
-
-            primary_machine_ip = get_ip.(primary_machine)
-            current_ip = get_ip.(machine)
+            primary_machine_ip = get_ip(primary_machine)
+            current_ip = get_ip(machine)
             if current_ip == primary_machine_ip
               entry = TemplateRenderer.render("guests/coreos/etcd.service", options: {
-                  my_ip: current_ip
-                })
+                my_ip: current_ip,
+              })
             else
               connection_string = "#{primary_machine_ip}:7001"
               entry = TemplateRenderer.render("guests/coreos/etcd.service", options: {
                 connection_string: connection_string,
-                my_ip: current_ip
+                my_ip: current_ip,
               })
             end
 
@@ -62,12 +41,44 @@ module VagrantPlugins
               comm.upload(f.path, "/tmp/etcd-cluster.service")
             end
 
-            comm.sudo("mv /tmp/etcd-cluster.service /media/state/units/")
-            comm.sudo("systemctl restart local-enable.service")
+            # Build a list of commands
+            commands = []
 
-            # Restart default etcd
-            comm.sudo("systemctl start etcd")
+            # Stop default systemd
+            commands << "systemctl stop etcd"
+
+            # Configure interfaces
+            # FIXME: fix matching of interfaces with IP adresses
+            networks.each do |network|
+              iface = interfaces[network[:interface].to_i]
+              commands << "ifconfig #{iface} #{network[:ip]} netmask #{network[:netmask]}".squeeze(" ")
+            end
+
+            commands << <<-EOH.gsub(/^ {14}/, '')
+              mv /tmp/etcd-cluster.service /media/state/units/
+              systemctl restart local-enable.service
+
+              # Restart default etcd
+              systemctl start etcd
+            EOH
+
+            # Run all network configuration commands in one communicator session.
+            comm.sudo(commands.join("\n"))
           end
+        end
+
+        private
+
+        def self.get_ip(machine)
+          ip = nil
+          machine.config.vm.networks.each do |type, opts|
+            if type == :private_network && opts[:ip]
+              ip = opts[:ip]
+              break
+            end
+          end
+
+          ip
         end
       end
     end
