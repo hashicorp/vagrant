e2b7e28082a4b5e546655526773477e8df833bdd
Seth Vargo <sethvargo@gmail.com>
2016-06-05 14:49:59 -0400

    guests/linux: Update insert_public_key cap to be one command

--- plugins/guests/linux/cap/insert_public_key.rb
+++ plugins/guests/linux/cap/insert_public_key.rb
@@ -1,19 +1,29 @@
-require "vagrant/util/shell_quote"
-
 module VagrantPlugins
   module GuestLinux
     module Cap
       class InsertPublicKey
         def self.insert_public_key(machine, contents)
+          comm = machine.communicate
           contents = contents.chomp
-          contents = Vagrant::Util::ShellQuote.escape(contents, "'")
 
-          machine.communicate.tap do |comm|
-            comm.execute("mkdir -p ~/.ssh")
-            comm.execute("chmod 0700 ~/.ssh")
-            comm.execute("printf '#{contents}\\n' >> ~/.ssh/authorized_keys")
-            comm.execute("chmod 0600 ~/.ssh/authorized_keys")
+          remote_path = "/tmp/vagrant-authorized-keys-#{Time.now.to_i}"
+          Tempfile.open("vagrant-linux-insert-public-key") do |f|
+            f.binmode
+            f.write(contents)
+            f.fsync
+            f.close
+            comm.upload(f.path, remote_path)
           end
+
+          comm.execute <<-EOH.gsub(/^ {12}/, '')
+            mkdir -p ~/.ssh
+            chmod 0700 ~/.ssh
+            cat '#{remote_path}' >> ~/.ssh/authorized_keys
+            chmod 0600 ~/.ssh/authorized_keys
+
+            # Remove the temporary file
+            rm -f '#{remote_path}'
+          EOH
         end
       end
     end
