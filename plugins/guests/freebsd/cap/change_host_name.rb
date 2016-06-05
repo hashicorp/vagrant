a4441109939ca24403ac255caa621016b291ee1b
Seth Vargo <sethvargo@gmail.com>
2016-06-05 13:14:02 -0400

    guests/freebsd: Configure hostname in a single command
    This updates freebsd to set the hostname in a single command and prepend
    the hostname to the /etc/hosts file.

--- plugins/guests/freebsd/cap/change_host_name.rb
+++ plugins/guests/freebsd/cap/change_host_name.rb
@@ -3,9 +3,27 @@ module VagrantPlugins
     module Cap
       class ChangeHostName
         def self.change_host_name(machine, name)
-          if !machine.communicate.test("hostname -f | grep '^#{name}$' || hostname -s | grep '^#{name}$'", {shell: "sh"})
-            machine.communicate.sudo("sed -i '' 's/^hostname=.*$/hostname=\"#{name}\"/' /etc/rc.conf", {shell: "sh"})
-            machine.communicate.sudo("hostname #{name}", {shell: "sh"})
+          options = { shell: "sh" }
+          comm = machine.communicate
+
+          if !comm.test("hostname -f | grep -w '#{name}' || hostname -s | grep -w '#{name}'", options)
+            basename = name.split(".", 2)[0]
+            command = <<-EOH.gsub(/^ {14}/, '')
+              # Set the hostname
+              hostname '#{name}'
+              sed -i '' 's/^hostname=.*$/hostname=\"#{name}\"/' /etc/rc.conf
+
+              # Remove comments and blank lines from /etc/hosts
+              sed -i'' -e 's/#.*$//' /etc/hosts
+              sed -i'' -e '/^$/d' /etc/hosts
+
+              # Prepend ourselves to /etc/hosts
+              grep -w '#{name}' /etc/hosts || {
+                echo -e '127.0.0.1\\t#{name}\\t#{basename}' | cat - /etc/hosts > /tmp/tmp-hosts
+                mv /tmp/tmp-hosts /etc/hosts
+              }
+            EOH
+            comm.sudo(command, options)
           end
         end
       end
