5e5a91956c37fa9b0690d928d0689913bb07d04b
Seth Vargo <sethvargo@gmail.com>
2016-06-05 16:56:05 -0400

    guests/photon: Use require_relative and double quotes

--- plugins/guests/photon/guest.rb
+++ plugins/guests/photon/guest.rb
@@ -1,6 +1,6 @@
 module VagrantPlugins
   module GuestPhoton
-    class Guest < Vagrant.plugin('2', :guest)
+    class Guest < Vagrant.plugin("2", :guest)
       def detect?(machine)
         machine.communicate.test("cat /etc/photon-release | grep 'VMware Photon Linux'")
       end
