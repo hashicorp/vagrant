90b62a0943be336cb59b874957230e8d94a791ed
Seth Vargo <sethvargo@gmail.com>
2016-06-05 15:14:59 -0400

    guests/mint: Use require_relative in plugin

--- plugins/guests/mint/plugin.rb
+++ plugins/guests/mint/plugin.rb
@@ -7,7 +7,7 @@ module VagrantPlugins
       description "Mint guest support."
 
       guest("mint", "ubuntu") do
-        require File.expand_path("../guest", __FILE__)
+        require_relative "guest"
         Guest
       end
     end
