e925f3793ae8b2a7e0a6bde2ab1f477c892c332c
Seth Vargo <sethvargo@gmail.com>
2016-06-06 18:22:17 -0400

    provisioners/chef: Install in one command

--- plugins/provisioners/chef/cap/redhat/chef_install.rb
+++ plugins/provisioners/chef/cap/redhat/chef_install.rb
@@ -6,20 +6,16 @@ module VagrantPlugins
       module Redhat
         module ChefInstall
           def self.chef_install(machine, project, version, channel, options = {})
-            if dnf?(machine)
-              machine.communicate.sudo("dnf install -y -q curl")
-            else
-              machine.communicate.sudo("yum install -y -q curl")
-            end
+            machine.communicate.sudo <<-EOH.gsub(/^ {14}/, '')
+              if command -v dnf; then
+                dnf -y install curl
+              else
+                yum -y install curl
+              fi
+            EOH
 
             command = Omnibus.sh_command(project, version, channel, options)
             machine.communicate.sudo(command)
-          end
-
-          protected
-
-          def self.dnf?(machine)
-            machine.communicate.test("/usr/bin/which -s dnf")
           end
         end
       end
