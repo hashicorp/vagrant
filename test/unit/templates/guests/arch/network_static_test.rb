233120eebda5093dd86fada243b2a6abd1a6bd20
Seth Vargo <sethvargo@gmail.com>
2016-06-06 12:34:45 -0400

    templates/general: Fix failing tests

--- test/unit/templates/guests/arch/network_static_test.rb
+++ test/unit/templates/guests/arch/network_static_test.rb
@@ -7,13 +7,13 @@ describe "templates/guests/arch/network_static" do
 
   it "renders the template" do
     result = Vagrant::Util::TemplateRenderer.render(template, options: {
-      device: "en0",
+      device: "eth1",
       ip:     "1.1.1.1",
     })
     expect(result).to eq <<-EOH.gsub(/^ {6}/, "")
       Connection=ethernet
       Description='A basic static ethernet connection'
-      Interface=en0
+      Interface=eth1
       IP=static
       Address=('1.1.1.1/24')
     EOH
@@ -21,14 +21,14 @@ describe "templates/guests/arch/network_static" do
 
   it "includes the gateway" do
     result = Vagrant::Util::TemplateRenderer.render(template, options: {
-      device:  "en0",
+      device:  "eth1",
       ip:      "1.1.1.1",
       gateway: "1.2.3.4",
     })
     expect(result).to eq <<-EOH.gsub(/^ {6}/, "")
       Connection=ethernet
       Description='A basic static ethernet connection'
-      Interface=en0
+      Interface=eth1
       IP=static
       Address=('1.1.1.1/24')
       Gateway='1.2.3.4'
