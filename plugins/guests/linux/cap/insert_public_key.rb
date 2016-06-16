d1be7ae84a9858095054124f891cea487194d81c
Jonathan <jonnywilliamson@gmail.com>
2016-06-16 11:59:11 +0100

    Better method for string concatenation
    Never developed in Ruby before. I have only just discovered that apparently `<<` is a better method of string concatenation.

--- plugins/guests/linux/cap/insert_public_key.rb
+++ plugins/guests/linux/cap/insert_public_key.rb
@@ -4,7 +4,7 @@ module VagrantPlugins
       class InsertPublicKey
         def self.insert_public_key(machine, contents)
           comm = machine.communicate
-          contents = contents.chomp+"\n"
+          contents = contents.chomp << "\n"
 
           remote_path = "/tmp/vagrant-authorized-keys-#{Time.now.to_i}"
           Tempfile.open("vagrant-linux-insert-public-key") do |f|
