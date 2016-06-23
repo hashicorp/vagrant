1101ce0f759e2defa2499bea1f2af7fee36f28c4
Seth Vargo <sethvargo@gmail.com>
2016-06-22 19:31:12 -0700

    guests/rhel: Use FQDN for hostname
    Refs: https://lukas.zapletalovi.com/2011/12/setting-hostname-properly-in-fedora-and.html

--- plugins/guests/fedora/cap/change_host_name.rb
+++ plugins/guests/fedora/cap/change_host_name.rb
@@ -8,10 +8,10 @@ module VagrantPlugins
           if !comm.test("hostname -f | grep '^#{name}$'", sudo: false)
             basename = name.split(".", 2)[0]
             comm.sudo <<-EOH.gsub(/^ {14}/, "")
-              echo '#{basename}' > /etc/hostname
+              echo '#{name}' > /etc/hostname
               hostname -F /etc/hostname
-              hostnamectl set-hostname --static '#{basename}'
-              hostnamectl set-hostname --transient '#{basename}'
+              hostnamectl set-hostname --static '#{name}'
+              hostnamectl set-hostname --transient '#{name}'
 
               # Remove comments and blank lines from /etc/hosts
               sed -i'' -e 's/#.*$//' -e '/^$/d' /etc/hosts
