10b12ce393fa3aef06f027b4532d6a61c18a94bb
Seth Vargo <sethvargo@gmail.com>
2016-06-22 18:31:36 -0700

    guests/arch: Search for FQDN hostname

--- plugins/guests/arch/cap/change_host_name.rb
+++ plugins/guests/arch/cap/change_host_name.rb
@@ -5,19 +5,19 @@ module VagrantPlugins
         def self.change_host_name(machine, name)
           comm = machine.communicate
 
-          if !comm.test("hostname | grep -w '#{name}'")
+          if !comm.test("hostname -f | grep '^#{name}$'")
             basename = name.split(".", 2)[0]
-            comm.sudo <<-EOH
-hostnamectl set-hostname '#{name}'
+            comm.sudo <<-EOH.gsub(/^ {14}/, "")
+              hostnamectl set-hostname '#{basename}'
 
-# Remove comments and blank lines from /etc/hosts
-sed -i'' -e 's/#.*$//' -e '/^$/d' /etc/hosts
+              # Remove comments and blank lines from /etc/hosts
+              sed -i'' -e 's/#.*$//' -e '/^$/d' /etc/hosts
 
-# Prepend ourselves to /etc/hosts
-grep -w '#{name}' /etc/hosts || {
-  sed -i'' '1i 127.0.0.1\\t#{name}\\t#{basename}' /etc/hosts
-}
-EOH
+              # Prepend ourselves to /etc/hosts
+              grep -w '#{name}' /etc/hosts || {
+                sed -i'' '1i 127.0.0.1\\t#{name}\\t#{basename}' /etc/hosts
+              }
+            EOH
           end
         end
       end
