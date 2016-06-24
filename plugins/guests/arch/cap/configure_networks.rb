0e6f196c8867796b5f3bb15e91b1166569721da1
Seth Vargo <sethvargo@gmail.com>
2016-06-24 19:16:44 -0400

    guests/arch: compute netmask
    Arch expects /24 for netmask, but users usually specify as a netmask
    block like 255.255.255.0. This automatically converts it.

--- plugins/guests/arch/cap/configure_networks.rb
+++ plugins/guests/arch/cap/configure_networks.rb
@@ -1,3 +1,5 @@
+require "ipaddr"
+require "socket"
 require "tempfile"
 
 require_relative "../../../../lib/vagrant/util/template_renderer"
@@ -16,6 +18,13 @@ module VagrantPlugins
 
           networks.each.with_index do |network, i|
             network[:device] = interfaces[network[:interface]]
+
+            # Arch expects netmasks to be in the "24" or "64", but users may
+            # specify IPV4 netmasks like "255.255.255.0". This magic converts
+            # the netmask to the proper value.
+            if network[:netmask] && network[:netmask].to_s.include?(".")
+              network[:netmask] = (32-Math.log2((IPAddr.new(network[:netmask], Socket::AF_INET).to_i^0xffffffff)+1)).to_i
+            end
 
             entry = TemplateRenderer.render("guests/arch/network_#{network[:type]}",
               options: network,
