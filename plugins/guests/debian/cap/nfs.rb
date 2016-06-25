9134172ce6d6bc6a93cf3fc1d045e1fe29c0ad1a
Seth Vargo <sethvargo@gmail.com>
2016-06-24 21:17:45 -0400

    guests/debian: Move NFS into same file

--- plugins/guests/debian/cap/nfs.rb
+++ plugins/guests/debian/cap/nfs.rb
@@ -1,10 +1,11 @@
 module VagrantPlugins
   module GuestDebian
     module Cap
-      class NFSClient
+      class NFS
         def self.nfs_client_install(machine)
           comm = machine.communicate
           comm.sudo <<-EOH.gsub(/^ {12}/, '')
+            set -e
             apt-get -yqq update
             apt-get -yqq install nfs-common portmap
           EOH
