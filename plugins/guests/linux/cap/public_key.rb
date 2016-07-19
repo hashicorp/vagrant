374deb1cdd11ff49557da9afa80f7da5f10ba0a3
hendrenj <jay.hendren@Colorado.EDU>
2016-07-19 11:22:35 -0600

    fixes #7610

--- plugins/guests/linux/cap/public_key.rb
+++ plugins/guests/linux/cap/public_key.rb
@@ -54,6 +54,7 @@ module VagrantPlugins
             if test -f ~/.ssh/authorized_keys; then
               grep -v -x -f '#{remote_path}' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp
               mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys
+              chmod 0600 ~/.ssh/authorized_keys
             fi
 
             rm -f '#{remote_path}'
