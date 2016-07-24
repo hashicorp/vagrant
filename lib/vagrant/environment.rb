188a7dab0c904d04f6ae4390f0367922135b01e2
Carlos Rodrigues <cefrodrigues@gmail.com>
2016-07-24 18:40:09 +0100

    Another try, without string substitution

--- lib/vagrant/environment.rb
+++ lib/vagrant/environment.rb
@@ -157,21 +157,16 @@ module Vagrant
       end
 
       # Setup the local data directory. If a configuration path is given,
-      # it is expanded relative to the root path or to the working directory
-      # depending on whether it starts with the "{}" placeholder. Otherwise,
-      # we use the default which is expanded relative to the root path.
+      # it is expanded relative to the root path. Otherwise, we use the
+      # default (which is also expanded relative to the root path).
       if ENV.has_key?("VAGRANT_DOTFILE_PATH") && !opts[:child]
-        dotfile_path = ENV["VAGRANT_DOTFILE_PATH"]
-        if dotfile_path.match(/^\{\}/) && !root_path.nil?
-          opts[:local_data_path] = File.join(root_path, dotfile_path.sub(/^\{\}/, ''))
-        else
-          opts[:local_data_path] = dotfile_path
-        end
+        opts[:local_data_path] ||= root_path.join(ENV["VAGRANT_DOTFILE_PATH"]) if !root_path.nil?
       end
       opts[:local_data_path] ||= root_path.join(DEFAULT_LOCAL_DATA) if !root_path.nil?
       if opts[:local_data_path]
         @local_data_path = Pathname.new(File.expand_path(opts[:local_data_path], @cwd))
       end
+      @logger.debug("Effective local data path: #{@local_data_path}")
 
       # If we have a root path, load the ".vagrantplugins" file.
       if root_path
