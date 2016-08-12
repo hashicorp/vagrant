ce3329e1847e9696cbb4c8a49c08661ec9376c31
Chris Roberts <code@chrisroberts.org>
2016-08-11 17:01:36 -0700

    guests/linux: Always order discovered network interfaces

--- plugins/guests/linux/cap/network_interfaces.rb
+++ plugins/guests/linux/cap/network_interfaces.rb
@@ -2,6 +2,8 @@ module VagrantPlugins
   module GuestLinux
     module Cap
       class NetworkInterfaces
+        @@logger = Log4r::Logger.new("vagrant::guest::linux::network_interfaces")
+
         # Get network interfaces as a list. The result will be something like:
         #
         #   eth0\nenp0s8\nenp0s9
@@ -12,7 +14,21 @@ module VagrantPlugins
           machine.communicate.sudo("#{path} -o -0 addr | grep -v LOOPBACK | awk '{print $2}' | sed 's/://'") do |type, data|
             s << data if type == :stdout
           end
-          s.split("\n")
+          ifaces = s.split("\n")
+          @@logger.debug("Unsorted list: #{ifaces.inspect}")
+          # Break out integers from strings and sort the arrays to provide
+          # a natural sort for the interface names
+          ifaces = ifaces.map do |iface|
+            iface.scan(/(.+?)(\d+)/).flatten.map do |iface_part|
+              if iface_part.to_i.to_s == iface_part
+                iface_part.to_i
+              else
+                iface_part
+              end
+            end
+          end.sort.map(&:join)
+          @@logger.debug("Sorted list: #{ifaces.inspect}")
+          ifaces
         end
       end
     end
