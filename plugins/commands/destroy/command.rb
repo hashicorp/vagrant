93ffcb1f575b20f3ed81df6805a9405d9bc4694f
Taliesin Sisson <taliesins@yahoo.com>
2016-12-12 17:13:44 +0000

    Add parallel for destroy

--- plugins/commands/destroy/command.rb
+++ plugins/commands/destroy/command.rb
@@ -15,6 +15,11 @@ module VagrantPlugins
           o.separator "Options:"
           o.separator ""
 
+          o.on("--[no-]parallel",
+               "Enable or disable parallelism if provider supports it") do |parallel|
+            options[:parallel] = parallel
+          end
+
           o.on("-f", "--force", "Destroy without confirmation.") do |f|
             options[:force] = f
           end
@@ -25,25 +30,66 @@ module VagrantPlugins
         return if !argv
 
         @logger.debug("'Destroy' each target VM...")
-        declined = 0
-        total    = 0
-        with_target_vms(argv, reverse: true) do |vm|
-          action_env = vm.action(
-            :destroy, force_confirm_destroy: options[:force])
 
-          total    += 1
-          declined += 1 if action_env.key?(:force_confirm_destroy_result) &&
-            action_env[:force_confirm_destroy_result] == false
+        # Get the names of the machines we want to bring up
+        names = argv
+        if names.empty?
+          autostart = false
+          @env.vagrantfile.machine_names_and_options.each do |n, o|
+            autostart = true if o.key?(:autostart)
+            o[:autostart] = true if !o.key?(:autostart)
+            names << n.to_s if o[:autostart]
+          end
+
+          # If we have an autostart key but no names, it means that
+          # all machines are autostart: false and we don't start anything.
+          names = nil if autostart && names.empty?
         end
 
-        # Nothing was declined
-        return 0 if declined == 0
+        # Build up the batch job of what we'll do
+        machines = []
+        if names
+          # If we're installing providers, then do that. We don't
+          # parallelize this step because it is likely the same provider
+          # anyways.
+          if options[:install_provider]
+            install_providers(names, provider: options[:provider])
+          end
 
-        # Everything was declined
-        return 1 if declined == total
+          @env.batch(options[:parallel]) do |batch|
+            with_target_vms(names, reverse: true, provider: options[:provider]) do |machine|
+              @env.ui.info(I18n.t(
+                "vagrant.commands.destroy.destroying",
+                name: machine.name,
+                provider: machine.provider_name))
 
-        # Some was declined
-        return 2
+              machines << machine
+
+              batch.action(machine, :destroy, force_confirm_destroy: options[:force])
+            end
+          end
+        end
+
+        if machines.empty?
+          @env.ui.info(I18n.t("vagrant.destroy_no_machines"))
+          return 0
+        end
+
+        # Output the post-up messages that we have, if any
+        machines.each do |m|
+          next if !m.config.vm.post_destroy_message
+          next if m.config.vm.post_destroy_message == ""
+
+          # Add a newline to separate things.
+          @env.ui.info("", prefix: false)
+
+          m.ui.success(I18n.t(
+            "vagrant.post_destroy_message",
+            name: m.name.to_s,
+            message: m.config.vm.post_destroy_message))
+        end
+
+        return 0
       end
     end
   end
