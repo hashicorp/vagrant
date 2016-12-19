ae96a527ea26f96f23351b26053b1db71a0515c4
Gerben Meijer <infernix@gmail.com>
2016-12-19 17:21:07 +0100

    Fix grepping for GUEST_DETECTION_NAME with spaces
    Some GUEST_DETECTION_NAME definitions use white spaces but the grep
    commands do not quote the variable while checking. That causes guest
    detection to fail for those OSes since grep uses arguments after the
    witespace as files instead of parsing stdin.

--- plugins/guests/linux/guest.rb
+++ plugins/guests/linux/guest.rb
@@ -7,13 +7,13 @@ module VagrantPlugins
       def detect?(machine)
         machine.communicate.test <<-EOH.gsub(/^ */, '')
           if test -r /etc/os-release; then
-            source /etc/os-release && test x#{self.class.const_get(:GUEST_DETECTION_NAME)} = x$ID && exit
+            source /etc/os-release && test "x#{self.class.const_get(:GUEST_DETECTION_NAME)}" = "x$ID" && exit
           fi
           if test -x /usr/bin/lsb_release; then
-            /usr/bin/lsb_release -i 2>/dev/null | grep -qi #{self.class.const_get(:GUEST_DETECTION_NAME)} && exit
+            /usr/bin/lsb_release -i 2>/dev/null | grep -qi "#{self.class.const_get(:GUEST_DETECTION_NAME)}" && exit
           fi
           if test -r /etc/issue; then
-            cat /etc/issue | grep -qi #{self.class.const_get(:GUEST_DETECTION_NAME)} && exit
+            cat /etc/issue | grep -qi "#{self.class.const_get(:GUEST_DETECTION_NAME)}" && exit
           fi
           exit 1
         EOH
