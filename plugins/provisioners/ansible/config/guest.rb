b84936a854d9b8ccff43da4b85c3d7c1979ef8de
James Carr <james.r.carr@gmail.com>
2017-01-06 07:19:44 -0600

    Allows optional pip_args option to pass additional arguments to pip when the install_mode is pip

--- plugins/provisioners/ansible/config/guest.rb
+++ plugins/provisioners/ansible/config/guest.rb
@@ -10,6 +10,7 @@ module VagrantPlugins
         attr_accessor :tmp_path
         attr_accessor :install
         attr_accessor :install_mode
+        attr_accessor :pip_args
         attr_accessor :version
 
         def initialize
@@ -17,6 +18,7 @@ module VagrantPlugins
 
           @install           = UNSET_VALUE
           @install_mode      = UNSET_VALUE
+          @pip_args          = UNSET_VALUE
           @provisioning_path = UNSET_VALUE
           @tmp_path          = UNSET_VALUE
           @version           = UNSET_VALUE
@@ -27,6 +29,7 @@ module VagrantPlugins
 
           @install           = true                   if @install           == UNSET_VALUE
           @install_mode      = :default               if @install_mode      == UNSET_VALUE
+          @pip_args          = ""                     if @pip_args          == UNSET_VALUE
           @provisioning_path = "/vagrant"             if provisioning_path  == UNSET_VALUE
           @tmp_path          = "/tmp/vagrant-ansible" if tmp_path           == UNSET_VALUE
           @version           = ""                     if @version           == UNSET_VALUE
