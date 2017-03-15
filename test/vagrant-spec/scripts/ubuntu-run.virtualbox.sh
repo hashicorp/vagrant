ce2a1ac634cfd38cd03cf2bb339f503aa1b35312
Chris Roberts <code@chrisroberts.org>
2017-03-15 09:30:57 -0700

    Adjustable host memory. Allow custom command arguments.

--- test/vagrant-spec/scripts/ubuntu-run.virtualbox.sh
+++ test/vagrant-spec/scripts/ubuntu-run.virtualbox.sh
@@ -2,7 +2,7 @@
 set -x
 
 export VAGRANT_SPEC_BOX="${VAGRANT_SPEC_BOX}"
-vagrant vagrant-spec /vagrant/test/vagrant-spec/configs/vagrant-spec.config.virtualbox.rb
+vagrant vagrant-spec ${VAGRANT_SPEC_ARGS} /vagrant/test/vagrant-spec/configs/vagrant-spec.config.virtualbox.rb
 result=$?
 
 exit $result
