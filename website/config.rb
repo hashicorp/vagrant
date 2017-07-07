df62f27e278e76a8e7933d79918fab233115394c
Brian Cain <bcain@hashicorp.com>
2017-07-07 16:22:16 -0700

    Update vagrant website version to 1.9.7

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.vagrantup.com/"
 
 activate :hashicorp do |h|
   h.name         = "vagrant"
-  h.version      = "1.9.6"
+  h.version      = "1.9.7"
   h.github_slug  = "mitchellh/vagrant"
   h.website_root = "website"
 end
