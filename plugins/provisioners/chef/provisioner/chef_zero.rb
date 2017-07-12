dd3d78265940b5764ebcbacd8fcc007d19c37291
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2017-07-12 15:27:10 +0200

    Added exception if chef.node_path is defined on Vagrantfile but the directory does not exist locally

--- plugins/provisioners/chef/provisioner/chef_zero.rb
+++ plugins/provisioners/chef/provisioner/chef_zero.rb
@@ -35,6 +35,7 @@ module VagrantPlugins
           upload_encrypted_data_bag_secret
           setup_json
           setup_zero_config
+          verify_chef_nodes_folder
           run_chef_zero
           delete_encrypted_data_bag_secret
         end
@@ -98,6 +99,12 @@ module VagrantPlugins
             if !@machine.communicate.test("test -d #{folder}", sudo: true)
               raise ChefError, :missing_shared_folders
             end
+          end
+        end
+
+        def verify_chef_nodes_folder
+          if not File.exists? @config.nodes_path[0][1]
+            raise ChefError, :missing_chef_node_folder
           end
         end
 
