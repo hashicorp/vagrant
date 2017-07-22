c9b65bd129b17a2dae9b152c9b94e1ae89985695
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2017-07-22 11:00:05 +0200

    produce error when node_path is defined but the directory does not exist on disk locally

--- plugins/provisioners/chef/config/chef_zero.rb
+++ plugins/provisioners/chef/config/chef_zero.rb
@@ -79,6 +79,13 @@ module VagrantPlugins
 
           if !present?(Array(nodes_path))
             errors << I18n.t("vagrant.config.chef.nodes_path_empty")
+          else
+            missing_paths = Array.new
+            nodes_path.each { |dir| missing_paths << dir[1] if !File.exists? dir[1] }
+            # If it exists at least one path on disk it's ok for Chef provisioning
+            if missing_paths.size == nodes_path.size
+              errors << I18n.t("vagrant.config.chef.nodes_path_missing", path: missing_paths.to_s)
+            end
           end
 
           if environment && environments_path.empty?
