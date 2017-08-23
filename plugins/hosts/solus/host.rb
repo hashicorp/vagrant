1ed95e3869204d0ac6d1a93830f51c152ee5db47
matthew Andersen <matthew@barelystable.com>
2017-08-22 22:55:48 -0700

    Detect Solus using more standards compliant file

--- plugins/hosts/solus/host.rb
+++ plugins/hosts/solus/host.rb
@@ -4,7 +4,9 @@ module VagrantPlugins
   module HostSolus
     class Host < Vagrant.plugin("2", :host)
       def detect?(env)
-        File.exist?("/etc/solus-release")
+      	if File.exist?("/etc/os-release")
+      		File.readlines("/etc/os-release").grep(/^ID=["']{0,1}solus["']{0,1}$/).any?
+      	end
       end
     end
   end
