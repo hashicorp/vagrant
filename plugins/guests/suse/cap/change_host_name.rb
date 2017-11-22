a139dd2b59e05b9e159d9f3f6fbc232d505256a6
Matthias Haag <maha@if-sat.de>
2017-11-22 11:09:51 +0100

    use basename for setting hostname

--- plugins/guests/suse/cap/change_host_name.rb
+++ plugins/guests/suse/cap/change_host_name.rb
@@ -8,8 +8,8 @@ module VagrantPlugins
           if !comm.test("hostname -f | grep '^#{name}$'", sudo: false)
             basename = name.split(".", 2)[0]
             comm.sudo <<-EOH.gsub(/^ {14}/, '')
-              echo '#{name}' > /etc/HOSTNAME
-              hostname '#{name}'
+              echo '#{basename}' > /etc/HOSTNAME
+              hostname '#{basename}'
 
               # Prepend ourselves to /etc/hosts
               grep -w '#{name}' /etc/hosts || {
