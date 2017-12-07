d4d4ed64736e99e15159e33751078b1703ca94bd
Brian Cain <bcain@hashicorp.com>
2017-12-07 11:02:43 -0800

    (#9137) Exit 0 if vagrant destroy finds no running vms
    This commit reverts the behavior of `vagrant destroy` to exit 0 if no
    running vms were found when the destroy command is run.

--- plugins/commands/destroy/command.rb
+++ plugins/commands/destroy/command.rb
@@ -60,6 +60,10 @@ module VagrantPlugins
         # Nothing was declined
         return 0 if declined == 0
 
+        # Everything was declined, and all states are `not_created`
+        return 0 if declined == machines.length &&
+                    declined == init_states.values.count(:not_created)
+
         # Everything was declined, state was not changed
         return 1 if declined == machines.length
 
