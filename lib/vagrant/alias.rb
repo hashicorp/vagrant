42f72e1099aee7f37b699c273b28c5bdc0566726
Zachary Flower <zach@ninjaninja.net>
2018-02-25 22:21:16 -0700

    some light changes per PR feedback

--- lib/vagrant/alias.rb
+++ lib/vagrant/alias.rb
@@ -32,7 +32,7 @@ module Vagrant
       keyword, command = line.split("=", 2).collect(&:strip)
 
       # validate the keyword
-      if keyword.match(/[\s]/i)
+      if keyword.match(/\s/i)
         raise Errors::AliasInvalidError, alias: line, message: "Alias keywords must not contain any whitespace."
       end
 
@@ -45,7 +45,7 @@ module Vagrant
         lambda do |args|
           # directly execute shell commands
           if command.start_with?("!")
-            return exec "#{command[1..-1]} #{args.join(" ")}".strip
+            return Util::SafeExec.exec "#{command[1..-1]} #{args.join(" ")}".strip
           end
 
           return CLI.new(command.split.concat(args), @env).execute
