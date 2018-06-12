98c6903e9c757af91e1c59a51d409631b2e52a7b
Chris Roberts <croberts@hashicorp.com>
2018-06-12 11:56:37 -0700

    Fix path variable name. Return process result.

--- plugins/hosts/windows/cap/ssh.rb
+++ plugins/hosts/windows/cap/ssh.rb
@@ -10,14 +10,15 @@ module VagrantPlugins
         def self.set_ssh_key_permissions(env, key_path)
           script_path = Host.scripts_path.join("set_ssh_key_permissions.ps1")
           result = Vagrant::Util::PowerShell.execute(
-            script_path.to_s, path.to_s,
-            module_path: Host.module_path.to_s
+            script_path.to_s, key_path.to_s,
+            module_path: Host.modules_path.to_s
           )
           if result.exit_code != 0
             raise Vagrant::Errors::PowerShellError,
               script: script_path,
               stderr: result.stderr
           end
+          result
         end
       end
     end
