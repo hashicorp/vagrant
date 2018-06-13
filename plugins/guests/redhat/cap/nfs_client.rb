362a099ae44a76c5ca70c483e6f08ee988e26d42
Rahul Bajaj <rahulrb0509@gmail.com>
2018-06-13 18:07:29 +0530

    Fix the name for nfs-utils-lib package in fedora28

--- plugins/guests/redhat/cap/nfs_client.rb
+++ plugins/guests/redhat/cap/nfs_client.rb
@@ -5,7 +5,7 @@ module VagrantPlugins
         def self.nfs_client_install(machine)
           machine.communicate.sudo <<-EOH.gsub(/^ {12}/, '')
             if command -v dnf; then
-              dnf -y install nfs-utils nfs-utils-lib portmap
+              dnf -y install nfs-utils libnfs-utils portmap
             else
               yum -y install nfs-utils nfs-utils-lib portmap
             fi
