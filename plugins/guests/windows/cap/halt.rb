a951e8d90fcd994be83b21dad04a1c6da14f6ca7
le-garff-yoann <pe.weeble@yahoo.fr>
2018-07-25 23:46:09 +0200

    Make shutdown work on Nano Server

--- plugins/guests/windows/cap/halt.rb
+++ plugins/guests/windows/cap/halt.rb
@@ -8,7 +8,11 @@ module VagrantPlugins
           machine.communicate.execute("shutdown -a", error_check: false)
 
           # Force shutdown the machine now
-          machine.communicate.execute("shutdown /s /t 1 /c \"Vagrant Halt\" /f /d p:4:1")
+          begin
+            machine.communicate.execute("shutdown /s /t 1 /c \"Vagrant Halt\" /f /d p:4:1")
+          rescue Vagrant::Errors::VagrantError
+            machine.communicate.execute("shutdown /s /t 1 /c \"Vagrant Halt\" /f")
+          end
         end
       end
     end
