b1a0fa628f47f704a582534a0ce670b8ce70cdab
Brian Cain <bcain@hashicorp.com>
2018-09-05 12:56:17 -0700

    Add subcommand docstring to powershell

--- plugins/commands/powershell/command.rb
+++ plugins/commands/powershell/command.rb
@@ -17,6 +17,9 @@ module VagrantPlugins
           o.banner = "Usage: vagrant powershell [-- extra powershell args]"
 
           o.separator ""
+          o.separator "Opens a remote PowerShell session to the machine if it"
+          o.separator "supports powershell remoting."
+          o.separator ""
           o.separator "Options:"
           o.separator ""
 
