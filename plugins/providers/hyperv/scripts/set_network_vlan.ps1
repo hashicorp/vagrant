1ece44ca84f682ee734fb79ebd0d8526b6a3ef6d
Chris Roberts <croberts@hashicorp.com>
2018-11-02 10:42:28 -0700

    Remove module loading from vlan script

--- plugins/providers/hyperv/scripts/set_network_vlan.ps1
+++ plugins/providers/hyperv/scripts/set_network_vlan.ps1
@@ -7,12 +7,6 @@ param (
     [int]$VlanId
 )
 
-# Include the following modules
-$presentDir = Split-Path -parent $PSCommandPath
-$modules = @()
-$modules += $presentDir + "\utils\write_messages.ps1"
-forEach ($module in $modules) { . $module }
-
 try {
   $vm = Hyper-V\Get-VM -Id $VmId -ErrorAction "stop"
   Hyper-V\Set-VMNetworkAdapterVlan $vm -Access -Vlanid $VlanId
