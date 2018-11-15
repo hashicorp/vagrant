30d2e52c46df1200f9cc0b1e0c4ca55f204e55d4
Chris Roberts <croberts@hashicorp.com>
2018-11-15 15:20:54 -0800

    Update website version to 2.2.1

--- website/config.rb
+++ website/config.rb
@@ -5,7 +5,7 @@ set :vmware_utility_version, "1.0.5"
 
 activate :hashicorp do |h|
   h.name         = "vagrant"
-  h.version      = "2.2.0"
+  h.version      = "2.2.1"
   h.github_slug  = "hashicorp/vagrant"
   h.website_root = "website"
 end
