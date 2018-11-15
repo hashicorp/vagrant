e86824e098884f2edf66b03729260bf587174c6c
Chris Roberts <croberts@hashicorp.com>
2018-11-15 11:05:51 -0800

    Move common virtualbox 5 driver tests into shared examples

--- test/unit/plugins/providers/virtualbox/base.rb
+++ test/unit/plugins/providers/virtualbox/base.rb
@@ -2,3 +2,4 @@
 
 require_relative "../../../base"
 require_relative "support/shared/virtualbox_driver_version_4_x_examples"
+require_relative "support/shared/virtualbox_driver_version_5_x_examples"
