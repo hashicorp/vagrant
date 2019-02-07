93a41bd104caa6448ebcb2ce29f3168dd107db52
Alessandro De Blasis <alex@deblasis.net>
2019-02-07 12:51:21 +0000

    coreos: changes to network reconfigured etcd via deprecated CoreOS services
    This commit removes code that relied on deprecated systemd services
    etcd-member.service is now the only systemd service available, with etcd v2 removed since June 2018
    https://coreos.com/etcd/docs/latest/os-faq.html
    https://coreos.com/blog/toward-etcd-v3-in-container-linux.html
    Also, this change promotes separation of concerns.
    Changes to network configuration should be applied to etcd via the provided APIs.
    See Container Linux Config/Ignition
    
    Fixes #9573

--- plugins/guests/coreos/cap/configure_networks.rb
+++ plugins/guests/coreos/cap/configure_networks.rb
@@ -16,36 +16,11 @@ module VagrantPlugins
               interfaces = result.split("\n")
             end
 
-            primary_machine_config = machine.env.active_machines.first
-            primary_machine = machine.env.machine(*primary_machine_config, true)
-
-            primary_machine_ip = get_ip(primary_machine)
-            current_ip = get_ip(machine)
-            if current_ip == primary_machine_ip
-              entry = TemplateRenderer.render("guests/coreos/etcd.service", options: {
-                my_ip: current_ip,
-              })
-            else
-              connection_string = "#{primary_machine_ip}:7001"
-              entry = TemplateRenderer.render("guests/coreos/etcd.service", options: {
-                connection_string: connection_string,
-                my_ip: current_ip,
-              })
-            end
-
-            Tempfile.open("vagrant-coreos-configure-networks") do |f|
-              f.binmode
-              f.write(entry)
-              f.fsync
-              f.close
-              comm.upload(f.path, "/tmp/etcd-cluster.service")
-            end
-
             # Build a list of commands
             commands = []
 
             # Stop default systemd
-            commands << "systemctl stop etcd"
+            #commands << "systemctl stop etcd"
 
             # Configure interfaces
             # FIXME: fix matching of interfaces with IP addresses
@@ -54,13 +29,6 @@ module VagrantPlugins
               commands << "ifconfig #{iface} #{network[:ip]} netmask #{network[:netmask]}".squeeze(" ")
             end
 
-            commands << <<-EOH.gsub(/^ {14}/, '')
-              mv /tmp/etcd-cluster.service /media/state/units/
-              systemctl restart local-enable.service
-
-              # Restart default etcd
-              systemctl start etcd
-            EOH
 
             # Run all network configuration commands in one communicator session.
             comm.sudo(commands.join("\n"))
