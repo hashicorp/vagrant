3b039443313962a22f3343a2c3ef9725c7b41691
Tim Schumacher <timschumi@gmx.de>
2019-07-17 16:51:36 +0200

    alpine: Replace capability strings with symbols
    Those were introduced upstream in b29864f45097.

--- plugins/guests/alpine/plugin.rb
+++ plugins/guests/alpine/plugin.rb
@@ -6,42 +6,42 @@ module VagrantPlugins
       name 'Alpine guest'
       description 'Alpine Linux guest support.'
 
-      guest('alpine', 'linux') do
+      guest(:alpine, :linux) do
         require File.expand_path('../guest', __FILE__)
         Guest
       end
 
-      guest_capability('alpine', 'configure_networks') do
+      guest_capability(:alpine, :configure_networks) do
         require_relative 'cap/configure_networks'
         Cap::ConfigureNetworks
       end
 
-      guest_capability('alpine', 'halt') do
+      guest_capability(:alpine, :halt) do
         require_relative 'cap/halt'
         Cap::Halt
       end
 
-      guest_capability('alpine', 'change_host_name') do
+      guest_capability(:alpine, :change_host_name) do
         require_relative 'cap/change_host_name'
         Cap::ChangeHostName
       end
 
-      guest_capability('alpine', 'nfs_client_install') do
+      guest_capability(:alpine, :nfs_client_install) do
         require_relative 'cap/nfs_client'
         Cap::NFSClient
       end
 
-      guest_capability('alpine', 'rsync_installed') do
+      guest_capability(:alpine, :rsync_installed) do
         require_relative 'cap/rsync'
         Cap::RSync
       end
 
-      guest_capability('alpine', 'rsync_install') do
+      guest_capability(:alpine, :rsync_install) do
         require_relative 'cap/rsync'
         Cap::RSync
       end
 
-      guest_capability('alpine', 'smb_install') do
+      guest_capability(:alpine, :smb_install) do
         require_relative 'cap/smb'
         Cap::SMB
       end
