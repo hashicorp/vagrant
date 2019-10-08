da15c1d1716e8620ac488aaae8c6eab4bfaf4782
Chris Roberts <croberts@hashicorp.com>
2019-10-08 16:41:07 -0700

    Add test coverage on host plugin updates for nfs and resove_host_path capabilities

--- test/unit/plugins/hosts/bsd/cap/nfs_test.rb
+++ test/unit/plugins/hosts/bsd/cap/nfs_test.rb
@@ -1,39 +1,48 @@
 require_relative "../../../../base"
 require_relative "../../../../../../plugins/hosts/bsd/cap/nfs"
-require_relative "../../../../../../lib/vagrant/util"
 
 describe VagrantPlugins::HostBSD::Cap::NFS do
 
   include_context "unit"
 
-  describe ".nfs_check_folders_for_apfs" do
-    it "should prefix host paths that are mounted in /System/Volumes/Data" do
-      output_from_df = <<-EOH
-Filesystem    512-blocks      Used Available Capacity iused      ifree %iused  Mounted on
-/dev/disk1s1   976490568 392813584 555082648    42% 1177049 4881275791    0%   /System/Volumes/Data
-EOH
-      expect(Vagrant::Util::Subprocess).to receive(:execute).and_return(
-        Vagrant::Util::Subprocess::Result.new(0, output_from_df, "")
-      )
+  describe ".nfs_export" do
+    let(:environment) { double("environment", host: host) }
+    let(:host) { double("host") }
+    let(:ui) { double("ui") }
+    let(:id) { "UUID" }
+    let(:ips) { [] }
+    let(:folders) { {} }
 
-      folders = {"/vagrant"=>{:hostpath=>"/Users/johndoe/vagrant",:bsd__nfs_options=>["rw"]}}
-      described_class.nfs_check_folders_for_apfs(folders)
-      expect(folders["/vagrant"][:hostpath]).to eq("/System/Volumes/Data/Users/johndoe/vagrant")
+    before do
+      allow(host).to receive(:capability).and_return("")
+      allow(Vagrant::Util::TemplateRenderer).to receive(:render).and_return("")
+      allow(described_class).to receive(:sleep)
+      allow(described_class).to receive(:nfs_cleanup)
+      allow(described_class).to receive(:system)
+      allow(File).to receive(:writable?).with("/etc/exports")
+      allow(ui).to receive(:info)
     end
 
-    it "should not prefix host paths that are mounted in elsewhere" do
-      output_from_df = <<-EOH
-Filesystem    512-blocks      Used Available Capacity iused      ifree %iused  Mounted on
-/dev/disk1s5   976490568  20634032 554201072     4%  481588 4881971252    0%   /
-EOH
-      expect(Vagrant::Util::Subprocess).to receive(:execute).and_return(
-        Vagrant::Util::Subprocess::Result.new(0, output_from_df, "")
-      )
-
-      folders = {"/vagrant"=>{:hostpath=>"/",:bsd__nfs_options=>["rw"]}}
-      described_class.nfs_check_folders_for_apfs(folders)
-      expect(folders["/vagrant"][:hostpath]).to eq("/")
+    it "should execute successfully when no folders are defined" do
+      expect { described_class.nfs_export(environment, ui, id, ips, folders) }.
+        not_to raise_error
     end
 
+    context "with single folder defined" do
+      let(:folders) {
+        {"/vagrant" => {
+          type: :nfs, guestpath: "/vagrant", hostpath: "/Users/vagrant/paths", disabled: false}}
+      }
+
+      it "should execute successfully" do
+        expect { described_class.nfs_export(environment, ui, id, ips, folders) }.
+          not_to raise_error
+      end
+
+      it "should resolve the host path" do
+        expect(host).to receive(:capability).with(:resolve_host_path, folders["/vagrant"][:hostpath]).and_return("")
+        described_class.nfs_export(environment, ui, id, ips, folders)
+      end
+    end
   end
 end
