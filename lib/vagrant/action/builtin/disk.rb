cef1bd47b3408c704c74e9ca0c471712ceb67753
Brian Cain <bcain@hashicorp.com>
2019-10-23 15:31:51 -0700

    Add initial disk builtin action

--- lib/vagrant/action/builtin/disk.rb
+++ lib/vagrant/action/builtin/disk.rb
@@ -9,9 +9,24 @@ module Vagrant
 
         def call(env)
           machine = env[:machine]
+          defined_disks = get_disks(machine, env)
+
+          # Check that provider plugin is installed for disk
+          # If not, log warning or error to user that disks won't be managed
 
           # Continue On
           @app.call(env)
+        end
+
+        def get_disks(machine, env)
+          return @_disks if @_disks
+
+          @_disks = []
+          @_disks = machine.config.vm.disks.map do |disk|
+            # initialize the disk provider??
+          end
+
+          @_disks
         end
       end
     end
