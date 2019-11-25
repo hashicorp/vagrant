c6ee1049aa5922ad783bc33dd71019da031aeabe
Jeff Bonhag <jbonhag@hashicorp.com>
2019-11-25 15:41:05 -0500

    Darwin: put each NFS export on its own line (#11216)
    This commit introduces a Darwin-specific template for NFS exports.  This is almost identical to the standard BSD template except it puts each NFS export on its own line.
    
    This resolves NFS issues discovered in macOS Catalina.

---
+++ test/unit/templates/nfs/exports_darwin_test.rb
@@ -0,0 +1,68 @@
+require_relative "../../base"
+
+require "vagrant/util/template_renderer"
+
+describe "templates/nfs/exports_darwin" do
+  let(:template) { "nfs/exports_darwin" }
+  let(:user) { "501" }
+  let(:uuid) { "UUID" }
+  let(:opts) { {:bsd__compiled_nfs_options => "-alldirs -mapall=501:80"} }
+  let(:ips) { ["172.16.0.2"] }
+
+  it "renders the template" do
+    result = Vagrant::Util::TemplateRenderer.render(template, {
+      user:    user,
+      uuid:    uuid,
+      folders: []
+    })
+    expect(result).to eq <<-EOH.gsub(/^ {6}/, "")
+      # VAGRANT-BEGIN: 501 UUID
+      # VAGRANT-END: 501 UUID
+    EOH
+  end
+
+  context "one nfs mount" do
+    let(:folders) {
+      {
+        ["/vagrant"] => opts
+      }
+    }
+
+    it "renders the template" do
+      result = Vagrant::Util::TemplateRenderer.render(template, {
+        user:    user,
+        uuid:    uuid,
+        folders: folders,
+        ips:     ips
+      })
+      expect(result).to eq <<-EOH.gsub(/^ {8}/, "")
+        # VAGRANT-BEGIN: 501 UUID
+        /vagrant -alldirs -mapall=501:80 172.16.0.2
+        # VAGRANT-END: 501 UUID
+      EOH
+    end
+  end
+
+  context "subdirectory that should also be exported" do
+    let(:folders) {
+      {
+        ["/vagrant", "/vagrant/other"] => opts
+      }
+    }
+
+    it "puts each directory on its own line" do
+      result = Vagrant::Util::TemplateRenderer.render(template, {
+        user:    user,
+        uuid:    uuid,
+        folders: folders,
+        ips:     ips
+      })
+      expect(result).to eq <<-EOH.gsub(/^ {8}/, "")
+        # VAGRANT-BEGIN: 501 UUID
+        /vagrant -alldirs -mapall=501:80 172.16.0.2
+        /vagrant/other -alldirs -mapall=501:80 172.16.0.2
+        # VAGRANT-END: 501 UUID
+      EOH
+    end
+  end
+end
