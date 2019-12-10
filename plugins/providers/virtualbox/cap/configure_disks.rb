16cccd504a05ad2effd9b1da9851cf7f81bcd2f5
Brian Cain <bcain@hashicorp.com>
2019-12-10 11:05:22 -0800

    Add virtualbox driver method for obtaining vm information

--- plugins/providers/virtualbox/cap/configure_disks.rb
+++ plugins/providers/virtualbox/cap/configure_disks.rb
@@ -8,6 +8,7 @@ module VagrantPlugins
         # @param [VagrantPlugins::Kernel_V2::VagrantConfigDisk] defined_disks
         def self.configure_disks(machine, defined_disks)
           return nil if defined_disks.empty?
+          disks = current_vm_disks(machine)
         end
 
         protected
@@ -15,13 +16,15 @@ module VagrantPlugins
         # Maybe move these into the virtualbox driver??
         # Versioning might be an issue :shrug:
 
-        def current_vm_disks(driver)
+        def self.current_vm_disks(machine)
+          disks = {}
+          info = machine.provider.driver.show_vm_info(machine.id)
         end
 
-        def vmdk_to_vdi(driver)
+        def self.vmdk_to_vdi(driver)
         end
 
-        def vdi_to_vmdk(driver)
+        def self.vdi_to_vmdk(driver)
         end
       end
     end
