02b1ed65ec0824e5fbac5b853c63c1d77d7e412e
Brian Cain <bcain@hashicorp.com>
2019-12-17 11:10:17 -0800

    Fix missing params for ansible_install capability

--- plugins/provisioners/ansible/cap/guest/suse/ansible_install.rb
+++ plugins/provisioners/ansible/cap/guest/suse/ansible_install.rb
@@ -6,7 +6,7 @@ module VagrantPlugins
         module SUSE
           module AnsibleInstall
 
-            def self.ansible_install(machine, install_mode, ansible_version, pip_args)
+            def self.ansible_install(machine, install_mode, ansible_version, pip_args, pip_install_cmd="")
               if install_mode != :default
                 raise Ansible::Errors::AnsiblePipInstallIsNotSupported
               else
