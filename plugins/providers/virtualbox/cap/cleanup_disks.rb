252a7f7a4ef5b69743d779972bded0304c0d625a
Brian Cain <bcain@hashicorp.com>
2020-01-23 09:43:16 -0800

    Do not clean up and detach primary disk

--- plugins/providers/virtualbox/cap/cleanup_disks.rb
+++ plugins/providers/virtualbox/cap/cleanup_disks.rb
@@ -44,10 +44,11 @@ module VagrantPlugins
         # @param [Hash] disk_meta - A hash of all the previously defined disks from the last configure_disk action
         def self.handle_cleanup_disk(machine, defined_disks, disk_meta)
           vm_info = machine.provider.driver.show_vm_info
+          primary_disk = vm_info["SATA Controller-ImageUUID-0-0"]
 
           disk_meta.each do |d|
             dsk = defined_disks.select { |dk| dk.name == d["name"] }
-            if !dsk.empty?
+            if !dsk.empty? || d["uuid"] == primary_disk
               next
             else
               LOGGER.warn("Found disk not in Vagrantfile config: '#{d["name"]}'. Removing disk from guest #{machine.name}")
