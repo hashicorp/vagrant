dbec1a4182b2fe9dddbd8f5db6089fedf0bdc81c
Brian Cain <bcain@hashicorp.com>
2020-02-18 15:16:29 -0800

    Ensure bundler exists

--- .ci/spec/build-vagrant-spec.sh
+++ .ci/spec/build-vagrant-spec.sh
@@ -2,6 +2,7 @@
 
 git clone git@github.com:hashicorp/vagrant-spec.git
 cd vagrant-spec
+gem install bundle
 bundle install
 gem build vagrant-spec.gemspec
 # Assumes this is being run in the same workspace as the main vagrant repo
