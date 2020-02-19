f207754694c707f8cff6890c1a3080f1a88568b7
Brian Cain <bcain@hashicorp.com>
2020-02-19 15:52:35 -0800

    Remove building vagrant-spec gem

--- .ci/spec/build-vagrant-spec.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git clone git@github.com:hashicorp/vagrant-spec.git
-cd vagrant-spec
-gem install bundle
-bundle install
-gem build vagrant-spec.gemspec
-# Assumes this is being run in the same workspace as the main vagrant repo
-mv vagrant-spec-*.gem ../vagrant-spec.gem
