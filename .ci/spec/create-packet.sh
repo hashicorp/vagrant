984d1d74c066ac1bf525e8ff49480bef8e8bfc21
Brian Cain <bcain@hashicorp.com>
2020-02-19 14:28:30 -0800

    Split out packet creation from test runner

--- .ci/spec/create-packet.sh
+++ .ci/spec/create-packet.sh
@@ -23,8 +23,6 @@ root="$( cd -P "$( dirname "$csource" )/../" && pwd )"
 
 pushd "${root}" > "${output}"
 
-# Assumes packet is already set up
-
 # Define a custom cleanup function to destroy any orphan guests
 # on the packet device
 function cleanup() {
@@ -34,28 +32,18 @@ function cleanup() {
                 "Vagrant command failed"
 }
 
+# Ensure we have a packet device to connect
+echo "Creating packet device if needed..."
+
+packet-exec info
+
+if [ $? -ne 0 ]; then
+    wrap_stream packet-exec create \
+                "Failed to create packet device"
+fi
+
 # job_id is provided by common.sh
 export PACKET_EXEC_REMOTE_DIRECTORY="${job_id}"
 export PACKET_EXEC_PERSIST="1"
 
-# spec test configuration, defined by action runners, used by Vagrant on packet
-export PKT_VAGRANT_HOST_BOXES="${VAGRANT_HOST_BOXES}"
-export PKT_VAGRANT_GUEST_BOXES="${VAGRANT_GUEST_BOXES}"
-###
-
-echo "Syncing up remote packet device for current job... "
-# NOTE: We only need to call packet-exec with the -upload option once
-#       since we are persisting the job directory. This command
-#       is used simply to seed the work directory.
-wrap_stream packet-exec run -upload -- /bin/true \
-            "Failed to setup project on remote packet instance"
-
-# Run the job
-
-echo "Running vagrant spec tests..."
-# Need to make memory customizable for windows hosts
-pkt_wrap_stream "cd vagrant;VAGRANT_HOST_MEMORY=5000 VAGRANT_CWD=test/vagrant-spec/ VAGRANT_VAGRANTFILE=Vagrantfile.spec vagrant up --provider vmware_desktop" \
-                "Vagrant command failed"
-
-
-echo "Finished vagrant spec tests"
+echo "Finished creating spec test packet instance"
