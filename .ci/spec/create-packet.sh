6427576edd31d6f0bbb23eebae9ffbc65ad422bb
Brian Cain <bcain@hashicorp.com>
2020-03-04 16:19:38 -0800

    Remove unused cleanup function

--- .ci/spec/create-packet.sh
+++ .ci/spec/create-packet.sh
@@ -9,15 +9,6 @@ root="$( cd -P "$( dirname "$csource" )/../" && pwd )"
 
 pushd "${root}" > "${output}"
 
-# Define a custom cleanup function to destroy any orphan guests
-# on the packet device
-function cleanup() {
-    (>&2 echo "Cleaning up packet device")
-    unset PACKET_EXEC_PERSIST
-    pkt_wrap_stream "cd vagrant;VAGRANT_CWD=test/vagrant-spec/ VAGRANT_VAGRANTFILE=Vagrantfile.spec vagrant destroy -f" \
-                "Vagrant command failed"
-}
-
 # Ensure we have a packet device to connect
 echo "Creating packet device if needed..."
 
