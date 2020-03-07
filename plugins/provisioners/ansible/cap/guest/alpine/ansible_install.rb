e3461d354bfec433194e0617321d4bfc381b67ff
Alexzander thunder Shevchenko <thunder@blackdeath.ru>
2020-03-07 16:31:55 +0300

    python3 setup
    Python 3 setup for any mode;
    Move python setup to separate method;

--- plugins/provisioners/ansible/cap/guest/alpine/ansible_install.rb
+++ plugins/provisioners/ansible/cap/guest/alpine/ansible_install.rb
@@ -8,30 +8,32 @@ module VagrantPlugins
         module Alpine
           module AnsibleInstall
 
-            DEV_PACKAGES = "python3-dev libffi-dev openssl-dev build-base".freeze
-
             def self.ansible_install(machine, install_mode, ansible_version, pip_args, pip_install_cmd = "")
+              python_setup machine
               case install_mode
-              when :pip
-                pip_setup machine, pip_install_cmd
-                Pip::pip_install machine, "ansible", ansible_version, pip_args, true
-              when :pip_args_only
-                pip_setup machine, pip_install_cmd
-                Pip::pip_install machine, "", "", pip_args, false
-              else
-                ansible_apk_install machine
+                when :pip
+                  pip_setup machine
+                  Pip::pip_install machine, "ansible", ansible_version, pip_args, true
+                when :pip_args_only
+                  pip_setup machine
+                  Pip::pip_install machine, "", "", pip_args, false
+                else
+                  ansible_apk_install machine
               end
             end
 
             private
 
-            def self.ansible_apk_install(machine)
-              machine.communicate.sudo "apk add --update python ansible"
+            def self.python_setup(machine)
+              machine.communicate.sudo "apk add --update --no-cache python3"
+              machine.communicate.sudo "if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi"
             end
 
-            def self.pip_setup(machine, pip_install_cmd = "")
-              machine.communicate.sudo "apk add --update python3"
-              machine.communicate.sudo "apk add --update #{DEV_PACKAGES}"
+            def self.ansible_apk_install(machine)
+              machine.communicate.sudo "apk add --update --no-cache ansible"
+            end
+
+            def self.pip_setup(machine)
               machine.communicate.sudo "pip3 install --upgrade pip"
             end
 
