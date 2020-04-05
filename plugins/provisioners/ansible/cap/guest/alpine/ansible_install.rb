d73c2c5fa7c03e1c7f43a2de6c849a65b975825c
Alexzander thunder Shevchenko <thunder@blackdeath.ru>
2020-04-05 14:28:19 +0300

    Get back to install pip via `get_pip` script

--- plugins/provisioners/ansible/cap/guest/alpine/ansible_install.rb
+++ plugins/provisioners/ansible/cap/guest/alpine/ansible_install.rb
@@ -9,13 +9,12 @@ module VagrantPlugins
           module AnsibleInstall
 
             def self.ansible_install(machine, install_mode, ansible_version, pip_args, pip_install_cmd = "")
-              python_setup machine
               case install_mode
                 when :pip
-                  pip_setup machine
+                  pip_setup machine, pip_install_cmd
                   Pip::pip_install machine, "ansible", ansible_version, pip_args, true
                 when :pip_args_only
-                  pip_setup machine
+                  pip_setup machine, pip_install_cmd
                   Pip::pip_install machine, "", "", pip_args, false
                 else
                   ansible_apk_install machine
@@ -24,17 +23,15 @@ module VagrantPlugins
 
             private
 
-            def self.python_setup(machine)
+            def self.ansible_apk_install(machine)
+              machine.communicate.sudo "apk add --update --no-cache python3 ansible"
+            end
+
+            def self.pip_setup(machine, pip_install_cmd = "")
               machine.communicate.sudo "apk add --update --no-cache python3"
               machine.communicate.sudo "if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi"
-            end
-
-            def self.ansible_apk_install(machine)
-              machine.communicate.sudo "apk add --update --no-cache ansible"
-            end
-
-            def self.pip_setup(machine)
-              machine.communicate.sudo "pip3 install --upgrade pip"
+              machine.communicate.sudo "apk add --update --no-cache --virtual .build-deps python3-dev libffi-dev openssl-dev build-base"
+              Pip::get_pip machine, pip_install_cmd
             end
 
           end
