f5d5baec40d8a601b400ecf4f5bfdedb8d65c7b1
sophia <scastellarin95@gmail.com>
2020-04-30 15:13:52 -0400

    Set hostname according to hostname flag and ip for suse

--- plugins/guests/suse/cap/change_host_name.rb
+++ plugins/guests/suse/cap/change_host_name.rb
@@ -4,18 +4,37 @@ module VagrantPlugins
       class ChangeHostName
         def self.change_host_name(machine, name)
           comm = machine.communicate
-
           basename = name.split(".", 2)[0]
           if !comm.test('test "$(hostnamectl --static status)" = "#{basename}"', sudo: false)
             comm.sudo <<-EOH.gsub(/^ {14}/, '')
-              hostnamectl set-hostname '#{basename}'
-
-              # Prepend ourselves to /etc/hosts
-              grep -w '#{name}' /etc/hosts || {
-                sed -i'' '1i 127.0.0.1\\t#{name}\\t#{basename}' /etc/hosts
-              }
+            hostnamectl set-hostname '#{basename}'
+            echo #{name} > /etc/HOSTNAME
             EOH
+            network_with_hostname = machine.config.vm.networks.map {|t, c| c if c[:hostname] }.compact[0]
+            if network_with_hostname
+              replace_host(comm, name, basename, network_with_hostname[:ip])
+            else
+              add_hostname_to_loopback(comm, name, basename)
+            end
           end
+        end
+
+        def self.add_hostname_to_loopback(comm, name, basename)
+          # Add hostname to /etc/hosts if not already there
+          comm.sudo <<-EOH.gsub(/^ {14}/, '')
+          grep -w '#{name}' /etc/hosts || {
+            sed -i'' '1i 127.0.0.1\\t#{name}\\t#{basename}' /etc/hosts
+          }
+          EOH
+        end
+
+        def self.replace_host(comm, name, basename, ip)
+          # Remove any line in /etc/hosts that contains hostname,
+          # then add hostname with associated ip 
+          comm.sudo <<-EOH.gsub(/^ {14}/, '')
+          sed -i '/#{name}/d' /etc/hosts
+          sed -i'' '1i '#{ip}'\\t#{name}\\t#{basename}' /etc/hosts
+          EOH
         end
       end
     end
