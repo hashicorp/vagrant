c844f7689dc95199d04b9638785b26e362202c76
sophia <scastellarin95@gmail.com>
2020-06-29 16:03:38 -0500

    Make pld guest respect 'hostname' network config option

--- plugins/guests/pld/cap/change_host_name.rb
+++ plugins/guests/pld/cap/change_host_name.rb
@@ -1,10 +1,21 @@
+require 'vagrant/util/guest_hosts'
+
 module VagrantPlugins
   module GuestPld
     module Cap
       class ChangeHostName
+        extend Vagrant::Util::GuestHosts::Linux
+
         def self.change_host_name(machine, name)
           comm = machine.communicate
 
+          network_with_hostname = machine.config.vm.networks.map {|_, c| c if c[:hostname] }.compact[0]
+          if network_with_hostname
+            replace_host(comm, name, network_with_hostname[:ip])
+          else
+            add_hostname_to_loopback_interface(comm, name)
+          end
+          
           if !comm.test("hostname -f | grep '^#{name}$'", sudo: false)
             basename = name.split(".", 2)[0]
             comm.sudo <<-EOH.gsub(/^ {14}/, '')
@@ -12,11 +23,6 @@ module VagrantPlugins
               sed -i 's/\\(HOSTNAME=\\).*/\\1#{name}/' /etc/sysconfig/network
 
               sed -i 's/\\(DHCP_HOSTNAME=\\).*/\\1\"#{name}\"/' /etc/sysconfig/interfaces/ifcfg-*
-
-              # Prepend ourselves to /etc/hosts
-              grep -w '#{name}' /etc/hosts || {
-                sed -i'' '1i 127.0.0.1\\t#{name}\\t#{basename}' /etc/hosts
-              }
 
               # Restart networking
               service network restart
