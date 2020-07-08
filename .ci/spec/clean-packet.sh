3c936b75ada4b4e40b4ab505dd8a462c00434e3d
Brian Cain <bcain@hashicorp.com>
2020-07-08 16:46:42 -0700

    Load common prior to env to include job_id

--- .ci/spec/clean-packet.sh
+++ .ci/spec/clean-packet.sh
@@ -4,8 +4,8 @@ csource="${BASH_SOURCE[0]}"
 while [ -h "$csource" ] ; do csource="$(readlink "$csource")"; done
 root="$( cd -P "$( dirname "$csource" )/../../" && pwd )"
 
-. "${root}/.ci/spec/env.sh"
 . "${root}/.ci/common.sh"
+. "${root}/.ci/spec/env.sh"
 
 pushd "${root}" > "${output}"
 
