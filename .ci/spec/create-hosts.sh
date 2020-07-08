f20bb886180e892dc3f08a06425341d795369570
Brian Cain <bcain@hashicorp.com>
2020-07-08 16:18:45 -0700

    Wrap aws command with error if failure

--- .ci/spec/create-hosts.sh
+++ .ci/spec/create-hosts.sh
@@ -24,7 +24,8 @@ export PKT_VAGRANT_VAGRANTFILE=Vagrantfile.spec
 ###
 
 # Grab vagrant-spec gem and place inside root dir of Vagrant repo
-aws s3 cp "${ASSETS_PRIVATE_BUCKET}/vagrant-spec/vagrant-spec.gem" "vagrant-spec.gem"
+wrap aws s3 cp "${ASSETS_PRIVATE_BUCKET}/vagrant-spec/vagrant-spec.gem" "vagrant-spec.gem" \
+  "Failed to download the vagrant-spec gem"
 ###
 
 # Run the job
