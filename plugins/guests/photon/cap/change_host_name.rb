169d7b4011aa7a3c0130d749c4ebb72811eaf851
sophia <scastellarin95@gmail.com>
2020-07-20 13:25:41 -0500

    Update photon cap for setting hostname
    sqash

--- plugins/guests/photon/cap/change_host_name.rb
+++ plugins/guests/photon/cap/change_host_name.rb
@@ -1,11 +1,11 @@
-require 'vagrant/util/guest_hosts'
+require_relative '../../linux/cap/change_host_name'
 
 module VagrantPlugins
   module GuestPhoton
     module Cap
       class ChangeHostName
-        extend Vagrant::Util::GuestHosts::Linux
-        
+        extend VagrantPlugins::GuestLinux::Cap::ChangeHostName
+
         def self.change_name_command(name)
           return <<-EOH.gsub(/^ {14}/, "")
              # Set the hostname
