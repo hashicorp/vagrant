8d6bc662fbb6ce6a056df9d3eb4e901b92910071
sophia <scastellarin95@gmail.com>
2020-08-31 16:25:19 -0500

    Use latest built assests to run test

--- .ci/spec/create-hosts.sh
+++ .ci/spec/create-hosts.sh
@@ -25,6 +25,16 @@ wrap aws s3 cp "${ASSETS_PRIVATE_BUCKET}/hashicorp/vagrant-spec/vagrant-spec.gem
   "Could not download vagrant-spec.gem from s3 asset bucket"
 ###
 
+# Grab vagrant installer place inside root dir of Vagrant repo
+# TODO: Have this variable set
+VAGRANT_INSTALLER_VERSION="2.2.11"
+# TODO: Get release by reference
+INSTALLER_URL=`curl -s https://api.github.com/repos/hashicorp/vagrant-installers/releases | jq -r --arg installer_name "vagrant_${VAGRANT_INSTALLER_VERSION}_x86_64.deb" '.[0].assets[] | select(.name == $installer_name) | .url'`
+
+wrap curl -Lso ./vagrant_${VAGRANT_INSTALLER_VERSION}_x86_64.deb ${INSTALLER_URL} \
+  "Could not download vagrant installers"
+###
+
 # Run the job
 
 echo "Creating vagrant spec guests..."
