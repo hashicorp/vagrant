a843fd9da7b4d1cdf4084007b3c57f2369163bee
sophia <scastellarin95@gmail.com>
2020-09-14 18:12:58 -0500

    Add docker specific tests

--- test/vagrant-spec/configs/vagrant-spec.config.docker.rb
+++ test/vagrant-spec/configs/vagrant-spec.config.docker.rb
@@ -1,11 +1,11 @@
 require_relative "../../acceptance/base"
 
 Vagrant::Spec::Acceptance.configure do |c|
-  c.component_paths << File.expand_path("../test/acceptance", __FILE__)
-  c.skeleton_paths << File.expand_path("../test/acceptance/skeletons", __FILE__)
+  c.component_paths << File.expand_path("../../../acceptance", __FILE__)
+  c.skeleton_paths << File.expand_path("../../../acceptance/skeletons", __FILE__)
   # Allow for slow setup to still pass
   c.assert_retries = 15
   c.provider "docker",
-    box: ENV["VAGRANT_SPEC_DOCKER_IMAGE"],
-    contexts: ["provider-context/docker"]
+    image: ENV["VAGRANT_SPEC_DOCKER_IMAGE"],
+    box: "placeholder"
 end
