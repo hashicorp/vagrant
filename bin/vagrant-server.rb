c1dbc5efad90b86db5356e45d9bbc07f91444251
sophia <scastellarin95@gmail.com>
2020-11-30 13:55:58 -0600

    Return mock plugin list

--- bin/vagrant-server.rb
+++ bin/vagrant-server.rb
@@ -1,22 +1,20 @@
-require_relative '../proto/gen/plugin_pb'
-require_relative '../proto/gen/plugin_services_pb'
+#!/Users/sophia/miniconda3/envs/vagrant/bin/ruby
+
+require_relative '../proto/gen/ruby-server_pb'
+require_relative '../proto/gen/ruby-server_services_pb'
 
 require 'grpc'
 require 'grpc/health/checker'
 require 'grpc/health/v1/health_services_pb'
 
-class ProviderService < Hashicorp::Vagrant::Sdk::Provider::Service
-  def documentation(req, _unused_call)
-    
+class PluginService < Hashicorp::Vagrant::RubyVagrant::Service
+  def get_plugins(req, _unused_call)
+    plugins = [Hashicorp::Vagrant::Plugin.new(name: "test")]
+    Hashicorp::Vagrant::GetPluginsResponse.new(
+      plugins: plugins
+    )
   end
 
-  def configure(req, _unused_call)
-    
-  end
-
-  def config_struct(req, _unused_call)
-
-  end
 end
 
 def main
@@ -24,11 +22,11 @@ def main
   # Listen on port 24000 on all interfaces. Update for production use.
   s.add_http2_port('[::]:10001', :this_port_is_insecure)
 
-  s.handle(ProviderService.new)
+  s.handle(PluginService.new)
 
   health_checker = Grpc::Health::Checker.new
   health_checker.add_status(
-    "Hashicorp::Vagrant::Sdk::Provider::ProviderService",
+    "Hashicorp::Vagrant::RubyVagrant::Service::PluginService",
     Grpc::Health::V1::HealthCheckResponse::ServingStatus::SERVING)
   s.handle(health_checker)
 
