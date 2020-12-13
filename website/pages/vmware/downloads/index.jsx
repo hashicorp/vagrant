315c93f81a1c474046d5f18ff6fb5531abfe18f4
Zack Tanner <zacktanner@gmail.com>
2020-12-13 11:06:22 -0800

    include vmware/downloads page

--- website/pages/vmware/downloads/index.jsx
+++ website/pages/vmware/downloads/index.jsx
@@ -1,38 +1,114 @@
-import s from './style.module.css'
-import { VMWARE_UTILITY_VERSION } from 'data/version.json'
-import ProductDownloader from '@hashicorp/react-product-downloader'
+import { VMWARE_UTILITY_VERSION as VERSION } from 'data/version.json'
+import { productName, productSlug } from 'data/metadata'
 import Head from 'next/head'
 import HashiHead from '@hashicorp/react-head'
+import ProductDownloader from '@hashicorp/react-product-downloader'
+import styles from './style.module.css'
+import logo from '@hashicorp/mktg-assets/dist/product/vagrant-logo/color.svg'
 
-export default function DownloadsPage({ releaseData }) {
+export default function DownloadsPage({ releases }) {
   return (
-    <div className={s.root}>
+    <>
       <HashiHead
         is={Head}
         title="VMware Utility Downloads | Vagrant by HashiCorp"
       />
+
       <ProductDownloader
-        product="Vagrant VMware Utility"
-        baseProduct="Vagrant"
-        version={VMWARE_UTILITY_VERSION}
-        releaseData={releaseData}
-        changelog={false}
+        releases={releases}
+        packageManagers={[
+          {
+            label: 'Homebrew',
+            commands: [
+              'brew tap hashicorp/tap',
+              'brew install hashicorp/tap/vagrant',
+            ],
+            os: 'darwin',
+          },
+          {
+            label: 'Ubuntu/Debian',
+            commands: [
+              'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -',
+              'sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"',
+              'sudo apt-get update && sudo apt-get install vagrant',
+            ],
+            os: 'linux',
+          },
+          {
+            label: 'CentOS/RHEL',
+            commands: [
+              'sudo yum install -y yum-utils',
+              'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo',
+              'sudo yum -y install vagrant',
+            ],
+            os: 'linux',
+          },
+          {
+            label: 'Fedora',
+            commands: [
+              'sudo dnf install -y dnf-plugins-core',
+              'sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo',
+              'sudo dnf -y install vagrant',
+            ],
+            os: 'linux',
+          },
+          {
+            label: 'Amazon Linux',
+            commands: [
+              'sudo yum install -y yum-utils',
+              'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo',
+              'sudo yum -y install vagrant',
+            ],
+            os: 'linux',
+          },
+        ]}
+        productName="Vagrant VMware Utility"
+        productId={productSlug}
+        latestVersion={VERSION}
+        getStartedDescription="Follow step-by-step tutorials on AWS, Azure, GCP, and localhost."
+        getStartedLinks={[
+          {
+            label: 'Placeholder',
+            href: '#',
+          },
+          {
+            label: 'Placeholder',
+            href: '#',
+          },
+        ]}
+        logo={<img className={styles.logo} alt={productName} src={logo} />}
+        brand={productSlug}
+        tutorialLink={{
+          href: 'https://learn.hashicorp.com/vagrant',
+          label: 'View Tutorials at HashiCorp Learn',
+        }}
       />
-    </div>
+    </>
   )
 }
 
 export async function getStaticProps() {
   return fetch(
-    `https://releases.hashicorp.com/vagrant-vmware-utility/${VMWARE_UTILITY_VERSION}/index.json`
+    `https://releases.hashicorp.com/vagrant-vmware-utility/index.json`,
+    {
+      headers: {
+        'Cache-Control': 'no-cache',
+      },
+    }
   )
     .then((res) => res.json())
-    .then((releaseData) => ({ props: { releaseData } }))
+    .then((result) => {
+      return {
+        props: {
+          releases: result,
+        },
+      }
+    })
     .catch(() => {
       throw new Error(
         `--------------------------------------------------------
-        Unable to resolve version ${VMWARE_UTILITY_VERSION} on releases.hashicorp.com from link
-        <https://releases.hashicorp.com/vagrant-vmware-utility/${VMWARE_UTILITY_VERSION}/index.json>. Usually this
+        Unable to resolve version ${VERSION} on releases.hashicorp.com from link
+        <https://releases.hashicorp.com/vagrant/${VERSION}/index.json>. Usually this
         means that the specified version has not yet been released. The downloads page
         version can only be updated after the new version has been released, to ensure
         that it works for all users.
