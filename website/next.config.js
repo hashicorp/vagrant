495db348d2f573a86d995fd883d537da3d6d70b8
Jeff Escalante <jescalan@users.noreply.github.com>
2020-12-16 21:57:04 -0500

    smooth out some config syntax

--- website/next.config.js
+++ website/next.config.js
@@ -1,5 +1,5 @@
 const withHashicorp = require('@hashicorp/nextjs-scripts')
-const redirects = require('./redirects.next.js')
+const redirects = require('./redirects.next')
 
 console.log(`Environment: ${process.env.HASHI_ENV}`)
 
@@ -8,9 +8,7 @@ module.exports = withHashicorp({
   transpileModules: ['is-absolute-url'],
 })({
   svgo: { plugins: [{ removeViewBox: false }] },
-  redirects() {
-    return redirects
-  },
+  redirects: () => redirects,
   env: {
     HASHI_ENV: process.env.HASHI_ENV || 'development',
     SEGMENT_WRITE_KEY: 'wFMyBE4PJCZttWfu0pNhYdWr7ygW0io4',
