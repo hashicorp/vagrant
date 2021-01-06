f412672b24147878b42c889e9a6f464e5e8f8550
Max Arnold <arnold.maxim@gmail.com>
2021-01-06 21:56:57 +0700

    Download upstream Salt bootstrap script into the same temp directory

--- plugins/provisioners/salt/bootstrap-salt.ps1
+++ plugins/provisioners/salt/bootstrap-salt.ps1
@@ -1,8 +1,11 @@
 # Powershell supports only TLS 1.0 by default. Add support for TLS 1.2 and TLS 1.3
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12,Tls13'
 
+# Define script root for PowerShell 2.0
+$ScriptRoot = Split-Path $script:MyInvocation.MyCommand.Path
+
 # Download the upstream bootstrap script
-(New-Object System.Net.WebClient).DownloadFile('https://winbootstrap.saltstack.com', 'upstream-bootstrap.ps1')
+(New-Object System.Net.WebClient).DownloadFile('https://winbootstrap.saltstack.com', "${ScriptRoot}\bootstrap_salt_upstream.ps1")
 
 # Run the upstream bootstrap script with passthrough arguments
-./upstream-bootstrap.ps1 @args
+& "${ScriptRoot}\bootstrap_salt_upstream.ps1" @args
