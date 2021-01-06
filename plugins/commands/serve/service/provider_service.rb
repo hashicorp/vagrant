7438d2d7e7a77977807d1e60fc794a894db0f61c
sophia <scastellarin95@gmail.com>
2021-01-06 17:29:34 -0600

    Get to run action up

--- plugins/commands/serve/service/provider_service.rb
+++ plugins/commands/serve/service/provider_service.rb
@@ -1,11 +1,14 @@
 
 require_relative 'proto/gen/plugin/plugin_pb'
 require_relative 'proto/gen/plugin/plugin_services_pb'
+require 'logger'
 
 module VagrantPlugins
   module CommandServe
     module Serve
       class ProviderService < Hashicorp::Vagrant::Sdk::ProviderService::Service
+        LOG = Logger.new('/Users/sophia/project/vagrant-ruby/log.txt')
+
         def usable(req, _unused_call)
           nil
         end
@@ -31,11 +34,22 @@ module VagrantPlugins
         end
 
         def action_up(req, _unused_call)
+          LOG.debug("action up")
+          LOG.debug(req)
           nil
         end
 
         def action_up_spec(req, _unused_call)
-          nil
+          LOG.debug("action up spec")
+          # resultSpec = Hashicorp::Vagrant::FuncSpec.Value.new(
+          #   name: "",
+          #   type: ""
+          # )
+          Hashicorp::Vagrant::Sdk::FuncSpec.new(
+            name: "test",
+            args: [],
+            result: []
+          )
         end
       end
     end
