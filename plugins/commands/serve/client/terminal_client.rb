548a0339dff4aa532f1a484eaeab466d91710b7e
Chris Roberts <croberts@hashicorp.com>
2021-01-28 17:09:27 -0800

    Update name and use constant alias

--- plugins/commands/serve/client/terminal_client.rb
+++ plugins/commands/serve/client/terminal_client.rb
@@ -1,15 +1,14 @@
 module VagrantPlugins
   module CommandServe
     module Client
-      class TerminalClient
-
-        # @params [String] endpoint for the core service 
+      class Terminal
+        # @params [String] endpoint for the core service
         def initialize(server_endpoint)
-          @client = Hashicorp::Vagrant::Sdk::TerminalUIService::Stub.new(server_endpoint, :this_channel_is_insecure)
+          @client = SDK::TerminalUIService::Stub.new(server_endpoint, :this_channel_is_insecure)
         end
 
         def self.terminal_arg_to_terminal_ui(raw_terminal)
-          terminal_arg = Hashicorp::Vagrant::Sdk::Args::TerminalUI.decode(raw_terminal)
+          terminal_arg = SDK::Args::TerminalUI.decode(raw_terminal)
           # TODO (sophia): this should have an option to not be a unix socket
           addr = "unix:" + terminal_arg.addr
           self.new(addr)
@@ -17,7 +16,7 @@ module VagrantPlugins
 
         # @params [Array] the content to print
         def output(content)
-          req = Hashicorp::Vagrant::Sdk::TerminalUI::OutputRequest.new(
+          req = SDK::TerminalUI::OutputRequest.new(
             lines: content
           )
           @client.output(req)
