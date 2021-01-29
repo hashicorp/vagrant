28ebbce108ea7e38f23c516f3315bbd38db27bc2
Chris Roberts <croberts@hashicorp.com>
2021-01-28 17:08:57 -0800

    Update to use the service info helper

--- plugins/commands/serve/service/command_service.rb
+++ plugins/commands/serve/service/command_service.rb
@@ -1,58 +1,64 @@
 module VagrantPlugins
   module CommandServe
     module Service
-      class CommandService < Hashicorp::Vagrant::Sdk::CommandService::Service
+      class CommandService < SDK::CommandService::Service
         def help_spec(*args)
-          Hashicorp::Vagrant::Sdk::FuncSpec.new
+          SDK::FuncSpec.new
         end
 
-        def help(*args)
-          options = command_options_for(args.last.metadata["plugin_name"])
-          Hashicorp::Vagrant::Sdk::Command::HelpResp.new(
-            help: options.help
-          )
+        def help(req, ctx)
+          ServiceInfo.with_info(ctx) do |info|
+            options = command_options_for(info.plugin_name)
+            SDK::Command::HelpResp.new(
+              help: options.help
+            )
+          end
         end
 
         def synopsis_spec(*args)
-          return Hashicorp::Vagrant::Sdk::FuncSpec.new
+          return SDK::FuncSpec.new
         end
 
-        def synopsis(*args)
-          plugin_name = args.last.metadata["plugin_name"]
-          plugin = Vagrant::Plugin::V2::Plugin.manager.commands[plugin_name.to_sym].to_a.first
-          if !plugin
-            raise "Failed to locate command plugin for: #{plugin_name}"
+        def synopsis(req, ctx)
+          ServiceInfo.with_info(ctx) do |info|
+            plugin_name = info.plugin_name
+            plugin = Vagrant::Plugin::V2::Plugin.manager.commands[plugin_name.to_sym].to_a.first
+            if !plugin
+              raise "Failed to locate command plugin for: #{plugin_name}"
+            end
+            klass = plugin.call
+            SDK::Command::SynopsisResp.new(
+              synopsis: klass.synopsis
+            )
           end
-          klass = plugin.call
-          Hashicorp::Vagrant::Sdk::Command::SynopsisResp.new(
-            synopsis: klass.synopsis
-          )
         end
 
         def flags_spec(*args)
-          Hashicorp::Vagrant::Sdk::FuncSpec.new
+          SDK::FuncSpec.new
         end
 
-        def flags(*args)
-          options = command_options_for(args.last.metadata["plugin_name"])
+        def flags(req, ctx)
+          ServiceInfo.with_info(ctx) do |info|
+            options = command_options_for(info.plugin_name)
 
-          # Now we can build our list of flags
-          flags = options.top.list.find_all { |o|
-            o.is_a?(OptionParser::Switch)
-          }.map { |o|
-            Hashicorp::Vagrant::Sdk::Command::Flag.new(
-              description: o.desc.join(" "),
-              long_name: o.switch_name,
-              short_name: o.short.first,
-              type: o.is_a?(OptionParser::Switch::NoArgument) ?
-                Hashicorp::Vagrant::Sdk::Command::Flag::Type::BOOL :
-                Hashicorp::Vagrant::Sdk::Command::Flag::Type::STRING
+            # Now we can build our list of flags
+            flags = options.top.list.find_all { |o|
+              o.is_a?(OptionParser::Switch)
+            }.map { |o|
+              SDK::Command::Flag.new(
+                description: o.desc.join(" "),
+                long_name: o.switch_name,
+                short_name: o.short.first,
+                type: o.is_a?(OptionParser::Switch::NoArgument) ?
+                  SDK::Command::Flag::Type::BOOL :
+                  SDK::Command::Flag::Type::STRING
+              )
+            }
+
+            SDK::Command::FlagsResp.new(
+              flags: flags
             )
-          }
-
-          Hashicorp::Vagrant::Sdk::Command::FlagsResp.new(
-            flags: flags
-          )
+          end
         end
 
         def command_options_for(name)
@@ -88,15 +94,15 @@ module VagrantPlugins
         end
 
         def execute_spec(req, ctx)
-          Hashicorp::Vagrant::Sdk::FuncSpec.new(
+          SDK::FuncSpec.new(
             args: [
-              Hashicorp::Vagrant::Sdk::FuncSpec::Value.new(
+              SDK::FuncSpec::Value.new(
                 type: "hashicorp.vagrant.sdk.Args.TerminalUI",
                 name: "",
               ),
             ],
             result: [
-              Hashicorp::Vagrant::Sdk::FuncSpec::Value.new(
+              SDK::FuncSpec::Value.new(
                 type: "hashicorp.vagrant.sdk.Command.ExecuteResp",
                 name: "",
               ),
@@ -105,29 +111,29 @@ module VagrantPlugins
         end
 
         def execute(req, ctx)
-          plugin_name = ctx.metadata["plugin_name"]
-          if plugin_name.nil?
-            raise "missing plugin name in context: #{ctx.metadata.inspect}"
-          end
-          raw_terminal = req.args.first.value.value
-          ui_client = Client::TerminalClient.terminal_arg_to_terminal_ui(raw_terminal)
-          ui = Vagrant::UI::RemoteUI.new(ui_client)
-          env = Vagrant::Environment.new(ui: ui, ui_class: Vagrant::UI::Silent)
+          ServiceInfo.with_info(ctx) do |info|
+            plugin_name = info.plugin_name
+            raw_terminal = req.args.first.value.value
+            ui_client = Client::Terminal.terminal_arg_to_terminal_ui(raw_terminal)
+            ui = Vagrant::UI::RemoteUI.new(ui_client)
+            env = Vagrant::Environment.new(ui: ui)
 
-          plugin = Vagrant::Plugin::V2::Plugin.manager.commands[plugin_name.to_sym].to_a.first
-          if !plugin
-            raise "Failed to locate command plugin for: #{plugin_name}"
+            plugin = Vagrant::Plugin::V2::Plugin.manager.commands[plugin_name.to_sym].to_a.first
+            if !plugin
+              raise "Failed to locate command plugin for: #{plugin_name}"
+            end
+            cmd_klass = plugin.call
+            cmd = cmd_klass.new([], env)
+            begin
+              result = cmd.execute
+            rescue => e
+              raise e.to_s + "\n" + e.backtrace.join("\n")
+            end
+
+            SDK::Command::ExecuteResp.new(
+              exit_code: result.to_i
+            )
           end
-          cmd_klass = plugin.call
-          cmd = cmd_klass.new([], env)
-          begin
-            result = cmd.execute
-          rescue => e
-            raise e.to_s + "\n" + e.backtrace.join("\n")
-          end
-          Hashicorp::Vagrant::Sdk::Command::ExecuteResp.new(
-            exit_code: result
-          )
         end
       end
     end
