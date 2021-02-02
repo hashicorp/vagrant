1022e5b99153f92e4d91d0a5169dfb84d6937cb7
Connor Martin <connormartin7@gmail.com>
2021-02-01 21:54:10 -0600

    remove all space from checksums

--- lib/vagrant/action/builtin/box_add.rb
+++ lib/vagrant/action/builtin/box_add.rb
@@ -543,6 +543,8 @@ module Vagrant
           @logger.info("Expected checksum: #{checksum}")
 
           actual = FileChecksum.new(path, checksum_type).checksum
+          actual.gsub!(/\s+/, '')
+          checksum.gsub!(/\s+/, '')
           @logger.info("Actual checksum: #{actual}")
           if actual.casecmp(checksum) != 0
             raise Errors::BoxChecksumMismatch,
