2c3397c46851ef29a3589bf3214a3eee12da8484
Bryce Kalow <bkalow@hashicorp.com>
2021-03-31 12:10:21 -0500

    feat(website): migrates nav data format and updates docs pages (#12259)

--- website/pages/vmware/[...page].jsx
+++ website/pages/vmware/[...page].jsx
@@ -1,28 +1,44 @@
 import { productName, productSlug } from 'data/metadata'
-import order from 'data/vmware-navigation.js'
 import DocsPage from '@hashicorp/react-docs-page'
 import {
   generateStaticPaths,
   generateStaticProps,
 } from '@hashicorp/react-docs-page/server'
 
-const subpath = 'vmware'
+const NAV_DATA_FILE = 'data/vmware-nav-data.json'
+const CONTENT_DIR = 'content/vmware'
+const basePath = 'vmware'
 
-export default function IntroLayout(props) {
+export default function DocsLayout(props) {
   return (
     <DocsPage
       product={{ name: productName, slug: productSlug }}
-      subpath={subpath}
-      order={order}
+      baseRoute={basePath}
       staticProps={props}
     />
   )
 }
 
 export async function getStaticPaths() {
-  return generateStaticPaths(subpath)
+  const paths = await generateStaticPaths({
+    navDataFile: NAV_DATA_FILE,
+    localContentDir: CONTENT_DIR,
+  })
+
+  return {
+    fallback: false,
+    // This path has an index route explicitly declared as a page
+    paths: paths.filter(({ params }) => params.page.length > 0),
+  }
 }
 
 export async function getStaticProps({ params }) {
-  return generateStaticProps({ subpath, productName, params })
+  return {
+    props: await generateStaticProps({
+      navDataFile: NAV_DATA_FILE,
+      localContentDir: CONTENT_DIR,
+      product: { name: productName, slug: productSlug },
+      params,
+    }),
+  }
 }
