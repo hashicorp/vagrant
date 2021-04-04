6c0a5ba072aa8dab66ed3ab8d36c10e1a4dded42
Vasilyev, Viacheslav <avoidik@gmail.com>
2021-04-04 18:34:34 +0300

    ssh communicator - reset retries counter, sleep between connection attempts

--- plugins/communicators/ssh/communicator.rb
+++ plugins/communicators/ssh/communicator.rb
@@ -84,7 +84,7 @@ module VagrantPlugins
             message  = nil
             begin
               begin
-                connect(retries: 1)
+                connect
                 return true if ready?
               rescue Vagrant::Errors::VagrantError => e
                 @logger.info("SSH not ready: #{e.inspect}")
@@ -429,7 +429,7 @@ module VagrantPlugins
           timeout = 60
 
           @logger.info("Attempting SSH connection...")
-          connection = retryable(tries: opts[:retries], on: SSH_RETRY_EXCEPTIONS) do
+          connection = retryable(tries: opts[:retries], on: SSH_RETRY_EXCEPTIONS, sleep: 5) do
             Timeout.timeout(timeout) do
               begin
                 # This logger will get the Net-SSH log data for us.
@@ -514,6 +514,9 @@ module VagrantPlugins
           # is used. Show a nicer error.
           raise Vagrant::Errors::SSHKeyTypeNotSupported
         end
+
+        @logger.debug("Resetting retries counter")
+        opts.delete(:retries) if opts.key?(:retries)
 
         @connection          = connection
         @connection_ssh_info = ssh_info
