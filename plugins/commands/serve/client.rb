3d000609ec1bb4b5bc6a8e1eea3237577b03d119
Chris Roberts <croberts@hashicorp.com>
2021-04-21 16:09:01 -0700

    Update autoload definition for terminal client

--- plugins/commands/serve/client.rb
+++ plugins/commands/serve/client.rb
@@ -7,7 +7,7 @@ module VagrantPlugins
       ServiceInfo = Service::ServiceInfo
 
       autoload :Machine, Vagrant.source_root.join("plugins/commands/serve/client/machine").to_s
-      autoload :Terminal, Vagrant.source_root.join("plugins/commands/serve/client/terminal_client").to_s
+      autoload :Terminal, Vagrant.source_root.join("plugins/commands/serve/client/terminal").to_s
     end
   end
 end
