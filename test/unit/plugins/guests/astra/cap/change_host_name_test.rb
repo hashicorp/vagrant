dc546e3af3ac75e13ec9c4f4c17c90937ccd2241
Andrey Kirillov <akirillov@astralinux.ru>
2021-04-21 13:22:15 +0300

    Add capability tests for Astra Linux

---
+++ test/unit/plugins/guests/astra/cap/change_host_name_test.rb
@@ -0,0 +1,195 @@
+require_relative "../../../../base"
+
+describe "VagrantPlugins::GuestAstra::Cap::ChangeHostName" do
+  let(:caps) do
+    VagrantPlugins::GuestAstra::Plugin
+      .components
+      .guest_capabilities[:astra]
+  end
+
+  let(:machine) { double("machine", name: "guestname") }
+  let(:logger) { double("logger", debug: true) }
+  let(:comm) { VagrantTests::DummyCommunicator::Communicator.new(machine) }
+
+  before do
+    allow(machine).to receive(:communicate).and_return(comm)
+  end
+
+  after do
+    comm.verify_expectations!
+  end
+
+  describe ".change_host_name" do
+    let(:cap) { caps.get(:change_host_name) }
+    let(:name) { 'banana-rama.example.com' }
+    let(:systemd) { true }
+    let(:hostnamectl) { true }
+    let(:networkd) { true }
+    let(:network_manager) { false }
+    let(:networks) { [
+      [:forwarded_port, {:guest=>22, :host=>2222, :host_ip=>"127.0.0.1", :id=>"ssh", :auto_correct=>true, :protocol=>"tcp"}]
+    ] }
+
+    before do
+      allow(cap).to receive(:systemd?).and_return(systemd)
+      allow(cap).to receive(:hostnamectl?).and_return(hostnamectl)
+      allow(cap).to receive(:systemd_networkd?).and_return(networkd)
+      allow(cap).to receive(:systemd_controlled?).with(anything, /NetworkManager/).and_return(network_manager)
+      allow(machine).to receive_message_chain(:config, :vm, :networks).and_return(networks)
+      allow(cap).to receive(:add_hostname_to_loopback_interface)
+      allow(cap).to receive(:replace_host)
+    end
+
+    context "minimal network config" do
+      it "sets the hostname if not set" do
+        comm.stub_command("hostname -f | grep '^#{name}$'", exit_code: 1)
+        cap.change_host_name(machine, name)
+        expect(comm.received_commands[1]).to match(/echo 'banana-rama' > \/etc\/hostname/)
+      end
+
+      it "sets the hostname if not set" do
+        comm.stub_command("hostname -f | grep '^#{name}$'", exit_code: 0)
+        cap.change_host_name(machine, name)
+        expect(comm.received_commands[1]).to_not match(/echo 'banana-rama' > \/etc\/hostname/)
+      end
+    end
+
+    context "multiple networks configured with hostname" do 
+      it "adds a new entry only for the hostname" do 
+        networks = [
+          [:forwarded_port, {:guest=>22, :host=>2222, :host_ip=>"127.0.0.1", :id=>"ssh", :auto_correct=>true, :protocol=>"tcp"}],
+          [:public_network, {:ip=>"192.168.0.1", :hostname=>true, :protocol=>"tcp", :id=>"93a4ad88-0774-4127-a161-ceb715ff372f"}],
+          [:public_network, {:ip=>"192.168.0.2", :protocol=>"tcp", :id=>"5aebe848-7d85-4425-8911-c2003d924120"}]
+        ]
+        allow(machine).to receive_message_chain(:config, :vm, :networks).and_return(networks)
+        expect(cap).to receive(:replace_host)
+        expect(cap).to_not receive(:add_hostname_to_loopback_interface)
+        cap.change_host_name(machine, name)
+      end
+
+      it "appends an entry to the loopback interface" do 
+        networks = [
+          [:forwarded_port, {:guest=>22, :host=>2222, :host_ip=>"127.0.0.1", :id=>"ssh", :auto_correct=>true, :protocol=>"tcp"}],
+          [:public_network, {:ip=>"192.168.0.1", :protocol=>"tcp", :id=>"93a4ad88-0774-4127-a161-ceb715ff372f"}],
+          [:public_network, {:ip=>"192.168.0.2", :protocol=>"tcp", :id=>"5aebe848-7d85-4425-8911-c2003d924120"}]
+        ]
+        allow(machine).to receive_message_chain(:config, :vm, :networks).and_return(networks)
+        expect(cap).to_not receive(:replace_host)
+        expect(cap).to receive(:add_hostname_to_loopback_interface).once
+        cap.change_host_name(machine, name)
+      end
+    end
+
+    context "when hostnamectl is in use" do
+      let(:hostnamectl) { true }
+
+      it "sets hostname with hostnamectl" do
+        cap.change_host_name(machine, name)
+        expect(comm.received_commands[2]).to match(/hostnamectl/)
+      end
+    end
+
+    context "when hostnamectl is not in use" do
+      let(:hostnamectl) { false }
+
+      it "sets hostname with hostname command" do
+        cap.change_host_name(machine, name)
+        expect(comm.received_commands[2]).to match(/hostname -F/)
+      end
+    end
+
+    context "restarts the network" do
+      context "when networkd is in use" do
+        let(:networkd) { true }
+
+        it "restarts networkd with systemctl" do
+          cap.change_host_name(machine, name)
+          expect(comm.received_commands[3]).to match(/systemctl restart systemd-networkd/)
+        end
+      end
+
+      context "when NetworkManager is in use" do
+        let(:networkd) { false }
+        let(:network_manager) { true }
+
+        it "restarts NetworkManager with systemctl" do
+          cap.change_host_name(machine, name)
+          expect(comm.received_commands[3]).to match(/systemctl restart NetworkManager/)
+        end
+      end
+
+      context "when networkd and NetworkManager are not in use" do
+        let(:networkd) { false }
+        let(:network_manager) { false }
+        let(:systemd) { true }
+
+        it "restarts the network using systemctl" do
+          expect(cap).to receive(:restart_each_interface).
+            with(machine, anything)
+          cap.change_host_name(machine, name)
+        end
+
+        it "restarts networking with networking init script" do
+          expect(cap).to receive(:restart_each_interface).
+            with(machine, anything)
+          cap.change_host_name(machine, name)
+        end
+      end
+
+      context "when systemd is not in use" do
+        let(:systemd) { false }
+
+        it "restarts the network using service" do
+          expect(cap).to receive(:restart_each_interface).
+            with(machine, anything)
+          cap.change_host_name(machine, name)
+        end
+
+        it "restarts the network using ifdown/ifup" do
+          expect(cap).to receive(:restart_each_interface).
+            with(machine, anything)
+          cap.change_host_name(machine, name)
+        end
+      end
+    end
+
+    it "does not set the hostname if unset" do
+      comm.stub_command("hostname -f | grep '^#{name}$'", exit_code: 0)
+      expect(cap).to_not receive(:add_hostname_to_loopback_interface)
+      expect(cap).to_not receive(:replace_host)
+      cap.change_host_name(machine, name)
+    end
+  end
+
+  describe ".restart_each_interface" do
+    let(:cap) { caps.get(:change_host_name) }
+    let(:systemd) { true }
+    let(:interfaces) { ["eth0", "eth1", "eth2"] }
+
+    before do
+      allow(cap).to receive(:systemd?).and_return(systemd)
+      allow(VagrantPlugins::GuestLinux::Cap::NetworkInterfaces).to receive(:network_interfaces).
+        and_return(interfaces)
+    end
+
+    context "with nettools" do
+      let(:systemd) { false }
+
+      it "restarts every interface" do
+        cap.send(:restart_each_interface, machine, logger)
+        expect(comm.received_commands[0]).to match(/ifdown eth0;ifup eth0/)
+        expect(comm.received_commands[1]).to match(/ifdown eth1;ifup eth1/)
+        expect(comm.received_commands[2]).to match(/ifdown eth2;ifup eth2/)
+      end
+    end
+
+    context "with systemctl" do
+      it "restarts every interface" do
+        cap.send(:restart_each_interface, machine, logger)
+        expect(comm.received_commands[0]).to match(/systemctl stop ifup@eth0.service;systemctl start ifup@eth0.service/)
+        expect(comm.received_commands[1]).to match(/systemctl stop ifup@eth1.service;systemctl start ifup@eth1.service/)
+        expect(comm.received_commands[2]).to match(/systemctl stop ifup@eth2.service;systemctl start ifup@eth2.service/)
+      end
+    end
+  end
+end
---
+++ test/unit/plugins/guests/astra/cap/configure_networks_test.rb
@@ -0,0 +1,186 @@
+require_relative "../../../../base"
+
+describe "VagrantPlugins::GuestAstra::Cap::ConfigureNetworks" do
+  let(:caps) do
+    VagrantPlugins::GuestAstra::Plugin
+      .components
+      .guest_capabilities[:astra]
+  end
+
+  let(:guest) { double("guest") }
+  let(:machine) { double("machine", guest: guest) }
+  let(:comm) { VagrantTests::DummyCommunicator::Communicator.new(machine) }
+
+
+  before do
+    allow(machine).to receive(:communicate).and_return(comm)
+  end
+
+  after do
+    comm.verify_expectations!
+  end
+
+  describe "#build_interface_entries" do
+    let(:network_0) do
+      {
+        interface: 0,
+        type: "dhcp",
+      }
+    end
+
+    let(:network_1) do
+      {
+        interface: 1,
+        type: "static",
+        ip: "33.33.33.10",
+        netmask: "255.255.0.0",
+        gateway: "33.33.0.1",
+      }
+    end
+  end
+
+  describe ".configure_networks" do
+    let(:cap) { caps.get(:configure_networks) }
+
+    before do
+      allow(guest).to receive(:capability).with(:network_interfaces)
+        .and_return(["eth1", "eth2"])
+    end
+
+    let(:network_0) do
+      {
+        interface: 0,
+        type: "dhcp",
+      }
+    end
+
+    let(:network_1) do
+      {
+        interface: 1,
+        type: "static",
+        ip: "33.33.33.10",
+        netmask: "255.255.0.0",
+        gateway: "33.33.0.1",
+      }
+    end
+
+    before do
+      allow(comm).to receive(:test).with("nmcli -t d show eth1").and_return(false)
+      allow(comm).to receive(:test).with("nmcli -t d show eth2").and_return(false)
+      allow(comm).to receive(:test).with("ps -o comm= 1 | grep systemd", {sudo: true}).and_return(false)
+      allow(comm).to receive(:test).with("systemctl -q is-active systemd-networkd.service", anything).and_return(false)
+      allow(comm).to receive(:test).with("command -v netplan").and_return(false)
+    end
+
+    it "creates and starts the networks using net-tools" do
+      cap.configure_networks(machine, [network_0, network_1])
+
+      expect(comm.received_commands[0]).to match("/sbin/ifdown 'eth1' || true")
+      expect(comm.received_commands[0]).to match("/sbin/ip addr flush dev 'eth1'")
+      expect(comm.received_commands[0]).to match("/sbin/ifdown 'eth2' || true")
+      expect(comm.received_commands[0]).to match("/sbin/ip addr flush dev 'eth2'")
+      expect(comm.received_commands[0]).to match("/sbin/ifup 'eth1'")
+      expect(comm.received_commands[0]).to match("/sbin/ifup 'eth2'")
+    end
+
+    context "with systemd" do
+      before do
+        expect(comm).to receive(:test).with("ps -o comm= 1 | grep systemd", {sudo: true}).and_return(true)
+        allow(comm).to receive(:test).with("command -v netplan").and_return(false)
+      end
+
+      it "creates and starts the networks using net-tools" do
+        cap.configure_networks(machine, [network_0, network_1])
+
+        expect(comm.received_commands[0]).to match("/sbin/ifdown 'eth1' || true")
+        expect(comm.received_commands[0]).to match("/sbin/ip addr flush dev 'eth1'")
+        expect(comm.received_commands[0]).to match("/sbin/ifdown 'eth2' || true")
+        expect(comm.received_commands[0]).to match("/sbin/ip addr flush dev 'eth2'")
+        expect(comm.received_commands[0]).to match("/sbin/ifup 'eth1'")
+        expect(comm.received_commands[0]).to match("/sbin/ifup 'eth2'")
+      end
+
+      context "with systemd-networkd" do
+        let(:net_conf_dhcp) { "[Match]\nName=eth1\n[Network]\nDHCP=yes" }
+        let(:net_conf_static) { "[Match]\nName=eth2\n[Network]\nDHCP=no\nAddress=33.33.33.10/16\nGateway=33.33.0.1" }
+
+        before do
+          expect(comm).to receive(:test).with("systemctl -q is-active systemd-networkd.service", anything).and_return(true)
+        end
+
+        it "creates and starts the networks using systemd-networkd" do
+          cap.configure_networks(machine, [network_0, network_1])
+
+          expect(comm.received_commands[0]).to match("mv -f '/tmp/vagrant-network-entry.*' '/etc/systemd/network/.*network'")
+          expect(comm.received_commands[0]).to match("chown")
+          expect(comm.received_commands[0]).to match("chmod")
+          expect(comm.received_commands[2]).to match("systemctl restart")
+        end
+
+        it "properly configures DHCP and static IPs if defined" do
+          expect(cap).to receive(:upload_tmp_file).with(comm, net_conf_dhcp)
+          expect(cap).to receive(:upload_tmp_file).with(comm, net_conf_static)
+
+          cap.configure_networks(machine, [network_0, network_1])
+
+          expect(comm.received_commands[0]).to match("mkdir -p /etc/systemd/network")
+          expect(comm.received_commands[0]).to match("mv -f '' '/etc/systemd/network/50-vagrant-eth1.network'")
+          expect(comm.received_commands[0]).to match("chown root:root '/etc/systemd/network/50-vagrant-eth1.network'")
+          expect(comm.received_commands[0]).to match("chmod 0644 '/etc/systemd/network/50-vagrant-eth1.network'")
+          expect(comm.received_commands[2]).to match("systemctl restart")
+        end
+      end
+
+      context "with netplan" do
+        before do
+          expect(comm).to receive(:test).with("command -v netplan").and_return(true)
+        end
+
+        let(:nm_yml) { "---\nnetwork:\n  version: 2\n  renderer: NetworkManager\n  ethernets:\n    eth1:\n      dhcp4: true\n    eth2:\n      addresses:\n      - 33.33.33.10/16\n      gateway4: 33.33.0.1\n" }
+        let(:networkd_yml) { "---\nnetwork:\n  version: 2\n  renderer: networkd\n  ethernets:\n    eth1:\n      dhcp4: true\n    eth2:\n      addresses:\n      - 33.33.33.10/16\n      gateway4: 33.33.0.1\n" }
+
+        it "uses NetworkManager if detected on device" do
+          allow(cap).to receive(:systemd_networkd?).and_return(false)
+          allow(cap).to receive(:nmcli?).and_return(true)
+          allow(cap).to receive(:nm_controlled?).and_return(true)
+          allow(comm).to receive(:test).with("nmcli -t d show eth1").and_return(true)
+          allow(comm).to receive(:test).with("nmcli -t d show eth2").and_return(true)
+
+          expect(cap).to receive(:upload_tmp_file).with(comm, nm_yml)
+            .and_return("/tmp/vagrant-network-entry.1234")
+
+          cap.configure_networks(machine, [network_0, network_1])
+
+
+          expect(comm.received_commands[0]).to match("mv -f '/tmp/vagrant-network-entry.*' '/etc/netplan/.*.yaml'")
+          expect(comm.received_commands[0]).to match("chown")
+          expect(comm.received_commands[0]).to match("chmod")
+          expect(comm.received_commands[0]).to match("netplan apply")
+        end
+
+        it "raises and error if NetworkManager is detected on device but nmcli is not installed" do
+          allow(cap).to receive(:systemd_networkd?).and_return(true)
+          allow(cap).to receive(:nmcli?).and_return(false)
+          allow(cap).to receive(:nm_controlled?).and_return(true)
+          allow(comm).to receive(:test).with("nmcli -t d show eth1").and_return(true)
+          allow(comm).to receive(:test).with("nmcli -t d show eth2").and_return(true)
+
+          expect { cap.configure_networks(machine, [network_0, network_1]) }.to raise_error(Vagrant::Errors::NetworkManagerNotInstalled)
+        end
+
+        it "creates and starts the networks for systemd with netplan" do
+          allow(cap).to receive(:systemd_networkd?).and_return(true)
+          expect(cap).to receive(:upload_tmp_file).with(comm, networkd_yml)
+            .and_return("/tmp/vagrant-network-entry.1234")
+
+          cap.configure_networks(machine, [network_0, network_1])
+
+          expect(comm.received_commands[0]).to match("mv -f '/tmp/vagrant-network-entry.*' '/etc/netplan/.*.yaml'")
+          expect(comm.received_commands[0]).to match("chown")
+          expect(comm.received_commands[0]).to match("chmod")
+          expect(comm.received_commands[0]).to match("netplan apply")
+        end
+      end
+    end
+  end
+end
