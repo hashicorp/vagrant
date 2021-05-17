ab1d9ab80fa2b29dcc9b737eefdcd6badf713330
Chris Roberts <croberts@hashicorp.com>
2021-05-17 16:48:49 -0700

    Provider broker via actual method. Fix scope of exception logger.

--- plugins/commands/serve/util.rb
+++ plugins/commands/serve/util.rb
@@ -4,11 +4,10 @@ module VagrantPlugins
       # Requires a broker to be set when initializing an
       # instance and adds an accessor to the broker
       module HasBroker
-        def self.prepended(klass)
-          klass.class_eval do
-            attr_reader :broker
-          end
+        def broker
+          @broker
         end
+
         def initialize(*args, **opts, &block)
           @broker = opts.delete(:broker)
           raise ArgumentError,
@@ -16,20 +15,20 @@ module VagrantPlugins
           super
         end
       end
-    end
 
-    # Adds exception logging to all public instance methods
-    module ExceptionLogger
-      def self.prepended(klass)
-        klass.const_set(:LOGGER, Log4r::Logger.new(klass.name.downcase))
-        klass.public_instance_methods(false).each do |m_name|
-          klass.define_method(m_name) do |*args, **opts, &block|
-            begin
-              super(*args, **opts, &block)
-            rescue => err
-              Logger.error(err)
-              Logger.debug("#{err.class}: #{err}\n#{err.backtrace.join("\n")}")
-              raise
+      # Adds exception logging to all public instance methods
+      module ExceptionLogger
+        def self.prepended(klass)
+          klass.const_set(:LOGGER, Log4r::Logger.new(klass.name.downcase))
+          klass.public_instance_methods(false).each do |m_name|
+            klass.define_method(m_name) do |*args, **opts, &block|
+              begin
+                super(*args, **opts, &block)
+              rescue => err
+                self.class.const_get(:LOGGER).error(err)
+                self.class.const_get(:LOGGER).debug("#{err.class}: #{err}\n#{err.backtrace.join("\n")}")
+                raise
+              end
             end
           end
         end
