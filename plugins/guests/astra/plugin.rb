a68a40e79b40038c8a33c778e497c3071ec401b0
Andrey Kirillov <akirillov@astralinux.ru>
2021-06-28 16:56:05 +0300

    Astra Linux defined as a Debian-based distribution

--- plugins/guests/astra/plugin.rb
+++ plugins/guests/astra/plugin.rb
@@ -6,34 +6,9 @@ module VagrantPlugins
       name "Astra Linux guest"
       description "Astra Linux guest support."
 
-      guest(:astra, :linux) do
+      guest(:astra, :debian) do
         require_relative "guest"
         Guest
-      end
-
-      guest_capability(:astra, :configure_networks) do
-        require_relative "cap/configure_networks"
-        Cap::ConfigureNetworks
-      end
-
-      guest_capability(:astra, :change_host_name) do
-        require_relative "cap/change_host_name"
-        Cap::ChangeHostName
-      end
-
-      guest_capability(:astra, :nfs_client_install) do
-        require_relative "cap/nfs"
-        Cap::NFS
-      end
-
-      guest_capability(:astra, :rsync_install) do
-        require_relative "cap/rsync"
-        Cap::RSync
-      end
-
-      guest_capability(:astra, :smb_install) do
-        require_relative "cap/smb"
-        Cap::SMB
       end
     end
   end
