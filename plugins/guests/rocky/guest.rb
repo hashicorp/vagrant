a8c5980afbdc69dbf6d0870fe4cf2560076aba78
Chris Roberts <croberts@hashicorp.com>
2021-07-02 15:32:48 -0700

    Add Rocky Linux guest support

---
+++ plugins/guests/rocky/guest.rb
@@ -0,0 +1,10 @@
+require_relative "../linux/guest"
+
+module VagrantPlugins
+  module GuestRocky
+    class Guest < VagrantPlugins::GuestLinux::Guest
+      # Name used for guest detection
+      GUEST_DETECTION_NAME = "rocky".freeze
+    end
+  end
+end
---
+++ plugins/guests/rocky/plugin.rb
@@ -0,0 +1,20 @@
+require "vagrant"
+
+module VagrantPlugins
+  module GuestRocky
+    class Plugin < Vagrant.plugin("2")
+      name "Rocky guest"
+      description "Rocky guest support."
+
+      guest(:rocky, :redhat) do
+        require_relative "guest"
+        Guest
+      end
+
+      guest_capability(:rocky, :flavor) do
+        require_relative "cap/flavor"
+        Cap::Flavor
+      end
+    end
+  end
+end
