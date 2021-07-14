1a9f0821944642aebb6a20a10beeffa3516a9c22
sophia <scastellarin95@gmail.com>
2021-07-14 16:06:58 -0500

    Save a machine as a record inside a target

--- internal/core/machine.go
+++ internal/core/machine.go
@@ -1,11 +1,19 @@
 package core
 
 import (
+	"reflect"
+
+	"github.com/golang/protobuf/ptypes"
+
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vagrant-plugin-sdk/core"
+	"github.com/hashicorp/vagrant/internal/server/proto/vagrant_server"
 )
 
 type Machine struct {
 	*Target
+	machine *vagrant_server.Target_Machine
+	logger  hclog.Logger
 }
 
 // Close implements core.Machine
@@ -15,17 +23,18 @@ func (m *Machine) Close() (err error) {
 
 // ID implements core.Machine
 func (m *Machine) ID() (id string, err error) {
-	return m.target.Uuid, nil
+	return m.machine.Id, nil
 }
 
 // SetID implements core.Machine
 func (m *Machine) SetID(value string) (err error) {
-	m.target.Uuid = value
-	return m.Save()
+	m.machine.Id = value
+	return m.SaveMachine()
 }
 
 // Box implements core.Machine
 func (m *Machine) Box() (b core.Box, err error) {
+	// TODO: need Vagrantfile
 	return
 }
 
@@ -42,41 +51,59 @@ func (m *Machine) GetUUID() (id string, err error) {
 // SetUUID implements core.Machine
 func (m *Machine) SetUUID(id string) (err error) {
 	m.target.Uuid = id
-	return m.Save()
+	return m.SaveMachine()
 }
 
 // Inspect implements core.Machine
 func (m *Machine) Inspect() (printable string, err error) {
+	name, err := m.Name()
+	provider, err := m.Provider()
+	printable = "#<" + reflect.TypeOf(m).String() + ": " + name + " (" + reflect.TypeOf(provider).String() + ")>"
 	return
 }
 
 // Reload implements core.Machine
 func (m *Machine) Reload() (err error) {
+	// TODO
 	return
 }
 
 // ConnectionInfo implements core.Machine
 func (m *Machine) ConnectionInfo() (info *core.ConnectionInfo, err error) {
+	// TODO: need Vagrantfile
 	return
 }
 
 // MachineState implements core.Machine
 func (m *Machine) MachineState() (state *core.MachineState, err error) {
+	// TODO: need provider
 	return
 }
 
 // SetMachineState implements core.Machine
 func (m *Machine) SetMachineState(state *core.MachineState) (err error) {
-	return
+	// TODO: maybe this should come from the machine
+	s := vagrant_server.Operation_PhysicalState_value[state.ID]
+	m.target.State = vagrant_server.Operation_PhysicalState(s)
+	return m.SaveMachine()
 }
 
-func (m *Machine) UID() (userId int32, err error) {
-	return
+func (m *Machine) UID() (userId string, err error) {
+	return m.machine.Uid, nil
 }
 
 // SyncedFolders implements core.Machine
 func (m *Machine) SyncedFolders() (folders []core.SyncedFolder, err error) {
 	return
+}
+
+func (m *Machine) SaveMachine() (err error) {
+	m.logger.Debug("saving machine to db", "machine", m.machine.Id)
+	m.target.Record, err = ptypes.MarshalAny(m.machine)
+	if err != nil {
+		return nil
+	}
+	return m.Save()
 }
 
 var _ core.Machine = (*Machine)(nil)
