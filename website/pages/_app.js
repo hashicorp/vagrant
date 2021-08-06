c72f1aa1a7fc870a8d0b2976aad59f953351141f
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-06 14:37:01 -0400

    website: use hideOnMobile for alert banner

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -35,7 +35,7 @@ export default function App({ Component, pageProps }) {
         icon={[{ href: '/favicon.ico' }]}
       />
       {ALERT_BANNER_ACTIVE && (
-        <AlertBanner {...alertBannerData} product="vagrant" />
+        <AlertBanner {...alertBannerData} product="vagrant" hideOnMobile />
       )}
       <HashiStackMenu />
       <ProductSubnav />
