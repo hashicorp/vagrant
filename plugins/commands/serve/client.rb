d48ec9ace292970763da027e9e706cbb2fa0b94f
Chris Roberts <croberts@hashicorp.com>
2021-08-13 13:40:10 -0700

    Update index autoload entry and target autoload

--- plugins/commands/serve/client.rb
+++ plugins/commands/serve/client.rb
@@ -2,14 +2,16 @@ module VagrantPlugins
   module CommandServe
     module Client
       # Simple alias
+      Empty = Google::Protobuf::Empty
       SDK = Service::SDK
       SRV = Service::SRV
       ServiceInfo = Service::ServiceInfo
 
       autoload :Machine, Vagrant.source_root.join("plugins/commands/serve/client/machine").to_s
+      autoload :TargetIndex, Vagrant.source_root.join("plugins/commands/serve/client/target_index").to_s
       autoload :Project, Vagrant.source_root.join("plugins/commands/serve/client/project").to_s
+      autoload :Target, Vagrant.source_root.join("plugins/commands/serve/client/target").to_s
       autoload :Terminal, Vagrant.source_root.join("plugins/commands/serve/client/terminal").to_s
-      autoload :MachineIndex, Vagrant.source_root.join("plugins/commands/serve/client/machine_index").to_s
     end
   end
 end
