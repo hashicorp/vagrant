04222f0eb606043cc921e96b848943001f4b99ce
sophia <scastellarin95@gmail.com>
2021-08-27 11:43:15 -0500

    Define initialize function to be compatible with ruby 2.6

--- plugins/commands/serve/service/guest_service.rb
+++ plugins/commands/serve/service/guest_service.rb
@@ -9,6 +9,10 @@ module VagrantPlugins
         prepend Util::ExceptionLogger
         LOGGER  = Log4r::Logger.new("vagrant::command::serve::guest")
 
+        def initialize(*args, **opts, &block)
+          super()
+        end
+
         def detect_spec(*_)
           SDK::FuncSpec.new(
             name: "detect_spec",
@@ -30,8 +34,7 @@ module VagrantPlugins
         def detect(req, ctx)
           ServiceInfo.with_info(ctx) do |info|
             plugin_name = info.plugin_name
-            target = mapper.funcspec_map(req.args)
-
+            target = mapper.funcspec_map(req)
             project = target.project
             env = Vagrant::Environment.new({client: project})
             machine = env.machine(target.name.to_sym, target.provider_name.to_sym)
