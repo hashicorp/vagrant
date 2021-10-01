35277eff21d198007f454ddae058046f26865410
Chris Roberts <croberts@hashicorp.com>
2021-10-01 12:18:37 -0700

    Remove machine autoload from client top level

--- plugins/commands/serve/client.rb
+++ plugins/commands/serve/client.rb
@@ -4,7 +4,6 @@ module VagrantPlugins
       autoload :CapabilityPlatform, Vagrant.source_root.join("plugins/commands/serve/client/capability_platform").to_s
       autoload :Guest, Vagrant.source_root.join("plugins/commands/serve/client/guest").to_s
       autoload :Host, Vagrant.source_root.join("plugins/commands/serve/client/host").to_s
-      autoload :Machine, Vagrant.source_root.join("plugins/commands/serve/client/machine").to_s
       autoload :TargetIndex, Vagrant.source_root.join("plugins/commands/serve/client/target_index").to_s
       autoload :Project, Vagrant.source_root.join("plugins/commands/serve/client/project").to_s
       autoload :Target, Vagrant.source_root.join("plugins/commands/serve/client/target").to_s
