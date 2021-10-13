678ffa60dc17a5df2b215c0acea1b16f44370575
Chris Roberts <croberts@hashicorp.com>
2021-10-13 11:39:50 -0700

    Add state bag argument to dummy capability

--- plugins/hosts/void/cap/dummy.rb
+++ plugins/hosts/void/cap/dummy.rb
@@ -2,9 +2,9 @@ module VagrantPlugins
   module HostVoid
     module Cap
       class Dummy
-        def self.dummy(ui, argument)
-          ui.info "Dummy host cap in ruby runtime, sent argument: #{argument}"
-          true
+        def self.dummy(bag, ui, argument)
+          ui.info "Dummy host cap in ruby runtime, sent argument: `#{argument}' with bag: #{bag}"
+          "this is a result value string!"
         end
       end
     end
