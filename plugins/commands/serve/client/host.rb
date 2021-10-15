ec669645ad6cc4e095023d0437a46da9dc4fa3b8
Chris Roberts <croberts@hashicorp.com>
2021-10-15 10:06:01 -0700

    Use reader to access logger

--- plugins/commands/serve/client/host.rb
+++ plugins/commands/serve/client/host.rb
@@ -11,12 +11,12 @@ module VagrantPlugins
 
         # @return [<String>] parents
         def parents
-          @logger.debug("getting parents")
+          logger.debug("getting parents")
           req = SDK::FuncSpec::Args.new(
             args: []
           )
           res = client.parents(req)
-          @logger.debug("got parents #{res}")
+          logger.debug("got parents #{res}")
           res.parents
         end
       end
