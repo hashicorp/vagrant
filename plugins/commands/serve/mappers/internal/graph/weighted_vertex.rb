0a20c9d6650e2c587db44045ad62af3073c1aa89
Chris Roberts <croberts@hashicorp.com>
2021-10-22 16:07:02 -0700

    Update weighted vertex to be a decorator

--- plugins/commands/serve/mappers/internal/graph/weighted_vertex.rb
+++ plugins/commands/serve/mappers/internal/graph/weighted_vertex.rb
@@ -1,3 +1,6 @@
+require "delegate"
+require "forwardable"
+
 module VagrantPlugins
   module CommandServe
     class Mappers
@@ -8,17 +11,30 @@ module VagrantPlugins
           # preference. All vertices within a graph are
           # WeightedVertex instances. Paths with the
           # lowest weights are preferred.
-          class WeightedVertex
-            attr_reader :hash_code
+          class WeightedVertex < SimpleDelegator
+
+            # Force the delegator to properly look like the
+            # vertex its decorating
+            extend Forwardable
+            def_delegators :__getobj__, :is_a?, :kind_of?, :class
+
             attr_reader :weight
 
-            def initialize(code:, weight:)
-              @hash_code = code
-              if !weight.is_a?(Integer)
+            def initialize(vertex, weight:)
+              if !vertex.is_a?(Vertex)
                 raise TypeError,
-                  "Expected `Integer' type for weight, got `#{weight.class}'"
+                  "Expected `Vertex' type, got `#{vertex.class}'"
               end
-              @weight = weight
+              self.weight = weight
+              super(vertex)
+            end
+
+            def weight=(w)
+              if !w.is_a?(Integer)
+                raise TypeError,
+                  "Expected `Integer' type for weight, got `#{w.class}'"
+              end
+              @weight = w
             end
           end
         end
