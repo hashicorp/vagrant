2712046a9d989d41ba86f0582b32fab376a47514
Chris Roberts <croberts@hashicorp.com>
2021-10-29 09:02:11 -0700

    Add mapper for direct type to proto

--- plugins/commands/serve/mappers/direct.rb
+++ plugins/commands/serve/mappers/direct.rb
@@ -39,6 +39,28 @@ module VagrantPlugins
           end
         end
       end
+
+      class DirectToProto < Mapper
+        def initialize
+          inputs = [].tap do |i|
+            i << Input.new(type: Types::Direct)
+            i << Input.new(type: Mappers)
+          end
+          super(inputs: inputs, output: SDK::Args::Direct, func: method(:converter))
+        end
+
+        def converter(d, mappers)
+          args = d.args.map do |a|
+            begin
+              mappers.map(a, mappers, to: Google::Protobuf::Any)
+            rescue => err
+              raise "Failed to map value #{a} - #{err}\n#{err.backtrace.join("\n")}"
+            end
+
+          end
+          SDK::Args::Direct.new(list: args)
+        end
+      end
     end
   end
 end
