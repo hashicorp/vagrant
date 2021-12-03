7111294ec7473b3521970c36cfec0978667bcc0e
Collin McNeese <cmcneese@chef.io>
2021-12-02 22:23:57 -0600

    updates Chef provisioners for Cinc install verification
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- plugins/provisioners/chef/cap/omnios/chef_installed.rb
+++ plugins/provisioners/chef/cap/omnios/chef_installed.rb
@@ -7,9 +7,14 @@ module VagrantPlugins
           # Check if Chef is installed at the given version.
           # @return [true, false]
           def self.chef_installed(machine, product, version)
-            product_name = product == 'chef-workstation' ? 'chef-workstation' : 'chef'
-            verify_bin = product_name == 'chef-workstation' ? 'chef' : 'chef-client'
-            verify_path = "/opt/#{product_name}/bin/#{verify_bin}"
+            verify_bin =  case product
+                          when 'chef-workstation' then 'chef'
+                          when 'cinc-workstation' then 'cinc'
+                          when 'cinc' then 'cinc-client'
+                          else 'chef-client'
+                          end
+
+            verify_path = "/opt/#{product}/bin/#{verify_bin}"
             command = "test -x #{verify_path}"
 
             if version != :latest
--- plugins/provisioners/chef/cap/windows/chef_installed.rb
+++ plugins/provisioners/chef/cap/windows/chef_installed.rb
@@ -6,7 +6,13 @@ module VagrantPlugins
           # Check if Chef is installed at the given version.
           # @return [true, false]
           def self.chef_installed(machine, product, version)
-            verify_bin = product == 'chef-workstation' ? 'chef' : 'chef-client'
+            verify_bin =  case product
+                          when 'chef-workstation' then 'chef'
+                          when 'cinc-workstation' then 'cinc'
+                          when 'cinc' then 'cinc-client'
+                          else 'chef-client'
+                          end
+
             if version != :latest
               command = 'if ((&' + verify_bin + ' --version) -Match "' + version.to_s + '"){ exit 0 } else { exit 1 }'
             else
