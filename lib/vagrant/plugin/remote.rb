e951c4d9719dc9f2e16dc879e6799212f7c20eb0
sophia <scastellarin95@gmail.com>
2021-12-06 10:29:52 -0600

    Add remote plugin manager

--- lib/vagrant/plugin/remote.rb
+++ lib/vagrant/plugin/remote.rb
@@ -4,6 +4,7 @@ module Vagrant
   module Plugin
     module Remote
       autoload :Communicator, "vagrant/plugin/remote/communicator"
+      autoload :Manager, "vagrant/plugin/remote/manager"
       autoload :Plugin, "vagrant/plugin/remote/plugin"
       autoload :SyncedFolder, "vagrant/plugin/remote/synced_folder"
     end
