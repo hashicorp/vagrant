2bd9c995e90a3775a112557d9e779e96cfe024f5
sophia <scastellarin95@gmail.com>
2021-12-10 16:36:28 -0600

    Don't include folder if it is disabled

--- lib/vagrant/machine/remote.rb
+++ lib/vagrant/machine/remote.rb
@@ -374,6 +374,7 @@ module Vagrant
         folders = Vagrant::Plugin::V2::SyncedFolder::Collection.new
         synced_folder_clients = client.synced_folders
         synced_folder_clients.each do |f|
+          next if f[:folder][:disabled]
           # TODO: get type of synced folder
           impl = "virtualbox"
           sf = Vagrant::Plugin::V2::SyncedFolder.new(f[:plugin])
