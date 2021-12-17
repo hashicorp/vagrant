0989edb7946cadd8d928d091fed501c9c0de3126
Chris Roberts <croberts@hashicorp.com>
2021-12-17 14:59:46 -0800

    Update with_info to require broker and load plugin manager when available

--- plugins/commands/serve/util/service_info.rb
+++ plugins/commands/serve/util/service_info.rb
@@ -3,12 +3,23 @@ module VagrantPlugins
     module Util
       # Adds service info helper to be used with services
       module ServiceInfo
-        def with_info(context)
+        def with_info(context, broker:, &block)
           if !context.metadata["plugin_name"]
             raise KeyError,
-              "plugin name not defined: #{context.metadata.inspect}"
+              "plugin name not defined (metadata content: #{context.metadata.inspect})"
           end
-          info = Service::ServiceInfo.new(plugin_name: context.metadata['plugin_name'])
+
+          info = Service::ServiceInfo.new(
+            plugin_name: context.metadata["plugin_name"],
+            broker: broker
+          )
+          if context.metadata["plugin_manager"] && info.broker
+            info.plugin_manager = Client::PluginManager.load(
+              context.metadata["plugin_manager"],
+              broker: info.broker
+            )
+            Vagrant.plugin("2").enable_remote_manager
+          end
           Thread.current.thread_variable_set(:service_info, info)
           return if !block_given?
           begin
@@ -16,6 +27,7 @@ module VagrantPlugins
           rescue => e
             raise "#{e.class}: #{e}\n#{e.backtrace.join("\n")}"
           ensure
+            Vagrant.plugin("2").disable_remote_manager
             Thread.current.thread_variable_set(:service_info, nil)
           end
         end
