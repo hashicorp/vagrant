d734f849fb043ec2e616057de56f32a2b3db7e82
jaromaz <jm@iq.pl>
2022-01-10 11:11:44 +0100

    Add guest definition for EuroLinux

--- plugins/guests/redhat/cap/flavor.rb
+++ plugins/guests/redhat/cap/flavor.rb
@@ -10,9 +10,9 @@ module VagrantPlugins
           end
 
           # Detect various flavors we care about
-          if output =~ /(Red Hat Enterprise|Scientific|Cloud|Virtuozzo)\s*Linux( .+)? release 7/i
+          if output =~ /(Red Hat Enterprise|Scientific|Cloud|Virtuozzo|Euro)\s*Linux( .+)? release 7/i
             return :rhel_7
-          elsif output =~ /(Red Hat Enterprise|Scientific|Cloud|Virtuozzo)\s*Linux( .+)? release 8/i
+          elsif output =~ /(Red Hat Enterprise|Scientific|Cloud|Virtuozzo|Euro)\s*Linux( .+)? release 8/i
             return :rhel_8
           else
             return :rhel
