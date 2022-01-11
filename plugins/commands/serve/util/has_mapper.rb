53d15203f92c36f84f49c3533d71af4245fb1830
Chris Roberts <croberts@hashicorp.com>
2022-01-11 10:19:39 -0800

    Remove any mapper setup in the initializer

--- plugins/commands/serve/util/has_mapper.rb
+++ plugins/commands/serve/util/has_mapper.rb
@@ -4,27 +4,16 @@ module VagrantPlugins
       # Adds mapper initialization and will include
       module HasMapper
         def mapper
+          return @mapper if @mapper
+          @mapper = Mappers.new
+          if respond_to?(:broker) && broker
+            @mapper.add_argument(broker)
+          end
           if !Thread.current.thread_variable_get(:cacher)
             Thread.current.thread_variable_set(:cacher, Cacher.new)
           end
           @mapper.cacher = Thread.current.thread_variable_get(:cacher)
           @mapper
-        end
-
-        def initialize(*args, **opts, &block)
-          @mapper = Mappers.new
-          if respond_to?(:broker)
-            @mapper.add_argument(broker)
-          end
-
-          sup = self.method(:initialize).super_method
-          if sup.parameters.empty?
-            super()
-          elsif !opts.empty? && sup.parameters.detect{ |type, _| type == :keyreq || type == :keyrest }
-            super
-          else
-            super(*args, &block)
-          end
         end
       end
     end
