58dd2e491fd5df66b153dce019d2a379095bfd43
Chris Roberts <croberts@hashicorp.com>
2022-01-21 13:52:12 -0800

    Update cacher implementation

--- plugins/commands/serve/util/cacher.rb
+++ plugins/commands/serve/util/cacher.rb
@@ -1,18 +1,23 @@
-require "monitor"
+require "mutex_m"
 
 module VagrantPlugins
   module CommandServe
     module Util
       class Cacher
-        include MonitorMixin
+        include Mutex_m
 
         class Entry
-          include MonitorMixin
+          include Mutex_m
           attr_reader :value
 
           def initialize(value)
             super()
             @value = value
+          end
+
+          def value
+            raise "Entry must be locked to access value" if !mu_locked?
+            @value
           end
         end
 
@@ -22,23 +27,23 @@ module VagrantPlugins
         end
 
         def registered?(key)
-          synchronize { @registry.key?(key) }
+          mu_synchronize { @registry.key?(key) }
         end
 
         def []=(key, value)
           entry = Entry.new(value)
-          synchronize { @registry[key] = entry }
+          mu_synchronize { @registry[key] = entry }
         end
 
         def [](key)
-          synchronize { @registry[key]&.value }
+          mu_synchronize { @registry[key] }
         end
 
         def delete(key)
           entry = @registry[key]
           return if entry.nil?
-          entry.synchronize do
-            value = @registry[key].value
+          entry.mu_synchronize do
+            value = entry.value
             @registry.delete(key)
             value
           end
@@ -49,15 +54,40 @@ module VagrantPlugins
         #       and then return it without needing to deal with
         #       block wrapping to maintain the lock.
         def use(key)
-          entry = @registry[key]
+          entry = self[key]
           if entry.nil?
             raise KeyError,
               "No value cached with key `#{key}'"
           end
 
-          entry.synchronize do
+          entry.mu_synchronize do
             yield entry.value
           end
+        end
+
+        def checkout(key, wait: false)
+          entry = self[key]
+          if entry.nil?
+            raise KeyError,
+              "No value cached with key `#{key}'"
+          end
+          if wait
+            entry.mu_lock
+            entry.value
+          else
+            if !entry.mu_try_lock
+              raise LockError,
+                "Failed to lock cached entry with key `#{key}'"
+            end
+            entry.value
+          end
+        end
+
+        def checkin(key)
+          entry = self[key]
+          return if entry.nil?
+          entry.mu_unlock
+          nil
         end
       end
     end
