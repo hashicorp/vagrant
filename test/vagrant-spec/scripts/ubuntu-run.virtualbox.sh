cba6b76a26e217f887eb76a5f4893c2916196ec0
sophia <scastellarin95@gmail.com>
2022-02-01 17:06:20 -0600

    Run vagrant-spec tests

--- test/vagrant-spec/scripts/ubuntu-run.virtualbox.sh
+++ test/vagrant-spec/scripts/ubuntu-run.virtualbox.sh
@@ -3,7 +3,7 @@ set -x
 
 export VAGRANT_EXPERIMENTAL="${VAGRANT_EXPERIMENTAL:-1}"
 export VAGRANT_SPEC_BOX="${VAGRANT_SPEC_BOX}"
-vagrant vagrant-spec ${VAGRANT_SPEC_ARGS} /vagrant/test/vagrant-spec/configs/vagrant-spec.config.virtualbox.rb
+vagrant-spec ${VAGRANT_SPEC_ARGS} --config /vagrant/test/vagrant-spec/configs/vagrant-spec.config.virtualbox.rb
 result=$?
 
 exit $result
