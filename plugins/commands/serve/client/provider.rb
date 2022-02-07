9e58090d09e084a65aa86704cb608389d817937b
Chris Roberts <croberts@hashicorp.com>
2022-02-07 14:40:45 -0800

    Update the provider client to use funcspec utility

--- plugins/commands/serve/client/provider.rb
+++ plugins/commands/serve/client/provider.rb
@@ -2,87 +2,120 @@ require "google/protobuf/well_known_types"
 
 module VagrantPlugins
   module CommandServe
-    module Client
-      class Provider
-        prepend Util::ClientSetup
-        prepend Util::HasLogger
-
+    class Client
+      class Provider < Client
         include CapabilityPlatform
-        include Util::HasSeeds::Client
+
+        # Generate callback and spec for required arguments
+        #
+        # @return [SDK::FuncSpec, Proc]
+        def usable_func
+          spec = client.usable_spec(Empty.new)
+          cb = proc do |args|
+            client.usable(args).is_usable
+          end
+          [spec, cb]
+        end
 
         # @return [Boolean] is the provider usable
         def usable?
-          req = SDK::FuncSpec::Args.new(
-            args: []
-          )
-          res = client.usable(req)
-          res.is_usable
+          run_func
+        end
+
+        # Generate callback and spec for required arguments
+        #
+        # @return [SDK::FuncSpec, Proc]
+        def installed_func
+          spec = client.installed_spec(Empty.new)
+          cb = proc do |args|
+            client.installed(args).is_installed
+          end
+          [spec, cb]
         end
 
         # @return [Boolean] is the provider installed
         def installed?
-          req = SDK::FuncSpec::Args.new(
-            args: []
+          run_func
+        end
+
+        # Generate callback and spec for required arguments
+        #
+        # @param name [String, Symbol] name of action
+        # @return [SDK::FuncSpec, Proc]
+        def action_func(name)
+          name = name.to_s
+          spec = client.action_spec(
+            SDK::Provider::ActionRequest.new(
+              name: name
+            )
           )
-          res = client.installed(req)
-          res.is_installed
+          cb = proc do |args|
+            client.action(
+              SDK::Provider::ActionRequest.new(
+                name: name,
+                func_args: args,
+              )
+            )
+          end
+          [spec, cb]
         end
 
         # @param [Sdk::Args::Machine]
         # @param [Symbol] name of the action to run
         def action(machine, name)
-          arg_protos = seed_protos
-          d = Type::Direct.new(arguments: [machine])
-          da = mapper.map(d, to: Google::Protobuf::Any)
-          arg_protos << SDK::FuncSpec::Value.new(
-            name: "",
-            type: "hashicorp.vagrant.sdk.Args.Direct",
-            value: Google::Protobuf::Any.pack(da),
-          )
-          req = SDK::Provider::ActionRequest.new(
-            name: name.to_s,
-            func_args: SDK::FuncSpec::Args.new(
-              args: arg_protos,
-            )
-          )
-          client.action(req)
+          run_func(Type::Direct.new(value: [machine]), func_args: name)
+        end
+
+        # Generate callback and spec for required arguments
+        #
+        # @return [SDK::FuncSpec, Proc]
+        def machine_id_changed_func
+          spec = client.machine_id_changed_spec(Empty.new)
+          cb = proc do |args|
+            client.machine_id_changed(args)
+          end
+          [spec, cb]
         end
 
         # @param [Sdk::Args::Machine]
         def machine_id_changed(machine)
-          args = seed_protos
-          args << SDK::FuncSpec::Value.new(
-              type: "hashicorp.vagrant.sdk.Args.Target.Machine",
-              value: Google::Protobuf::Any.pack(machine),
-          )
-          req = SDK::FuncSpec::Args.new(args: args)
-          client.machine_id_changed(req)
+          run_func(machine)
+        end
+
+        # Generate callback and spec for required arguments
+        #
+        # @return [SDK::FuncSpec, Proc]
+        def ssh_info_func
+          spec = client.ssh_info_spec(Empty.new)
+          cb = proc do |args|
+            Vagrant::Util::HashWithIndifferentAccess.new(
+              client.ssh_info(args).to_h
+            )
+          end
+          [spec, cb]
         end
 
         # @param [Sdk::Args::Machine]
         # @return [Hash] ssh info for machine
         def ssh_info(machine)
-          args = seed_protos
-          args << SDK::FuncSpec::Value.new(
-              type: "hashicorp.vagrant.sdk.Args.Target.Machine",
-              value: Google::Protobuf::Any.pack(machine),
-          )
-          req = SDK::FuncSpec::Args.new(args: args)
-          machine_ssh_info = client.ssh_info(req)
-          machine_ssh_info.to_h
+          run_func(machine)
+        end
+
+        # Generate callback and spec for required arguments
+        #
+        # @return [SDK::FuncSpec, Proc]
+        def state_func
+          spec = client.state_spec(Empty.new)
+          cb = proc do |args|
+            mapper.map(client.state(args), to: Vagrant::MachineState)
+          end
+          [spec, cb]
         end
 
         # @param [Sdk::Args::Machine]
         # @return [Vagrant::MachineState] machine state
         def state(machine)
-          args = seed_protos
-          args << SDK::FuncSpec::Value.new(
-              type: "hashicorp.vagrant.sdk.Args.Target.Machine",
-              value: Google::Protobuf::Any.pack(machine),
-          )
-          req = SDK::FuncSpec::Args.new(args: args)
-          machine_state = client.state(req)
-          mapper.map(machine_state, to: Vagrant::MachineState)
+          run_func(machine)
         end
       end
     end
