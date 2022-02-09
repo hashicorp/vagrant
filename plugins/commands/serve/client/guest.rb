29eb55909c7c263f4471a42a1251c5f8398c3175
sophia <scastellarin95@gmail.com>
2022-02-09 11:23:24 -0600

    Refactor guest detect client

--- plugins/commands/serve/client/guest.rb
+++ plugins/commands/serve/client/guest.rb
@@ -9,26 +9,29 @@ module VagrantPlugins
         # Generate callback and spec for required arguments
         #
         # @return [SDK::FuncSpec, Proc]
+        def detect_func
+          spec = client.detect_spec(Empty.new)
+          cb = proc do |args|
+            client.detect(args).parent
+          end
+          [spec, cb]
+        end
+
+        # @param [Vagrant::Machine]
+        # @return [bool]
+        def detect(machine)
+          run_func(machine)
+        end
+
+        # Generate callback and spec for required arguments
+        #
+        # @return [SDK::FuncSpec, Proc]
         def parent_func
           spec = client.parent_spec(Empty.new)
           cb = proc do |args|
             client.parent(args).parent
           end
           [spec, cb]
-        end
-
-        # @param [Vagrant::Machine]
-        # @return [Boolean]
-        def detect(machine)
-          req = SDK::FuncSpec::Args.new(
-            args: [SDK::FuncSpec::Value.new(
-                name: "",
-                type: "hashicorp.vagrant.sdk.Args.Target.Machine",
-                value: Google::Protobuf::Any.pack(machine.to_proto)
-            )]
-          )
-          res = client.detect(req)
-          res.detected
         end
 
         # @return [String] parents
