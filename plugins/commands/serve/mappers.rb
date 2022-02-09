32ea6a45093761bd5423917cf1ad8920920e87b7
Chris Roberts <croberts@hashicorp.com>
2022-02-09 09:23:26 -0800

    Add new options type with mappers

--- plugins/commands/serve/mappers.rb
+++ plugins/commands/serve/mappers.rb
@@ -329,6 +329,7 @@ require Vagrant.source_root.join("plugins/commands/serve/mappers/guest.rb").to_s
 require Vagrant.source_root.join("plugins/commands/serve/mappers/host.rb").to_s
 require Vagrant.source_root.join("plugins/commands/serve/mappers/known_types.rb").to_s
 require Vagrant.source_root.join("plugins/commands/serve/mappers/machine.rb").to_s
+require Vagrant.source_root.join("plugins/commands/serve/mappers/options.rb").to_s
 require Vagrant.source_root.join("plugins/commands/serve/mappers/pathname.rb").to_s
 require Vagrant.source_root.join("plugins/commands/serve/mappers/plugin_manager.rb").to_s
 require Vagrant.source_root.join("plugins/commands/serve/mappers/project.rb").to_s
--- plugins/commands/serve/type.rb
+++ plugins/commands/serve/type.rb
@@ -7,6 +7,7 @@ module VagrantPlugins
       autoload :CommandInfo, Vagrant.source_root.join("plugins/commands/serve/type/command_info").to_s
       autoload :Direct, Vagrant.source_root.join("plugins/commands/serve/type/direct").to_s
       autoload :Duration, Vagrant.source_root.join("plugins/commands/serve/type/duration").to_s
+      autoload :Options, Vagrant.source_root.join("plugins/commands/serve/type/options").to_s
       autoload :NamedArgument, Vagrant.source_root.join("plugins/commands/serve/type/named_argument").to_s
       autoload :SSHInfo, Vagrant.source_root.join("plugins/commands/serve/type/ssh_info").to_s
       autoload :WinrmInfo, Vagrant.source_root.join("plugins/commands/serve/type/winrm_info").to_s
