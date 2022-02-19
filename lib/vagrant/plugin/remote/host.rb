7e9dad2b3eda684a4c2b9498bc2d675d120b10c3
Chris Roberts <croberts@hashicorp.com>
2022-02-18 16:34:33 -0800

    Add remote host plugin

---
+++ lib/vagrant/plugin/remote/host.rb
@@ -0,0 +1,23 @@
+module Vagrant
+  module Plugin
+    module Remote
+      class Host < V2::Host
+        attr_accessor :client
+
+        def initialize(*_, **kwargs)
+          @client = kwargs.delete(:client)
+          if @client.nil?
+            raise ArgumentError,
+              "Remote client is required for `#{self.class.name}`"
+          end
+          super
+        end
+
+        # @return [Boolean]
+        def detect?(env)
+          client.detect(env)
+        end
+      end
+    end
+  end
+end
