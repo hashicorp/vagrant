98385bd1c01f6d179ea178f4ad4449b6afdfcf28
Paul Hinze <phinze@phinze.com>
2022-04-05 17:34:14 -0500

    ci: Bump ulimits - gogo uses lots of open files :)

--- test/vagrant-spec/scripts/ubuntu-run.virtualbox.sh
+++ test/vagrant-spec/scripts/ubuntu-run.virtualbox.sh
@@ -7,6 +7,10 @@ export VAGRANT_SPEC_BOX="${VAGRANT_SPEC_BOX}"
 # Explicitly use Go binary
 export VAGRANT_PATH=/vagrant/vagrant
 
+# Explicitly set high open file limits... vagrant-ruby tends to run into the
+# default 1024 limit during some operations.
+ulimit -n 65535
+
 vagrant-spec ${VAGRANT_SPEC_ARGS} --config /vagrant/test/vagrant-spec/configs/vagrant-spec.config.virtualbox.rb
 result=$?
 
