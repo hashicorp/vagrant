373fc9c7b4f0e8e92c09a96b8fd40f5639a81bea
Chris Roberts <croberts@hashicorp.com>
2022-04-18 12:11:03 -0700

    Helper for adding individual seed to plugin

--- internal/core/core.go
+++ internal/core/core.go
@@ -1,5 +1,33 @@
 package core
 
+import (
+	"fmt"
+
+	"github.com/hashicorp/vagrant-plugin-sdk/core"
+)
+
 type closer interface {
 	Closer(func() error)
+}
+
+func seedPlugin(
+	plugin interface{},
+	seed interface{},
+) (err error) {
+	s, ok := plugin.(core.Seeder)
+	if !ok {
+		return fmt.Errorf("component does not implement core.Seeder")
+	}
+	seeds, err := s.Seeds()
+	if err != nil {
+		return
+	}
+
+	seeds.AddTyped(seed)
+
+	if err = s.Seed(seeds); err != nil {
+		return
+	}
+
+	return
 }
