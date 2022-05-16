02a06bca45c315dd04d27385b4ed96f768f62696
Paul Hinze <phinze@phinze.com>
2022-05-16 16:50:19 -0500

    Fixes for provider capability invocations
    It looks like I might have been the first to hit provider cabability
    invocation in testing these changes, and so I found these few missing
    methods on the client. They're just copied over from the other
    capability hosts.
    
    Calling capabilities on a provider also revealed that the wrong Machine
    type was being pulled out of the funcspec args, so we had to correct
    that too in order to get the capability calls working.

--- lib/vagrant/plugin/remote/provider.rb
+++ lib/vagrant/plugin/remote/provider.rb
@@ -31,6 +31,25 @@ module Vagrant
           client.action(@machine.to_proto, name)
         end
 
+        # Executes the capability with the given name, optionally passing more
+        # arguments onwards to the capability. If the capability returns a value,
+        # it will be returned.
+        #
+        # @param [Symbol] cap_name Name of the capability
+        def capability(cap_name, *args)
+          @logger.debug("running remote provider capability #{cap_name} with args #{args}")
+          client.capability(cap_name, *args)
+        end
+
+        # Tests whether the given capability is possible.
+        #
+        # @param [Symbol] cap_name Capability name
+        # @return [Boolean]
+        def capability?(cap_name)
+          @logger.debug("checking for remote provider capability #{cap_name}")
+          client.has_capability?(cap_name)
+        end
+
         def machine_id_changed
           client.machine_id_changed(@machine.to_proto)
         end
