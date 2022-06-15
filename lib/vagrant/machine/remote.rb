adadf26c9c9e30d63fb324941e8f3c65cbda3406
sophia <scastellarin95@gmail.com>
2022-06-15 11:08:45 -0500

    Check if communicator is available before detecting guests

--- lib/vagrant/machine/remote.rb
+++ lib/vagrant/machine/remote.rb
@@ -85,7 +85,6 @@ module Vagrant
       end
 
       def guest
-        # require "pry-remote"; binding.pry_remote
         raise Errors::MachineGuestNotReady if !communicate.ready?
         if !@guest
           @guest = Guest.new(self, nil, nil)
