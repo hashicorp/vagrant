b0c585214554f8786ad0838d83ec32e0208a52f2
Chris Roberts <croberts@hashicorp.com>
2023-05-18 11:49:42 -0700

    Move constants to isolated file to be loaded on-demand
    Since the protos are only loaded when the serve command is invoked, move
    the constant alias definitions into an isolated file so they are only
    loaded when needed by the serve command

---
+++ plugins/commands/serve/constants.rb
@@ -0,0 +1,8 @@
+module VagrantPlugins
+  module CommandServe
+    # Simple constant aliases to reduce namespace typing
+    SDK = Hashicorp::Vagrant::Sdk
+    SRV = Hashicorp::Vagrant
+    Empty = ::Google::Protobuf::Empty
+  end
+end
