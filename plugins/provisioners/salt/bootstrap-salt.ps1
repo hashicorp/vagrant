7db87b9da37e019e0201e0f8c7517da553a8910f
Allison Larson <allison.larson@hashicorp.com>
2023-05-18 17:06:27 -0700

    Salt: Remove old bootstrap scripts

--- plugins/provisioners/salt/bootstrap-salt.ps1
+++
@@ -1,5 +0,0 @@
-# Define script root for PowerShell 2.0
-$ScriptRoot = Split-Path $script:MyInvocation.MyCommand.Path
-
-# Run the upstream bootstrap script with passthrough arguments
-& "${ScriptRoot}\bootstrap_salt_upstream.ps1" @args
--- plugins/provisioners/salt/bootstrap-salt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -
-if [ -e bootstrap-salt.sh ]; then
-  sh bootstrap-salt.sh "$@"
-else
-  exit 1
-fi
